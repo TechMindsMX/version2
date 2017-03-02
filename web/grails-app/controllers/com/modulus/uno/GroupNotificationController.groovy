6178281106e1c044897ccf67d75eb47648d3f7d8
carlogilmar <carlogilmar12@gmail.com>
2017-03-01 19:11:49 -0600

    #171 Refactorizando GroupNotification Controller

--- web/grails-app/controllers/com/modulus/uno/GroupNotificationController.groovy
+++ web/grails-app/controllers/com/modulus/uno/GroupNotificationController.groovy
@@ -17,26 +17,31 @@ class GroupNotificationController {
       render (view:"create", model: [emailers : emailerStorage, users:corporateService.findCorporateUsers(session.corporate.id)])
     }
 
-    def save(GroupNotificationCommand groupNotificationCommand){
-      def usersCorporate = corporateService.findCorporateUsers(session.corporate.id)
-      groupNotificationService.addNewGroup(groupNotificationCommand, usersCorporate)
+    //TODO: Pasar al command
+    def save(GroupNotificationCommand command){
+      def groupUsers = groupNotificationService.getUserList(command.userList)
+      GroupNotification group = new GroupNotification(name:command.nameGroup, notificationId:command.notificationId, users:groupUsers)
+      //groupNotificationService.saveNewGroup(group.toDomain())
+      groupNotificationService.saveNewGroup(group)
       redirect action:"index", method:"GET"
     }
 
     def edit(){
-      def groupNotification = groupNotificationService.getGroup(params.id)
       def emailerStorage = emailerClientService.getEmailerStorage()
+      def groupNotification = groupNotificationService.getGroup(params.id)
       def usersCorporate = corporateService.findCorporateUsers(session.corporate.id)
       def usersWithoutGroup = groupNotificationService.getUserListWithoutGroup(groupNotification.users, usersCorporate)
       render (view:"edit", model: [group: groupNotification, emailer: emailerStorage, usersEmpty: usersWithoutGroup])
      }
 
-    def update (GroupNotificationCommand groupNotificationCommand){
-      def usersCorporate = corporateService.findCorporateUsers(session.corporate.id)
-      groupNotificationService.editGroup(groupNotificationCommand, usersCorporate)
+    //TODO: Pasar al command
+    def update (GroupNotificationCommand command){
+      def groupUsers = groupNotificationService.getUserList(command.userList)
+      def updateParams = ["id":command.idGroup, "name":command.nameGroup, "notification":command.notificationId, "users":groupUsers]
+      //groupNotificationService.updateGroup(command.toMap())
+      groupNotificationService.updateGroup(updateParams)
       redirect action:"index", method:"GET"
     }
-
 
     def delete(){
       groupNotificationService.deleteGroup(params.id)
