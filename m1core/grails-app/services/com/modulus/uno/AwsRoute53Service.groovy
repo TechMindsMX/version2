9347e9a6cf920b164f0c8e3d218f41cc19659254
neodevelop <juan@makingdevs.com>
2017-03-02 11:51:38 -0600

    #180 Ajustando las variables de aws pero desde la configuracion en lugar de env system

--- m1core/grails-app/services/com/modulus/uno/AwsRoute53Service.groovy
+++ m1core/grails-app/services/com/modulus/uno/AwsRoute53Service.groovy
@@ -1,15 +1,18 @@
 package com.modulus.uno
 
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.route53.AmazonRoute53Client;
-import com.amazonaws.services.route53.model.*;
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.route53.AmazonRoute53Client
+import com.amazonaws.services.route53.model.*
+import grails.util.Holders as H
 
 class AwsRoute53Service {
 
-    private final String AWS_ACCESS_KEY = System.env['AWS_ACCESS_KEY_ID']
-    private final String AWS_SECRET_KEY = System.env['AWS_SECRET_ACCESS_KEY']
-    private final String HOST_ZONE_ID = System.env['HOST_ZONE_ID']
-    private final String VALUE_HOST = System.env['VALUE_HOST_IP']
+    def grailsApplication
+
+    private final String AWS_ACCESS_KEY = H.grailsApplication.config.grails.plugin.awssdk.accessKey
+    private final String AWS_SECRET_KEY = H.grailsApplication.config.grails.plugin.awssdk.secretKey
+    private final String HOST_ZONE_ID = H.grailsApplication.config.grails.plugin.awssdk.host.zone.id
+    private final String VALUE_HOST = H.grailsApplication.config.grails.plugin.awssdk.value.host.ip
 
    void createRecordSet(String domainName,String completeUrl, String valueHost) {
     AmazonRoute53Client route53Client = new AmazonRoute53Client(new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY));
