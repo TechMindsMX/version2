0d6b6ca7d512bc0d5107d1cc367f8277f0747aac
egjimenezg <egjimenezg@gmail.com>
2017-03-03 12:54:23 -0600

    #186 actualizando datos del autocomplete al crear una nueva transición

--- web/grails-app/assets/javascripts/machine/machine_create_controller.js
+++ web/grails-app/assets/javascripts/machine/machine_create_controller.js
@@ -1,4 +1,5 @@
 //= require third-party/jquery-validation/dist/jquery.validate.js
+//= require third-party/EasyAutocomplete/dist/jquery.easy-autocomplete.js
 //= require third-party/d3/d3.js
 //= require helpers/machine_helpers.js
 //= require machine/machine.js
@@ -13,7 +14,7 @@ var MachineCreateController = (function(){
     stateTo:'input[name=stateTo]',
     transitionsDiv:'#transitionsDiv'
   },
-  machine = null,  
+  machine = null,
   svg = null,
   inner = null,
   render = null,
@@ -39,24 +40,33 @@ var MachineCreateController = (function(){
     $(selectors.action).rules("add",{required:true});
   },
 
+  updateAutocomplete = function(){
+    var stateNames = [];    
+    $.each(machine.getStates(),function(index,state){
+      stateNames.push(state.name);
+    });
+
+    var options = {
+      data:stateNames,
+      theme:'blue-light'
+    };
+
+    $(selectors.stateTo).easyAutocomplete(options);
+  },
+
   addNewTransition = function(event){
     event.preventDefault();
+
     var form = $(event.currentTarget);
 
     if(form.valid()){
-      if(machine.getStates().length == 0){
-        machine.addInitialState($(selectors.stateFrom).val());
-        MachineCreateView.render('#transitions-form-template','#transitionsDiv',{states:machine.getStates()});
-        addNewRules();
-      }
-      else{
-        machine.addTransition({stateFrom:$(selectors.stateFrom).val(),
-                               stateTo:$(selectors.stateTo).val(),
-                               action:$(selectors.action).val()});
-      }
+      machine.addTransition({stateFrom:$(selectors.stateFrom).val(),
+                             stateTo:$(selectors.stateTo).val(),
+                             action:$(selectors.action).val()});
       updateFromSelect();
     }
 
+    updateAutocomplete();
     renderGraph(machine.getGraph());
   },
 
@@ -81,17 +91,22 @@ var MachineCreateController = (function(){
   createInitialState = function(event){
     event.preventDefault();
     machine.addInitialState($(selectors.stateFrom).val());
-    MachineCreateView.render('#transitions-form-template','#machineCreationDiv',{states:machine.getStates()});
+    MachineCreateView.render('#transitions-form-template','#transitionsDiv',{states:machine.getStates()});
+    addNewRules();
+    $(selectors.machineForm).unbind('submit');
+    $(selectors.machineForm).on('submit',addNewTransition);
+    updateAutocomplete();
+    renderGraph(machine.getGraph());
   },
 
   bindEvents = function(){
-    $(selectors.machineForm).on('submit',addNewTransition);
+    $(selectors.machineForm).on('submit',createInitialState);
   },
 
   renderGraph = function(graph){
     render(inner, graph);
-    var xCenterOffset = ($('svg').width() - graph.graph().width) / 2;
-    inner.attr("transform", "translate(" + xCenterOffset + ", 20)");
+    var center = ($('svg').width() - graph.graph().width) / 2;
+    inner.attr("transform", "translate(" + center + ", 20)");
   },
 
   start = function(){
