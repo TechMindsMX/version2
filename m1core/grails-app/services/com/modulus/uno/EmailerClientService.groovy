3e2136eb1e20711c3e4851c1d214219e25c4b3de
carlogilmar <carlogilmar12@gmail.com>
2017-03-07 16:58:58 -0600

    #188 Ajustando Emailer Client Service y su prueba

--- m1core/grails-app/services/com/modulus/uno/EmailerClientService.groovy
+++ m1core/grails-app/services/com/modulus/uno/EmailerClientService.groovy
@@ -7,64 +7,42 @@ class EmailerClientService {
 
   WsliteRequestService wsliteRequestService
   NotifyService notifyService
+  EmailerDataService emailerDataService
 
-  def findAllSubjects(){
-    def storageEmailer = callEmailerServiceStorage()
-    def emailersList = obtainSubjectList(storageEmailer)
-  }
-
-  def findAllContents(){
-    def storageEmailer = callEmailerServiceStorage()
-    def emailersList = obtainContentList(storageEmailer)
-  }
-
-  def findSubject(def idEmailer){
-    def emailerStorage = findAllSubjects()
-    def emailerFound = findEmailer(idEmailer, emailerStorage)
-    emailerFound.subject
-  }
-
-  def findContent(def idEmailer){
-    def emailerStorage = findAllContents()
-    def emailerFound = findEmailer(idEmailer, emailerStorage)
-    emailerFound.content
-  }
-
-  def findEmailer(def idEmailer, def emailerStorage){
-    emailerStorage.find{
-      it.containsValue(idEmailer)
-    }
-  }
-
-  //Servicio para implementar en máquina de estados.
-  def sendNotifyToGroup(def idGroup, def paramsEmailer){
-    GroupNotification group = GroupNotification.findById(idGroup)
-    def userEmails = obtainEmailList(group.users)
-    notifyService.sendEmailNotifications(userEmails, group.notificationId, paramsEmailer)
-  }
-
-  //TODO: Externalizar URL
-  private callEmailerServiceStorage(){
+ //TODO: Externalizar URL
+  def callEmailerServiceStorage(){
     wsliteRequestService.doRequest("http://emailerv2.modulusuno.com"){
       endpointUrl "/show"
     }.doit().json
   }
 
-  private obtainEmailList(def users){
-    users.collect{ user ->
-      user.profile.email
-    }
+  def findAllSubjects(){
+    def storageEmailer = callEmailerServiceStorage()
+    def emailersList = emailerDataService.obtainSubjectList(storageEmailer)
   }
 
-  private obtainSubjectList(def emailerStorage){
-    emailerStorage.collect{ emailer ->
-      ["id":emailer._id, "subject":emailer.subject]
-    }
+  def findAllContents(){
+    def storageEmailer = callEmailerServiceStorage()
+    def emailersList =  emailerDataService.obtainContentList(storageEmailer)
   }
 
-  private obtainContentList(def emailerStorage){
-    emailerStorage.collect{ emailer ->
-      ["id":emailer._id, "content":emailer.content]
-    }
+  def findSubject(def idEmailer){
+    def emailerStorage = findAllSubjects()
+    def emailerFound =   emailerDataService.findEmailer(idEmailer, emailerStorage)
+    emailerFound.subject
   }
+
+  def findContent(def idEmailer){
+    def emailerStorage = findAllContents()
+    def emailerFound =   emailerDataService.findEmailer(idEmailer, emailerStorage)
+    emailerFound.content
+  }
+
+  //Servicio para implementar en máquina de estados.
+  def sendNotifyToGroup(def idGroup, def paramsEmailer){
+    GroupNotification group = GroupNotification.findById(idGroup)
+    def userEmails =   emailerDataService.obtainEmailList(group.users)
+    notifyService.sendEmailNotifications(userEmails, group.notificationId, paramsEmailer)
+  }
+
 }
