beba412b7a90c1acb96fa32fc9751539321d2ac7
carlogilmar <carlogilmar12@gmail.com>
2017-03-07 17:03:40 -0600

    #188 Renombrando métodos en EmailerClientService

--- m1core/grails-app/services/com/modulus/uno/EmailerClientService.groovy
+++ m1core/grails-app/services/com/modulus/uno/EmailerClientService.groovy
@@ -9,40 +9,38 @@ class EmailerClientService {
   NotifyService notifyService
   EmailerDataService emailerDataService
 
- //TODO: Externalizar URL
-  def callEmailerServiceStorage(){
-    wsliteRequestService.doRequest("http://emailerv2.modulusuno.com"){
-      endpointUrl "/show"
-    }.doit().json
-  }
-
   def findAllSubjects(){
-    def storageEmailer = callEmailerServiceStorage()
+    def storageEmailer = obtainEmailerStorage()
     def emailersList = emailerDataService.obtainSubjectList(storageEmailer)
   }
 
   def findAllContents(){
-    def storageEmailer = callEmailerServiceStorage()
-    def emailersList =  emailerDataService.obtainContentList(storageEmailer)
+    def storageEmailer = obtainEmailerStorage()
+    def emailersList = emailerDataService.obtainContentList(storageEmailer)
   }
 
   def findSubject(def idEmailer){
     def emailerStorage = findAllSubjects()
-    def emailerFound =   emailerDataService.findEmailer(idEmailer, emailerStorage)
+    def emailerFound = emailerDataService.findEmailer(idEmailer, emailerStorage)
     emailerFound.subject
   }
 
   def findContent(def idEmailer){
     def emailerStorage = findAllContents()
-    def emailerFound =   emailerDataService.findEmailer(idEmailer, emailerStorage)
+    def emailerFound = emailerDataService.findEmailer(idEmailer, emailerStorage)
     emailerFound.content
   }
 
-  //Servicio para implementar en máquina de estados.
   def sendNotifyToGroup(def idGroup, def paramsEmailer){
     GroupNotification group = GroupNotification.findById(idGroup)
     def userEmails =   emailerDataService.obtainEmailList(group.users)
     notifyService.sendEmailNotifications(userEmails, group.notificationId, paramsEmailer)
   }
 
+//TODO: Externalizar URL
+  private obtainEmailerStorage(){
+    wsliteRequestService.doRequest("http://emailerv2.modulusuno.com"){
+      endpointUrl "/show"
+    }.doit().json
+  }
 }
