9de2ba05a2513073d83251a8802dedf71de783b2
cggg88jorge <jorge@makingdevs.com>
2017-03-09 15:20:34 -0600

    #192 Implementando la generacion de la firma primer acercamiento

---
+++ m1core/grails-app/services/com/modulus/uno/SignService.groovy
@@ -0,0 +1,28 @@
+package com.modulus.uno
+
+class SignService {
+
+  def generateSign(Map data){
+    def listKeys = ["institucionContraparte","empresa",
+    "fechaDeOperacion","folioOrigen","claveDeRastreo",
+    "institucionOperante","montoDelPago","tipoDelPago",
+    "tipoDeLaCuentaDelOrdenante","nombreDelOrdenante",
+    "cuentaDelOrdenante","rfcCurpDelOrdenante",
+    "tipoDeCuentaDelBeneficiario","nombreDelBeneficiario",
+    "cuentaDelBeneficiario","rfcCurpDelBeneficiario",
+    "emailDelBeneficiario","tipoDeCuentaDelBeneficiario2",
+    "nombreDelBeneficiario2","cuentaDelBeneficiario2",
+    "rfcCurpDelBeneficiario2","conceptoDelPago",
+    "conceptoDelPago2","claveDelCatalogoDeUsuario1",
+    "claveDelCatalogoDeUsuario2","claveDelPago",
+    "referenciaDeCobranza","referenciaNumérica",
+    "tipoDeOperación","topologia","usuario",
+    "medioDeEntrega","prioridad"]
+    if (!(listKeys == data*.key.intersect(listKeys)))
+      throw new SignException("Datos faltantes para la generación de la firma")
+    def sign = listKeys.collect{ String key ->
+      data."$key"
+    }.join("|")
+    "||${sign}||"
+  }
+}
