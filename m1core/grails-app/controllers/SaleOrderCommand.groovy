a0b8e21005810181ac5926263aea881b4cf28fae
temocpalma <temopalmag@gmail.com>
2017-03-09 14:39:25 -0600

    #193 Ajustando el command de orden de venta considerando los parámetros del webservice

--- m1core/grails-app/controllers/SaleOrderCommand.groovy
+++ m1core/grails-app/controllers/SaleOrderCommand.groovy
@@ -15,23 +15,50 @@ class SaleOrderCommand implements Validateable {
   String paymentMethod
   String externalId
 
-  SaleOrder createSaleOrder() {
+  static constraints = {
+    currencyUsd blank:false, size:3..3, validator: { val -> val in ["MXN","USD"] }
+  }
+
+  SaleOrder createOrUpdateSaleOrder() {
     Company company = Company.get(this.companyId)
     BusinessEntity businessEntity = BusinessEntity.get(this.clientId)
     Address address = Address.get(this.addressId)
-    SaleOrder saleOrder = new SaleOrder(
-      rfc:businessEntity.rfc,
-      clientName: businessEntity.toString(),
-      company:company,
-      externalId:this.externalId,
-      note:this.note,
-      paymentMethod:PaymentMethod.find { it.toString() == this.paymentMethod },
-      status:SaleOrderStatus.CREADA,
-      fechaCobro:Date.parse("dd/MM/yyyy", this.fechaCobro),
-      currency:this.currencyUsd,
-      changeType:getValueInBigDecimal(this.changeType),
-    )
-    saleOrder.addToAddresses(address)
+
+    SaleOrder saleOrder
+    if (this.externalId) {
+      saleOrder = SaleOrder.findByCompanyAndExternalIdAndStatusInList(company, this.externalId, [SaleOrderStatus.CREADA, SaleOrderStatus.POR_AUTORIZAR])
+    }
+
+    PaymentMethod paymentMethod
+    if (this.paymentMethod == "0" || this.paymentMethod == "1" || this.paymentMethod == "2" || this.paymentMethod == "3") {
+      paymentMethod = PaymentMethod.find { it.ordinal().toString() == this.paymentMethod }
+    } else {
+      paymentMethod = PaymentMethod.find { it.toString() == this.paymentMethod }
+    }
+
+    if (saleOrder) {
+       saleOrder.rfc = businessEntity.rfc
+       saleOrder.clientName =  businessEntity.toString()
+       saleOrder.note = this.note
+       saleOrder.paymentMethod = paymentMethod
+       saleOrder.fechaCobro = Date.parse("dd/MM/yyyy", this.fechaCobro)
+       saleOrder.currency = this.currencyUsd
+       saleOrder.changeType = getValueInBigDecimal(this.changeType ?: "0")
+    } else {
+      saleOrder = new SaleOrder(
+        rfc:businessEntity.rfc,
+        clientName: businessEntity.toString(),
+        company:company,
+        externalId:this.externalId,
+        note:this.note,
+        paymentMethod:paymentMethod,
+        status:SaleOrderStatus.CREADA,
+        fechaCobro:Date.parse("dd/MM/yyyy", this.fechaCobro),
+        currency:this.currencyUsd,
+        changeType:getValueInBigDecimal(this.changeType ?: "0")
+      )
+      saleOrder.addToAddresses(address)
+    }
     saleOrder
   }
 
