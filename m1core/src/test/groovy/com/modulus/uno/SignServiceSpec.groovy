1ec1938a32ef0ec1d569b873a774d4618f0cb40f
cggg88jorge <jorge@makingdevs.com>
2017-03-09 16:53:29 -0600

    #192 Agregando caso de prueba real de una orden de pago

--- m1core/src/test/groovy/com/modulus/uno/SignServiceSpec.groovy
+++ m1core/src/test/groovy/com/modulus/uno/SignServiceSpec.groovy
@@ -41,12 +41,13 @@ class SignServiceSpec extends Specification {
         topologia: "example30",
         usuario: "example31",
         medioDeEntrega: "example32",
-        prioridad: "example33"
+        prioridad: "example33",
+        iva: "example34"
       ]
     when:"call service to generate sign"
       def sign = service.generateSign(map)
     then:
-      sign == "||example1|example2|example3|example4|example5|example6|example7|example8|example9|example10|example11|example12|example13|example14|example15|example16|example17|example18|example19|example20|example21|example22|example23|example24|example25|example26|example27|example28|example29|example30|example31|example32|example33||"
+      sign == "||example1|example2|example3|example4|example5|example6|example7|example8|example9|example10|example11|example12|example13|example14|example15|example16|example17|example18|example19|example20|example21|example22|example23|example24|example25|example26|example27|example28|example29|example30|example31|example32|example33|example34||"
   }
 
   void "Should throw exception because the info is incorrect"() {
@@ -63,6 +64,50 @@ class SignServiceSpec extends Specification {
       service.generateSign(map)
     then:
       thrown SignException
+  }
+
+  void "Should generate sign for real Sale Order"() {
+    given:"The necessary and complete data"
+      Map map = [
+        institucionContraparte: "40002",
+        empresa: "TECHMINDS",
+        fechaDeOperacion: "20170306",  
+        folioOrigen: "",
+        claveDeRastreo: "1488820184033",
+        institucionOperante: "90646",
+        montoDelPago: "250.00",
+        tipoDelPago: "1",
+        tipoDeLaCuentaDelOrdenante: "",
+        nombreDelOrdenante: "TECHMINDS",
+        cuentaDelOrdenante: "",
+        rfcCurpDelOrdenante: "",
+        tipoDeCuentaDelBeneficiario: "40",
+        nombreDelBeneficiario: "Provider Soft Temoc Uno",
+        cuentaDelBeneficiario: "002180000201612076",
+        rfcCurpDelBeneficiario: "NA",
+        emailDelBeneficiario: "mailBeneficiary@mail.com",
+        tipoDeCuentaDelBeneficiario2: "",
+        nombreDelBeneficiario2: "",
+        cuentaDelBeneficiario2: "",
+        rfcCurpDelBeneficiario2: "",
+        conceptoDelPago: "PAGO A PROVEEDOR",
+        conceptoDelPago2: "",
+        claveDelCatalogoDeUsuario1: "",
+        claveDelCatalogoDeUsuario2: "",
+        claveDelPago: "",
+        referenciaDeCobranza: "",
+        referenciaNumérica: "1170306",
+        tipoDeOperación: "",
+        topologia: "",
+        usuario: "",
+        medioDeEntrega: "",
+        prioridad: "",
+        iva: ""
+      ]
+    when:"call service to generate sign"
+      def sign = service.generateSign(map)
+    then:
+      sign == "||40002|TECHMINDS|20170306||1488820184033|90646|250.00|1||TECHMINDS|||40|Provider Soft Temoc Uno|002180000201612076|NA|mailBeneficiary@mail.com|||||PAGO A PROVEEDOR||||||1170306||||||||"
   }
 
 }
