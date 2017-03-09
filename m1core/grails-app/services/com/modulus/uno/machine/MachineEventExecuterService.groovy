d8774732f3e90415273e30f9896b179358810f44
egjimenezg <egjimenezg@gmail.com>
2017-03-09 11:10:22 -0600

    #196 buscando beans que implementan la interface MachineEvent y ejecutando su evento

--- m1core/grails-app/services/com/modulus/uno/machine/MachineEventExecuterService.groovy
+++ m1core/grails-app/services/com/modulus/uno/machine/MachineEventExecuterService.groovy
@@ -1,6 +1,8 @@
 package com.modulus.uno.machine
 
 import grails.transaction.Transactional
+import org.springframework.context.ApplicationContext
+import grails.util.Holders
 
 @Transactional
 class MachineEventExecuterService{
@@ -8,7 +10,13 @@ class MachineEventExecuterService{
   def grailsApplication
 
   def executeEvents(){
+    ArrayList<MachineEvent> events = grailsApplication.serviceClasses.findAll{ serviceClazz -> MachineEvent.isAssignableFrom(serviceClazz.clazz) }
+    ApplicationContext ctx = Holders.getApplicationContext()
 
+    events.each{ event ->
+      def service = ctx.getBean(event.propertyName)
+      service.executeEvent()
+    }
   }
 
 }
