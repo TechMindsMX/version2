64489cf28dfc37db0b278fde4c9c5ff32df25f4a
cggg88jorge <jorge@makingdevs.com>
2017-03-13 12:14:36 -0600

    #197 posible manejo de la info

--- m1core/grails-app/services/com/modulus/uno/GenerateXMLService.groovy
+++ m1core/grails-app/services/com/modulus/uno/GenerateXMLService.groovy
@@ -3,9 +3,12 @@ package com.modulus.uno
 class GenerateXMLService {
 
   def xmlOrderSaleRequest(Map data){
+    def saleOrder = new SaleOrderSTP(data)
+    if (saleOrder.validate())
+      println "manejo de error"
     def engine = new groovy.text.GStringTemplateEngine()
     def tenplate = new File(getClass().getClassLoader().getResource("templateSaleOrder.xml").file).text
-    engine.createTemplate(tenplate).make(data).toString()
+    engine.createTemplate(tenplate).make(saleOrder.asMap()).toString()
   }
 
 }
