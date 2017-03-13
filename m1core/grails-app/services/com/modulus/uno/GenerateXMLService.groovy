f1898ae34d052e81453662ef3af1fc91a3e0fcbb
cggg88jorge <jorge@makingdevs.com>
2017-03-13 16:04:39 -0600

    #197 Menajando el error en el caso de que los datos para el xml sean incorrectos

--- m1core/grails-app/services/com/modulus/uno/GenerateXMLService.groovy
+++ m1core/grails-app/services/com/modulus/uno/GenerateXMLService.groovy
@@ -4,8 +4,11 @@ class GenerateXMLService {
 
   def xmlOrderSaleRequest(Map data){
     def saleOrder = new SaleOrderSTP(data)
-    if (saleOrder.validate())
-      println "manejo de error"
+    if (!saleOrder.validate()){
+      log.error "Datos Incorrectos para el XML"
+      log.error saleOrder.errors.toString()
+      throw new XMLException("Datos erroneos para la generacion del xml")
+    }
     def engine = new groovy.text.GStringTemplateEngine()
     def tenplate = new File(getClass().getClassLoader().getResource("templateSaleOrder.xml").file).text
     engine.createTemplate(tenplate).make(saleOrder.asMap()).toString()
