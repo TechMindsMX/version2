222737800b497b394d6aad7c14d6e9ccc732dd22
temocpalma <temopalmag@gmail.com>
2017-03-14 17:22:30 -0600

    #205 Eliminando función utilizada para habilitar la edición del campo tipo de cambio que ya no será solicitado al crear la orden de venta

--- web/grails-app/assets/javascripts/saleOrder/create.js
+++ web/grails-app/assets/javascripts/saleOrder/create.js
@@ -4,14 +4,3 @@ $( function() {
   });
 } );
 
-$("#currencyUsd").click(function() {
-  if ($("#currencyUsd").prop("checked")) {
-    $("#changeType").val("");
-    $("#changeType").focus();
-    $("#changeType").prop("readOnly", false);
-  } else {
-    $("#changeType").val("0.00");
-    $("#changeType").prop("readOnly", true);
-  }
-});
-
