c3b3483fa9c5af4153c40bb0297c38b414514d0a
egjimenezg <egjimenezg@gmail.com>
2017-03-14 10:08:04 -0600

    #200 enviando instancia inmutable al actor

--- m1core/grails-app/services/com/modulus/uno/machine/MachineEventExecuterService.groovy
+++ m1core/grails-app/services/com/modulus/uno/machine/MachineEventExecuterService.groovy
@@ -1,22 +1,27 @@
 package com.modulus.uno.machine
 
 import grails.transaction.Transactional
-import org.springframework.context.ApplicationContext
-import grails.util.Holders
+import javax.annotation.PostConstruct
 
 @Transactional
 class MachineEventExecuterService{
 
   def grailsApplication
+  MachineEventExecuterActorService machineEventExecuterActorService 
+
+  @PostConstruct 
+  private def init() {
+    machineEventExecuterActorService.start()
+  }
 
   def executeEvents(def instance){
-    ArrayList<MachineEvent> events = grailsApplication.serviceClasses.findAll{ serviceClazz -> MachineEvent.isAssignableFrom(serviceClazz.clazz) }
-    ApplicationContext ctx = Holders.getApplicationContext()
-
-    events.each{ event ->
-      def service = ctx.getBean(event.propertyName)
-      service.executeEvent(instance)
+    ArrayList<String> eventImplementerNames = grailsApplication.serviceClasses.findAll{ serviceClazz -> MachineEventImplementer.isAssignableFrom(serviceClazz.clazz) }*.propertyName
+    
+    eventImplementerNames.each{ eventImplementerName ->
+      EventImplementer eventImplementer = new EventImplementer(eventImplementerName:eventImplementerName,instance:instance)
+      machineEventExecuterActorService.send(eventImplementer)
     }
+
   }
 
 }
