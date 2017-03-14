eea3dcf586fc8ce7dc9831ede69425c551bf81d0
egjimenezg <egjimenezg@gmail.com>
2017-03-14 15:39:37 -0600

    #200 enviando mensaje al supervisor cuando hay un error

--- m1core/grails-app/services/com/modulus/uno/machine/MachineEventExecuterActorService.groovy
+++ m1core/grails-app/services/com/modulus/uno/machine/MachineEventExecuterActorService.groovy
@@ -4,16 +4,21 @@ import org.springframework.context.ApplicationContext
 import grails.util.Holders
 import groovyx.gpars.actor.DefaultActor
 
-class MachineEventExecuterActorService extends DefaultActor {
+class MachineEventExecuterActorService extends SupervisedActor {
   
   def grailsApplication
+  MachineEventExecuterSupervisorService machineEventExecuterSupervisorService
+
+  void onException(Throwable throwable){
+    machineEventExecuterSupervisorService << new SupervisedException(throwable)
+  }
 
   void act(){
     loop{
       react { EventImplementer eventImplementer ->
         try {
           def service = Holders.getApplicationContext().getBean(eventImplementer.eventImplementerName)
-          service.executeEvent(eventImplementer.instance)
+          service.executeEvent(eventImplementer.instanceClassName,eventImplementer.instanceId)
         }
         catch(Exception e){
           log.error "${e.properties}"
