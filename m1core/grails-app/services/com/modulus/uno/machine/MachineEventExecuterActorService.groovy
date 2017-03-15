0149c754fe838dbe70bed53ee1591cda2a0997e9
egjimenezg <egjimenezg@gmail.com>
2017-03-15 17:37:30 -0600

    #200 eliminando clase supervisada, cambiando el scope del servicio a prototype y quitando try and catch

--- m1core/grails-app/services/com/modulus/uno/machine/MachineEventExecuterActorService.groovy
+++ m1core/grails-app/services/com/modulus/uno/machine/MachineEventExecuterActorService.groovy
@@ -1,28 +1,24 @@
 package com.modulus.uno.machine
 
 import org.springframework.context.ApplicationContext
-import grails.util.Holders
 import groovyx.gpars.actor.DefaultActor
 
-class MachineEventExecuterActorService extends SupervisedActor {
-  
-  def grailsApplication
+class MachineEventExecuterActorService extends DefaultActor {
+
+  static scope = "prototype"
+
   MachineEventExecuterSupervisorService machineEventExecuterSupervisorService
+  def grailsApplication
 
   void onException(Throwable throwable){
-    machineEventExecuterSupervisorService << new SupervisedException(throwable)
+    machineEventExecuterSupervisorService << new SupervisedException(throwable.message)
   }
 
   void act(){
     loop{
       react { EventImplementer eventImplementer ->
-        try {
-          def service = Holders.getApplicationContext().getBean(eventImplementer.eventImplementerName)
-          service.executeEvent(eventImplementer.instanceClassName,eventImplementer.instanceId)
-        }
-        catch(Exception e){
-          log.error "${e.properties}"
-        }
+        def service = grailsApplication.mainContext.getBean(eventImplementer.eventImplementerName)
+        service.executeEvent(eventImplementer.instanceClassName,eventImplementer.instanceId)
       }
     }
   }
