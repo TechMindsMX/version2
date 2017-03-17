9e9355a53426dbeb384a36e34c82e585a681ddf3
egjimenezg <egjimenezg@gmail.com>
2017-03-17 11:40:01 -0600

    #201 agregando script del controller que iniciará los componentes

---
+++ web/grails-app/assets/javascripts/machine/machine_show_controller.js
@@ -0,0 +1,25 @@
+//= require machine/machine_view.js
+
+var MachineShowController = (function(){
+  
+  var selectors = {
+
+  },
+
+  bindEvents = function(){
+
+  },
+
+  start = function(){
+    console.log('Starting this controller');
+  };
+
+  return {
+    start:start
+  };
+
+}());
+
+jQuery(function($){
+  MachineShowController.start();
+});
