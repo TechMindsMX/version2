ed58a98c5e5ef0f3fe7e24df664965c67ccd6ae4
temocpalma <temopalmag@gmail.com>
2017-03-17 16:37:47 -0600

    #145 Quitando referencia foránea de conciliación en la tabla de pagos de orden de venta

--- web/grails-app/migrations/create-domain-sale-order-payment.groovy
+++ web/grails-app/migrations/create-domain-sale-order-payment.groovy
@@ -14,10 +14,6 @@ databaseChangeLog = {
                 constraints(nullable: "false")
             }
 
-            column(name: "conciliation_id", type: "BIGINT") {
-                constraints(nullable: "false")
-            }
-
             column(name: "date_created", type: "datetime") {
                 constraints(nullable: "false")
             }
@@ -30,10 +26,6 @@ databaseChangeLog = {
                 constraints(nullable: "false")
             }
         }
-    }
-
-    changeSet(author: "tim (generated)", id: "1489776853825-3") {
-        addForeignKeyConstraint(baseColumnNames: "conciliation_id", baseTableName: "sale_order_payment", constraintName: "FKq4dgsc1nkxnuc8q2sm7u2gies", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "conciliation")
     }
 
     changeSet(author: "tim (generated)", id: "1489776853825-5") {
