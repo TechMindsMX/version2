f4d2512189fdac934aa16f7103077e21b3b42f0c
carlogilmar <carlogilmar12@gmail.com>
2017-03-17 15:54:57 -0600

    #199 Ajustado command de NotificationForState

--- web/src/main/groovy/com/modulus/uno/NotificationForStateCommand.groovy
+++ web/src/main/groovy/com/modulus/uno/NotificationForStateCommand.groovy
@@ -4,12 +4,22 @@ import grails.validation.Validateable
 
 class NotificationForStateCommand implements Validateable{
 
-  Long idNotification
+  Long notification
   Long state
   def orderClass
   Long groupId
 
   NotificationForState toNotification(){
-    new NotificationForState(groupNotification: groupId, stateMachine:state )
+    new NotificationForState(
+      groupNotification: groupId,
+      stateMachine:state )
   }
+
+  NotificationForState toUpdateNotification(){
+    new NotificationForState(
+      id:notification,
+      groupNotification: groupId,
+      stateMachine:state )
+  }
+
 }
