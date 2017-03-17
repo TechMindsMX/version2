fff4483233a59b99346234df6dbd2691508b105b
egjimenezg <egjimenezg@gmail.com>
2017-03-16 18:26:13 -0600

    #201 agregando prueba para verificar que las transiciones se ordenan correctamente

---
+++ m1core/src/test/groovy/com/modulus/uno/machine/TransitionServiceSpec.groovy
@@ -0,0 +1,51 @@
+package com.modulus.uno.machine
+
+import grails.test.mixin.TestFor
+import grails.test.mixin.Mock
+import spock.lang.Specification
+import spock.lang.Ignore
+import java.lang.Void as Should
+
+@TestFor(TransitionService)
+@Mock([Machine,State])
+class TransitionServiceSpec extends Specification {
+
+  Should "get the transitions of a machine in the correct order"(){
+    given:"the machine"
+      State initialState = new State(name:"Created")
+      initialState.save()
+      Machine machine = new Machine(initialState:initialState)
+
+      ArrayList<State> states = [new State(name:"For authorizing"),
+                                 new State(name:"Authorized"),
+                                 new State(name:"Canceled")]
+
+      states.each{ state ->
+        machine.addToStates(state)
+      }
+
+      machine.save()
+    and:"its transitions"
+      ArrayList<Transition> transitions = [new Transition(stateFrom:initialState,
+                                                          stateTo:states[0],
+                                                          actions:["Ask for authorization"]):
+                                           new Transition(stateFrom:states[0],
+                                                          stateTo:states[1],
+                                                          actions:["Authorize"]),
+                                           new Transition(stateFrom:states[0],
+                                                          statesTo:states[1],
+                                                          actions:["Cancel"])]
+
+      transitions.each{ transition ->
+        machine.addToTransitions(transition)
+      }
+      
+      machine.save()
+    when:
+      ArrayList<Transition> transitions = service.getMachineTransitions(machine.id)
+    then:
+      transitions.fist().stateFrom.finalState
+      transitions.size() == 3
+  }
+
+}
