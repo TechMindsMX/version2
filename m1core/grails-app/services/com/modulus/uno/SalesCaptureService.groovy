b7a7695e76ac09303583d72ae65ce0edcfdd26a1
LeoVergara1 <leovergara.dark@gmail.com>
2017-03-17 16:16:48 -0600

    #194 Se modifico el  nombre de la clase

--- m1core/grails-app/services/com/modulus/uno/SalesCaptureService.groovy
+++ m1core/grails-app/services/com/modulus/uno/SalesCaptureService.groovy
@@ -7,14 +7,14 @@ class SalesCaptureService {
 
   def notifyService
 
-  def sendNotify(SalesCaptureCommand command){
+  def sendNotify(SalesCaptureService command){
     def emails = ["carlo@makingdevs.com", "brandon@makingdevs.com"]
     def idEmailer ="Pendiente"
     def paramsEmailer = prepareDataForEmailer(command)
     notifyService.sendEmailNotifications(emails, idEmailer, paramsEmailer)
   }
 
-  def prepareDataForEmailer(SaleCaptureCommand command){
+  def prepareDataForEmailer(SalesCaptureService command){
   }
 
 }
