4c3b085b823a7119bb07a4865f485b01fa3bf815
temocpalma <temopalmag@gmail.com>
2017-03-17 12:44:29 -0600

    #145 Agregando métodos para guardar la conciliación y para obtener la lista de conciliaciones por aplicar de un pago, así como implementando el cálculo del monto disponible por aplicar de un pago

--- m1core/grails-app/services/com/modulus/uno/ConciliationService.groovy
+++ m1core/grails-app/services/com/modulus/uno/ConciliationService.groovy
@@ -5,9 +5,22 @@ import grails.transaction.Transactional
 @Transactional
 class ConciliationService {
 
+  def springSecurityService
+
   def getTotalToApplyForPayment(Payment payment) {
-    BigDecimal applied = new BigDecimal(5000)//Conciliation.findAllByPaymentAndStatus(payment, ConciliationStatus.TO_APPLY).sum().amount
+    def conciliations = getConciliationsToApplyForPayment(payment)
+    BigDecimal applied = conciliations ? conciliations*.amount.sum() : new BigDecimal(0)
     payment.amount - applied
   }
 
+  List<Conciliation> getConciliationsToApplyForPayment(Payment payment) {
+    Conciliation.findAllByPaymentAndStatus(payment, ConciliationStatus.TO_APPLY)
+  }
+
+  List<Conciliation> saveConciliation(Conciliation conciliation) {
+    conciliation.user = springSecurityService.currentUser
+    log.info "Saving conciliation: ${conciliation.dump()}"
+    conciliation.save()
+    getConciliationsToApplyForPayment(conciliation.payment)
+  }
 }
