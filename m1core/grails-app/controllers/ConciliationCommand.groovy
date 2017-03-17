2a2f0f2b4675cd7f3b4b2bfb01de259635f89a97
temocpalma <temopalmag@gmail.com>
2017-03-17 10:42:46 -0600

    #145 Agregando campo de comentarios y método que crea la instancia de conciliación en el command de conciliación

--- m1core/grails-app/controllers/ConciliationCommand.groovy
+++ m1core/grails-app/controllers/ConciliationCommand.groovy
@@ -9,6 +9,23 @@ class ConciliationCommand implements Validateable {
   String saleOrderId
   String changeType
   String amountToApply
+  String comment
+
+  static constraints = {
+    paymentId nullable:true
+    saleOrderId nullable:true
+    comment nullable:true
+  }
+
+  Conciliation createConciliation() {
+    new Conciliation(
+      payment:this.payment ? Payment.get(this.payment) : null,
+      saleOrder:this.saleOrderId ? SaleOrder.get(this.saleOrderId) : null,
+      changeType:getValueInBigDecimal(this.changeType),
+      amount:getValueInBigDecimal(this.amountApply),
+      comment:this.comment
+    )
+  }
 
   private def getValueInBigDecimal(String value) {
     Locale.setDefault(new Locale("es","MX"));
