a9c35aaaf5d985d164ebaaee577dee181b2c42a4
egjimenezg <egjimenezg@gmail.com>
2017-03-16 19:00:00 -0600

    #201 corrigiendo prueba

--- m1core/src/test/groovy/com/modulus/uno/machine/TransitionServiceSpec.groovy
+++ m1core/src/test/groovy/com/modulus/uno/machine/TransitionServiceSpec.groovy
@@ -7,7 +7,7 @@ import spock.lang.Ignore
 import java.lang.Void as Should
 
 @TestFor(TransitionService)
-@Mock([Machine,State])
+@Mock([Machine,State,Transition])
 class TransitionServiceSpec extends Specification {
 
   Should "get the transitions of a machine in the correct order"(){
@@ -28,12 +28,12 @@ class TransitionServiceSpec extends Specification {
     and:"its transitions"
       ArrayList<Transition> transitions = [new Transition(stateFrom:initialState,
                                                           stateTo:states[0],
-                                                          actions:["Ask for authorization"]):
+                                                          actions:["Ask for authorization"]),
                                            new Transition(stateFrom:states[0],
                                                           stateTo:states[1],
                                                           actions:["Authorize"]),
                                            new Transition(stateFrom:states[0],
-                                                          statesTo:states[1],
+                                                          stateTo:states[1],
                                                           actions:["Cancel"])]
 
       transitions.each{ transition ->
@@ -42,10 +42,10 @@ class TransitionServiceSpec extends Specification {
       
       machine.save()
     when:
-      ArrayList<Transition> transitions = service.getMachineTransitions(machine.id)
+      ArrayList<Transition> orderedTransitions = service.getMachineTransitions(machine.id)
     then:
-      transitions.fist().stateFrom.finalState
-      transitions.size() == 3
+      orderedTransitions.first().stateFrom.finalState
+      orderedTransitions.size() == 3
   }
 
 }
