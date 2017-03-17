08fab87d3aec8338fad47ba0eb3aa3cd3c4a17f8
temocpalma <temopalmag@gmail.com>
2017-03-17 09:38:09 -0600

    #145 Complementando domain de conciliación

--- m1core/grails-app/domain/com/modulus/uno/Conciliation.groovy
+++ m1core/grails-app/domain/com/modulus/uno/Conciliation.groovy
@@ -6,9 +6,18 @@ class Conciliation {
   SaleOrder saleOrder
   Payment payment
   BigDecimal amount
+  BigDecimal changeType
   ConciliationStatus status = ConciliationStatus.TO_APPLY
+  String comment
 
   Date dateCreated
   Date lastUpdated
 
+  static constraints = {
+    amount nullable:false, min:0.01
+    user nullable:false
+    saleOrder nullable:true
+    payment nullable:true
+    comment nullable:true, blank:true
+  }
 }
