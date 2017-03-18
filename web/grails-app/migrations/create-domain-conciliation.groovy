55a2a6824b1499a84c884666b9ea4076ccc00401
temocpalma <temopalmag@gmail.com>
2017-03-17 19:04:12 -0600

    #145 Regenerando changeset que agrega tabla para las conciliaciones y movimiendo la vista para crear conciliaciones a la carpeta de conciliación

--- web/grails-app/migrations/create-domain-conciliation.groovy
+++ web/grails-app/migrations/create-domain-conciliation.groovy
@@ -1,6 +1,6 @@
 databaseChangeLog = {
 
-    changeSet(author: "tim (generated)", id: "1489764631462-1") {
+    changeSet(author: "tim (generated)", id: "1489796434457-1") {
         createTable(tableName: "conciliation") {
             column(autoIncrement: "true", name: "id", type: "BIGINT") {
                 constraints(primaryKey: "true", primaryKeyName: "conciliationPK")
@@ -19,6 +19,10 @@ databaseChangeLog = {
             }
 
             column(name: "comment", type: "VARCHAR(255)")
+
+            column(name: "company_id", type: "BIGINT") {
+                constraints(nullable: "false")
+            }
 
             column(name: "date_created", type: "datetime") {
                 constraints(nullable: "false")
@@ -42,15 +46,19 @@ databaseChangeLog = {
         }
     }
 
-    changeSet(author: "tim (generated)", id: "1489764631462-3") {
+    changeSet(author: "tim (generated)", id: "1489796434457-3") {
+        addForeignKeyConstraint(baseColumnNames: "company_id", baseTableName: "conciliation", constraintName: "FK6r7ncouvhn19acm966xkhve0v", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "company")
+    }
+
+    changeSet(author: "tim (generated)", id: "1489796434457-4") {
         addForeignKeyConstraint(baseColumnNames: "user_id", baseTableName: "conciliation", constraintName: "FKalc2br1vp6dlxkoxav5ihrot1", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "user")
     }
 
-    changeSet(author: "tim (generated)", id: "1489764631462-4") {
+    changeSet(author: "tim (generated)", id: "1489796434457-5") {
         addForeignKeyConstraint(baseColumnNames: "payment_id", baseTableName: "conciliation", constraintName: "FKj3lmtf83iehup9xfj3eahu88m", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "payment")
     }
 
-    changeSet(author: "tim (generated)", id: "1489764631462-5") {
+    changeSet(author: "tim (generated)", id: "1489796434457-6") {
         addForeignKeyConstraint(baseColumnNames: "sale_order_id", baseTableName: "conciliation", constraintName: "FKocpmj8v0mkrcjegkeinltjst9", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "sale_order")
     }
 
