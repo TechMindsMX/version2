84b573e904626d43971abbdd8526eb224dcb6b68
temocpalma <temopalmag@gmail.com>
2017-03-21 18:15:38 -0600

    #145 Agregando test del método que agrega un pago a una orden de venta

--- m1core/src/test/groovy/com/modulus/uno/SaleOrderServiceSpec.groovy
+++ m1core/src/test/groovy/com/modulus/uno/SaleOrderServiceSpec.groovy
@@ -9,7 +9,7 @@ import org.springframework.mock.web.MockHttpServletRequest
 import grails.web.servlet.mvc.GrailsParameterMap
 
 @TestFor(SaleOrderService)
-@Mock([BusinessEntity, SaleOrder, SaleOrderItem, Company, User, Address,Authorization, Commission])
+@Mock([BusinessEntity, SaleOrder, SaleOrderItem, Company, User, Address,Authorization, Commission, SaleOrderPayment])
 class SaleOrderServiceSpec extends Specification {
 
   def items = []
@@ -181,6 +181,25 @@ class SaleOrderServiceSpec extends Specification {
       fechaCobro    | originalDate  | sendDate      | fechaCobroOriginal
       new Date()    | null          | new Date()+4  | new Date()
       new Date()+12 | new Date()+40 | new Date()+40 | new Date()+40
+  }
+
+  @Unroll
+  void "Should add payment to sale order when amount is #amount then amountToPay is #amountToPay and status is #status"() {
+    given:"A sale order"
+      SaleOrder saleOrder = new SaleOrder(status:SaleOrderStatus.EJECUTADA)
+    and:"Item"
+      SaleOrderItem item = new SaleOrderItem(price:100, ieps:0, iva:0, quantity:1).save(validate:false)
+      saleOrder.addToItems(item)
+      saleOrder.save(validate:false)
+    when:
+      def result = service.addPaymentToSaleOrder(saleOrder, amount)
+    then:
+      result.amountToPay == amountToPay
+      result.status == status
+    where:
+    amount    ||  amountToPay   |   status
+    20        ||  80            | SaleOrderStatus.EJECUTADA
+    100       ||  0             | SaleOrderStatus.PAGADA
   }
 
 }
