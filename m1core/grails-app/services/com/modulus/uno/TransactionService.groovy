d907f5b5d43255014911b4b4d42d0a2505fbc775
cggg88jorge <jorge@makingdevs.com>
2017-03-22 12:30:02 -0600

    #210 Ajustando el tipo de propagacion para la transaccion

--- m1core/grails-app/services/com/modulus/uno/TransactionService.groovy
+++ m1core/grails-app/services/com/modulus/uno/TransactionService.groovy
@@ -1,10 +1,12 @@
 package com.modulus.uno
 
 import grails.transaction.Transactional
+import org.springframework.transaction.annotation.Propagation
 
 @Transactional
 class TransactionService {
 
+  @Transactional(propagation = Propagation.REQUIRES_NEW)
   Transaction saveTransaction(Transaction transaction) {
     transaction.save()
     transaction
