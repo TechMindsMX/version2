45210201f84e887d1602b2f2841a0e7b8223434e
carlogilmar <carlogilmar12@gmail.com>
2017-03-23 15:14:09 -0600

    #212 Implementando prueba unitaria de EventNotificationService

--- m1core/src/test/groovy/com/modulus/uno/EventNotificationServiceSpec.groovy
+++ m1core/src/test/groovy/com/modulus/uno/EventNotificationServiceSpec.groovy
@@ -3,33 +3,36 @@ package com.modulus.uno
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 import grails.test.mixin.Mock
+import com.modulus.uno.machine.*
 
 @TestFor(EventNotificationService)
-@Mock([State, GroupNotification, NotificationForState])
+@Mock([PurchaseOrder, State, User, GroupNotification, NotificationForState])
 class EventNotificationServiceSpec extends Specification {
 
   MachineService machineService = Mock(MachineService)
   NotificationForStateService notificationForStateService = Mock(NotificationForStateService)
+  NotifyService notifyService = Mock(NotifyService)
 
   def setup(){
     service.machineService = machineService
     service.notificationForStateService = notificationForStateService
+    service.notifyService = notifyService
   }
 
   void "Send email notification when execute an event"(){
     given:"a class name and a instance id"
-      def order = new PurchaseOrder(providerName:"ProviderName").save(validate:false)
-      State secondState = new State(name:"second state", finalState:true).save(validate:false)
       GroupNotification group = new GroupNotification(notificationId:"123456qwer", name:"Contadores").save(validate:false)
+      def order = new PurchaseOrder(providerName:"ProviderName").save(validate:false)
+      State firstState = new State(name:"machine state", finalState:true).save(validate:false)
       NotificationForState notify = new NotificationForState(groupNotification:1, stateMachine:1).save(validate:false)
-    and:
-      machineService.getCurrentStateOfInstance(_) >> secondState
-      notificationForStateService.findByState(_) >> notify
-    when:
+
+    when:"we want to notify a group"
       service.executeEvent(order.class.simpleName, 1)
-    then:
-      true == false
-      //3 * restService.sendEmailToEmailer(_)
+
+    then:"We should call the services"
+      1 * machineService.getCurrentStateOfInstance(_) >> firstState
+      1 * notificationForStateService.findByState(_) >> notify
+      1 * notifyService.sendEmailToGroup(_, _)
   }
 
 }
