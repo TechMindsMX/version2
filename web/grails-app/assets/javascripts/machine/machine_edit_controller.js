ba0465c11ea0577c078fdf19de1fd0850da57b45
egjimenezg <egjimenezg@gmail.com>
2017-03-24 11:51:26 -0600

    #216 obteniendo la máquina para pintarla en la vista

--- web/grails-app/assets/javascripts/machine/machine_edit_controller.js
+++ web/grails-app/assets/javascripts/machine/machine_edit_controller.js
@@ -1,15 +1,48 @@
 //= require third-party/jquery-validation/dist/jquery.validate.js
 //= require third-party/EasyAutocomplete/dist/jquery.easy-autocomplete.js
+//= require machine/machine.js
 //= require machine/machine_view.js
+//= require machine/graph_view.js
 
 var MachineEditController = (function(){
 
   var selectors = {
-    machineForm:'form[name=machineForm]'
+    machineForm:'form[name=machineForm]',
+    machineShowURL:'#machineShowURL',
+    machineUuid:'#machineUuid'
+  },
+  machine = null,
+
+  loadMachine = function(){
+    $.ajax({
+      url:$(selectors.machineShowURL).val()+".json",
+      headers:{
+        Accept: "application/json"
+      },
+      type:'GET',
+      data:{id:$(selectors.machineUuid).val()},
+      success:function(result){
+        if(result.transitions.length > 0){
+          machine.addInitialState(result.transitions[0].stateFrom.name);
+
+          result.transitions.forEach(function(transition){
+            transition.actions.forEach(function(action){
+              machine.addTransition({stateFrom:transition.stateFrom.name,
+                                     stateTo:transition.stateTo.name,
+                                     action:action.name});
+            });
+          });
+        }
+        
+        GraphView.renderGraph(machine.getGraph());
+      },
+      error: function(data){
+        console.log(data);
+      }
+    });
+
   },
 
-  machine = null,
- 
   initValidations = function(){
     $(selectors.machineForm).validate({
       rules:{
@@ -33,7 +66,10 @@ var MachineEditController = (function(){
   },
 
   start = function(){
+    machine = Machine.create();
+    GraphView.initView();
     initValidations();
+    loadMachine();
   };
 
   return{
