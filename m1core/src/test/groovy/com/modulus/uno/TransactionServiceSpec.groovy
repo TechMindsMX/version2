f2715d493633c1008b51381b3a9c49a0d51c36ac
cggg88jorge <jorge@makingdevs.com>
2017-03-24 12:55:42 -0600

    #214 creando prueba para la busqueda de transacciones de una compañia por periodo de tiempo

--- m1core/src/test/groovy/com/modulus/uno/TransactionServiceSpec.groovy
+++ m1core/src/test/groovy/com/modulus/uno/TransactionServiceSpec.groovy
@@ -21,4 +21,22 @@ import java.lang.Void as Should
       assert transaction.id
   }
 
+  Should "get the transactions in period for account"(){
+    given:
+      (123123..123124).each{
+        new Transaction([keyTransaction:it,trackingKey:"123123",amount:12.12,
+                        paymentConcept:"Prestamo",keyAccount:"646180132408900006",referenceNumber:"201703100010",
+                        transactionType:TransactionType.WITHDRAW,transactionStatus:TransactionStatus.AUTHORIZED]).save(flush:true)
+      }
+      (123127..123128).each{
+        new Transaction([keyTransaction:it,trackingKey:"123123",amount:12.12,
+                        paymentConcept:"Prestamo",keyAccount:"646180132408900007",referenceNumber:"201703100010",
+                        transactionType:TransactionType.WITHDRAW,transactionStatus:TransactionStatus.AUTHORIZED]).save(flush:true)
+      }
+    when:
+      def transactions = service.getTransactionsAccountForPeriod("646180132408900006",(new Date()-10),(new Date()+5))
+    then:
+      transactions.size() == 2
+  }
+
 }
