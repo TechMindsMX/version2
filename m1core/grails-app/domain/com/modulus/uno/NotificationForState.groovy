4018d0be5f3719975bf5b18509f04b15d12c91a2
carlogilmar <carlogilmar12@gmail.com>
2017-03-27 10:16:22 -0600

    #212 Quitando campo de classOrder

--- m1core/grails-app/domain/com/modulus/uno/NotificationForState.groovy
+++ m1core/grails-app/domain/com/modulus/uno/NotificationForState.groovy
@@ -4,12 +4,10 @@ class NotificationForState {
 
   Long groupNotification
   Long stateMachine
-  String orderClass
 
   static constraints = {
     stateMachine blank:false
     groupNotification blank: false
-    orderClass blank:false
   }
 
 }
