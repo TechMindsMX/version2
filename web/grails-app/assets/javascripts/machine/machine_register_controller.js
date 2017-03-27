67f8fe6d3f94e09c209da4e1141390e2f10c02d9
egjimenezg <egjimenezg@gmail.com>
2017-03-27 15:26:27 -0600

    #225 agregando controller para validar el flujo del registro

---
+++ web/grails-app/assets/javascripts/machine/machine_register_controller.js
@@ -0,0 +1,40 @@
+//= require third-party/jquery-validation/dist/jquery.validate.js
+
+var MachineRegisterController = (function(){
+
+  var selectors = {
+    entityInput:'input[name=entity]',
+    registerForm:'form[name=registerForm]'
+  },
+
+  initValidations = function(){
+    $(selectors.registerForm).validate({
+      rules:{
+        entity:{
+          required:true
+        }
+      },
+
+      errorPlacement: function(error,element){
+        $(element).parent('div').addClass('has-error');
+      },
+
+      success: function(label,element){
+        $(element).parent('div').removeClass('has-error');
+      }
+    });
+  },
+
+  start = function(){
+    initValidations();
+  };
+
+  return {
+    start:start
+  };
+
+}());
+
+jQuery(function($){
+  MachineRegisterController.start();
+});
