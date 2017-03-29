3c04e92f8561baad72da38030f7da19ddc789b5d
egjimenezg <egjimenezg@gmail.com>
2017-03-29 17:55:36 -0600

    #242 agregando implementación para asignar una combinación

---
+++ m1core/grails-app/services/com/modulus/uno/CombinationLinkService.groovy
@@ -0,0 +1,21 @@
+package com.modulus.uno
+
+import grails.transaction.Transactional
+
+@Transactional
+class CombinationLinkService {
+
+  CombinationLink createCombinationLinkForThisInstance(def instance,Combination combination){
+    if(Linker.class.isAssignableFrom(instance.class)){
+      throw new RuntimeException("Linker is not assignable from ${instance.class.simpleName}")
+    }
+
+    CombinationLink combinationLink = CombinationLink.findByTypeAndInstanceRef(instance.class.simpleName,instance.id) ?: new CombinationLink(type:instance.class.simpleName,
+                                                                                                                                             instanceRef:instance.id)
+    combinationLink.combination = combination
+    combinationLink.save()
+    combinationLink
+  }
+ 
+
+}
