6037d74fda4758fde4651dab8b784265f280ff65
egjimenezg <egjimenezg@gmail.com>
2017-03-30 11:01:07 -0600

    #242 agregando prueba para verificar que se crea correctamente una combinación

---
+++ web/src/test/groovy/com/modulus/uno/CombinationServiceSpec.groovy
@@ -0,0 +1,26 @@
+package com.modulus.uno
+
+import grails.test.mixin.TestFor
+import grails.test.mixin.Mock
+import spock.lang.Specification
+import java.lang.Void as Should
+import com.modulus.uno.Machine
+
+@TestFor(CombinationService)
+@Mock([Combination])
+class CombinationServiceSpec extends Specification {
+
+  Should "create the combination for an instance with a another class"(){
+    given:"the instance"
+      Machine instance = new Machine().save(validate:false)
+    and:"the class which it is related to"
+      String className = "PurchaseOrder"
+    when:
+      Combination combination = service.createCombinationOfInstanceWithClass(instance,className)
+    then:
+      combination.classLeft == "PurchaseOrder"
+      combination.classRight == instance.class.simpleName
+      combination.rightInstanceId == instance.id
+  }
+
+}
