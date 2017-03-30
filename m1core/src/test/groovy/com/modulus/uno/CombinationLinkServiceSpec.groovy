62035e4c014f2a326dfcdb6d47588b36d7cd8f9f
egjimenezg <egjimenezg@gmail.com>
2017-03-30 09:24:20 -0600

    #242 actualizando la prueba para verificar que se arroje la excepción

--- m1core/src/test/groovy/com/modulus/uno/CombinationLinkServiceSpec.groovy
+++ m1core/src/test/groovy/com/modulus/uno/CombinationLinkServiceSpec.groovy
@@ -3,11 +3,12 @@ package com.modulus.uno
 import grails.test.mixin.TestFor
 import grails.test.mixin.Mock
 import spock.lang.Specification
+import spock.lang.FailsWith
 import java.lang.Void as Should
 import com.modulus.uno.machine.Machine
 
 @TestFor(CombinationLinkService)
-@Mock([Machine,Company,Combination])
+@Mock([Machine,Company,Combination,CombinationLink])
 class CombinationLinkServiceSpec extends Specification {
 
   Should "create a combination for an instance"(){
@@ -23,6 +24,27 @@ class CombinationLinkServiceSpec extends Specification {
     then:
       combinationLink.type == instance.class.simpleName
       combinationLink.instanceRef == instance.id
+  }
+  
+  @FailsWith((RuntimeException)
+  Should "fail while trying to create a combination for an instance without implements"(){
+    given:"the instance"
+      Bank instance = new Bank()
+      instance.save()
+    and:"the combination"
+      Combination combination = createCombination()
+    when:
+      CombinationLink combinationLink = service.createCombinationLinkForThisInstance(instance,combination)
+    then:
+      !combinationLink.id
+  }
+
+  private Combination createCombination(){
+    Machine machine = new Machine().save(validate:false)
+    Combination combination = new Combination(classLeft:"PurchaseOrder",
+                                              classRight:machine.class.simpleName,
+                                              rightInstanceId:machine.id)
+    combination
   }
 
 }
