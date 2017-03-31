95abd592606d99dfaddc503f68a634538d7813ea
egjimenezg <egjimenezg@gmail.com>
2017-03-31 16:59:14 -0600

    #242 validando que no se repita un link con una combinación

--- m1core/grails-app/services/com/modulus/uno/CombinationLinkService.groovy
+++ m1core/grails-app/services/com/modulus/uno/CombinationLinkService.groovy
@@ -5,15 +5,25 @@ import grails.transaction.Transactional
 @Transactional
 class CombinationLinkService {
 
-  CombinationLink createCombinationLinkForInstance(def instance,Combination combination){
+  CombinationLink createCombinationLinkForInstance(def instance,Combination currentCombination){
     if(!Linker.class.isAssignableFrom(instance.class)){
       throw new RuntimeException("Linker is not assignable from ${instance.class.simpleName}")
     }
 
-    CombinationLink combinationLink = CombinationLink.findByTypeAndInstanceRef(instance.class.simpleName,instance.id) ?: new CombinationLink(type:instance.class.simpleName,
-                                                                                                                                             instanceRef:instance.id)
+    def criteria = CombinationLink.createCriteria()
 
-    combinationLink.addToCombinations(combination)
+    CombinationLink combinationLink = criteria.get{
+                                        eq("type",instance.class.simpleName)
+                                        eq("instanceRef",instance.id)
+
+                                        combination{
+                                          eq("id",currentCombination.id)
+                                        }
+
+                                      } ?: new CombinationLink(type:instance.class.simpleName,
+                                                               instanceRef:instance.id)
+
+    combinationLink.combination = currentCombination
     combinationLink.save()
     combinationLink
   }
