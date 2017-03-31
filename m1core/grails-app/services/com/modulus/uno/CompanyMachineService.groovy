9b9d7f591c11e9a3479adbd3cb18aee492517d55
egjimenezg <egjimenezg@gmail.com>
2017-03-31 12:20:43 -0600

    #242 agregando implementación de método para obtener las máquinas de una instancia y una organización

---
+++ m1core/grails-app/services/com/modulus/uno/CompanyMachineService.groovy
@@ -0,0 +1,20 @@
+package com.modulus.uno
+
+import grails.transaction.Transactional
+import com.modulus.uno.machine.Machine
+
+
+@Transactional
+class CompanyMachineService {
+
+  CombinationLinkService combinationLinkService
+
+  ArrayList<Machine> getCompanyMachinesForEntity(Long companyId,String entity){
+    Company company = Company.get(companyId)
+    ArrayList<CombinationLink> combinationLinks = combinationLinkService.findCombinationLinksForInstance(company)
+    combinationLinks = combinationLinks.findAll{ combinationLink -> combinationLink.combination.classLeft == entity && combinationLink.combination.classRight == Machine.class.simpleName }
+    ArrayList<Long> machineIds =  combinationLinks*.combination*.rightInstanceId
+    Machine.getAll(machineIds)
+  }
+
+}
