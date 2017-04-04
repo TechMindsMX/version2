b8ed87f5b58fd78a9edeeff48f202ac9d9fee92c
cggg88jorge <jorge@makingdevs.com>
2017-04-04 10:06:09 -0500

    #237 Cambiando donde se ejecuta el comando de reinicio de nginx ademas de desplegarlo en log

--- web/grails-app/controllers/com/modulus/uno/CorporateController.groovy
+++ web/grails-app/controllers/com/modulus/uno/CorporateController.groovy
@@ -28,6 +28,7 @@ class CorporateController {
     }
     corporateService.saveNewCorporate(corporate)
     corporateService.createAVirtualHostNginx(corporate)
+    log.info "sudo service nginx reload".execute().text
     request.withFormat {
       form multipartForm {
         flash.message = message(code:'default.created.message', args:[message(code: 'corporate.label',
