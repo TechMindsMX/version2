27de369374b545a0a7b8915ecd791648e2b3e115
temocpalma <temopalmag@gmail.com>
2017-04-18 17:17:35 -0500

    #250 Ajustando domain de factura de comisiones agregando el campo del folio sat

--- m1core/grails-app/domain/com/modulus/uno/CommissionsInvoice.groovy
+++ m1core/grails-app/domain/com/modulus/uno/CommissionsInvoice.groovy
@@ -4,10 +4,14 @@ class CommissionsInvoice {
 
   Company receiver
   CommissionsInvoiceStatus status = CommissionsInvoiceStatus.CREATED
+  String folioSat
 
   static hasMany = [commissions:CommissionTransaction]
 
   Date dateCreated
   Date lastUpdated
 
+  static constraints = {
+    folioSat nullable:true
+  }
 }
