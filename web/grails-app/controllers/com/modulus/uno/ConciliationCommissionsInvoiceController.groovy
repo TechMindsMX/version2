1e768f8cd774799c02860800a33a9dddf665279b
temocpalma <temopalmag@gmail.com>
2017-04-25 16:10:00 -0500

    #257 Agregando los actions para guardar una conciliación de factura de comisiones y para borrarla

--- web/grails-app/controllers/com/modulus/uno/ConciliationCommissionsInvoiceController.groovy
+++ web/grails-app/controllers/com/modulus/uno/ConciliationCommissionsInvoiceController.groovy
@@ -11,11 +11,14 @@ class ConciliationCommissionsInvoiceController {
 
   def chooseInvoiceToConciliate(PaymentM1Emitter payment) {
     log.info "Payment to conciliate: ${payment.dump()}"
+    Corporate corporate = Corporate.get(params.corporateId)
+    Company company = Company.get(params.companyId)
+
     List<ConciliationCommissionsInvoice> conciliations = conciliationCommissionsInvoiceService.getConciliationsToApplyForPayment(payment)
     BigDecimal toApply = payment.amount - (conciliations ? conciliations*.amount.sum() : new BigDecimal(0))
     List<CommissionsInvoice> invoices = getInvoicesToListForPayment()
 
-    [payment:payment, invoices:invoices, toApply:toApply, conciliations:conciliations]
+    [payment:payment, invoices:invoices, toApply:toApply, conciliations:conciliations, corporate:corporate, company:company]
   }
 
   private List<CommissionsInvoice> getInvoicesToListForPayment() {
@@ -27,6 +30,36 @@ class ConciliationCommissionsInvoiceController {
       }
     }
     invoicesFiltered
+  }
+
+  @Transactional
+  def addInvoiceToConciliate(ConciliationCommissionsInvoiceCommand command) {
+    log.info "Adding conciliation to apply: ${command.dump()}"
+
+    if (command.hasErrors()){
+      transactionStatus.setRollbackOnly()
+      redirect action:"chooseInvoiceToConciliate", id:command.paymentId, params:[corporateId:params.corporateId, companyId:params.companyId]
+      return
+    }
+
+    ConciliationCommissionsInvoice conciliation = command.createConciliation()
+    try {
+      conciliationCommissionsInvoiceService.saveConciliation(conciliation)
+    } catch (BusinessException ex) {
+      flash.message = ex.message
+    }
+
+    redirect action:"chooseInvoiceToConciliate", id:command.paymentId, params:[corporateId:params.corporateId, companyId:params.companyId]
+  }
+
+  @Transactional
+  def deleteConciliation(ConciliationCommissionsInvoice conciliation) {
+    log.info "Deleting conciliation to apply: ${conciliation.dump()}"
+    PaymentM1Emitter payment = conciliation.payment
+
+    conciliationCommissionsInvoiceService.deleteConciliation(conciliation)
+
+    redirect action:"chooseInvoiceToConciliate", id:payment.id, params:[corporateId:params.corporateId, companyId:params.companyId]
   }
 
 }
