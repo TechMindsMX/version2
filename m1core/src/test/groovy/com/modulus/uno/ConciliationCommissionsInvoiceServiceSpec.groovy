c6dd1d14b66063622bca5ce1adff60f9c2708428
temocpalma <temopalmag@gmail.com>
2017-04-25 16:09:10 -0500

    #257 Creando tests del método para guardar una conciliación de factura de comisiones

--- m1core/src/test/groovy/com/modulus/uno/ConciliationCommissionsInvoiceServiceSpec.groovy
+++ m1core/src/test/groovy/com/modulus/uno/ConciliationCommissionsInvoiceServiceSpec.groovy
@@ -30,4 +30,36 @@ class ConciliationCommissionsInvoiceServiceSpec extends Specification {
       result.size() == 2
   }
 
+  void "Should save a conciliation for payment m1 emitter with commissions invoice"() {
+    given:"The commissions invoice"
+      CommissionsInvoice invoice = new CommissionsInvoice(status:CommissionsInvoiceStatus.STAMPED, payments:[]).save(validate:false)
+      CommissionTransaction commission = new CommissionTransaction(type:CommissionType.FIJA, amount:1000, invoice:invoice).save(validate:false)
+      invoice.addToCommissions(commission)
+      invoice.save(validate:false)
+    and:"The payment m1 emitter"
+      PaymentM1Emitter payment = new PaymentM1Emitter(amount:2000).save(validate:false)
+    and:"The conciliation commissions invoice"
+      ConciliationCommissionsInvoice conciliation = new ConciliationCommissionsInvoice(payment:payment, amount:500, invoice:invoice)
+    when:
+      def result = service.saveConciliation(conciliation)
+    then:
+      result
+  }
+
+  void "Should thrown a business exception when save a conciliation for payment m1 emitter with commissions invoice when amount is greatest than amount to pay"() {
+    given:"The commissions invoice"
+      CommissionsInvoice invoice = new CommissionsInvoice(status:CommissionsInvoiceStatus.STAMPED, payments:[]).save(validate:false)
+      CommissionTransaction commission = new CommissionTransaction(type:CommissionType.FIJA, amount:1000, invoice:invoice).save(validate:false)
+      invoice.addToCommissions(commission)
+      invoice.save(validate:false)
+    and:"The payment m1 emitter"
+      PaymentM1Emitter payment = new PaymentM1Emitter(amount:2000).save(validate:false)
+    and:"The conciliation commissions invoice"
+      ConciliationCommissionsInvoice conciliation = new ConciliationCommissionsInvoice(payment:payment, amount:1500, invoice:invoice)
+    when:
+      service.saveConciliation(conciliation)
+    then:
+      thrown BusinessException
+  }
+
 }
