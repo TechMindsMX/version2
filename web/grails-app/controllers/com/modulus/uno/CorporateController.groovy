5410fb2e1b9c7ceebd66b2a20d39f55f96143ad6
temocpalma <temopalmag@gmail.com>
2017-06-07 13:25:56 -0500

    #289 Eliminando referencia a método de servicio ya no usado

--- web/grails-app/controllers/com/modulus/uno/CorporateController.groovy
+++ web/grails-app/controllers/com/modulus/uno/CorporateController.groovy
@@ -12,7 +12,6 @@ class CorporateController {
   def managerApplicationService
   def recoveryService
   CommissionTransactionService commissionTransactionService
-  CommissionsInvoiceService commissionsInvoiceService
   CollaboratorService collaboratorService
 
   def create(){
@@ -183,7 +182,7 @@ class CorporateController {
   private List getTotalInvoicedCommissionsForCorporate(Corporate corporate) {
     List totalCommissions = []
     corporate.companies.each {
-      totalCommissions << [company:it, total:commissionsInvoiceService.getTotalInvoicedCommissionsForCompany(it) ?: 0]
+      totalCommissions << [company:it, total:0]
     }
     totalCommissions
   }
