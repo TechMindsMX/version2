72b36cd7261ba06b74d996b290d541f6152d976b
temocpalma <temopalmag@gmail.com>
2017-06-16 11:07:10 -0500

    #302 Agregando los beans nuevos del estado de cuenta al proyecto web

---
+++ web/src/main/groovy/com/modulus/uno/AccountStatementTransaction.groovy
@@ -0,0 +1,13 @@
+package com.modulus.uno
+
+class AccountStatementTransaction {
+
+  BankAccount account
+  Date date
+  String concept
+  String transactionId
+  BigDecimal amount
+  TransactionType type
+  BigDecimal balance
+
+}
