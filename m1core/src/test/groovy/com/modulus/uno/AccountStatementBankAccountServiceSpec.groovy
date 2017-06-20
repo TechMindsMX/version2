11c3d334c34df8f917495558f0104746695dcad7
temocpalma <temopalmag@gmail.com>
2017-06-20 12:09:39 -0500

    #299 Creando test del método para guardar un estado de cuenta bancario, validando excepción cuando ya existe para el mes seleccionado

--- m1core/src/test/groovy/com/modulus/uno/AccountStatementBankAccountServiceSpec.groovy
+++ m1core/src/test/groovy/com/modulus/uno/AccountStatementBankAccountServiceSpec.groovy
@@ -28,4 +28,20 @@ class AccountStatementBankAccountServiceSpec extends Specification {
     then:
       1 * s3AssetService.createTempFilesOfMultipartsFiles(_,_)
   }
+
+  void "Should throw business exception when the account statement for bank account is already exists"() {
+    given:"The account statement"
+      BankAccount bankAccount = new BankAccount().save(validate:false)
+      Date month = new Date().parse("dd-MM-yyyy","01-06-2017")
+      AccountStatementBankAccount accountStatementDoc = new AccountStatementBankAccount(month:month, bankAccount:bankAccount)
+    and:"The document"
+      def document = new MockMultipartFile("file.txt", "", "plain/text", [] as byte[])
+    and:
+      AccountStatementBankAccount.metaClass.static.findByMonth = { new AccountStatementBankAccount() }
+    when:
+      def result = service.saveAccountStatementForBankAccount(accountStatementDoc, document)
+    then:
+      thrown BusinessException
+  }
+
 }
