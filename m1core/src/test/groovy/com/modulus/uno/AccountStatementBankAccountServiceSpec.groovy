62c75924cbc886ad607b7c5073d1c76b782e4de2
temocpalma <temopalmag@gmail.com>
2017-06-20 08:53:46 -0500

    #299 Creando test del método que guarda y carga el documento del edo de cuenta de una cuenta bancaria

---
+++ m1core/src/test/groovy/com/modulus/uno/AccountStatementBankAccountServiceSpec.groovy
@@ -0,0 +1,31 @@
+package com.modulus.uno
+
+import grails.test.mixin.TestFor
+import grails.test.mixin.Mock
+import spock.lang.Specification
+import java.lang.Void as Should
+import org.springframework.mock.web.MockMultipartFile
+
+@TestFor(AccountStatementBankAccountService)
+@Mock([BankAccount, AccountStatementBankAccount, S3Asset])
+class AccountStatementBankAccountServiceSpec extends Specification {
+
+  S3AssetService s3AssetService = Mock(S3AssetService)
+
+  def setup() {
+    service.s3AssetService = s3AssetService
+  }
+
+  void "Should save an account statement for bank account"() {
+    given:"The account statement"
+      BankAccount bankAccount = new BankAccount().save(validate:false)
+      Date month = new Date().parse("dd-MM-yyyy","01-06-2017")
+      AccountStatementBankAccount accountStatementDoc = new AccountStatementBankAccount(month:month, bankAccount:bankAccount)
+    and:"The document"
+      def document = new MockMultipartFile("file.txt", "", "plain/text", [] as byte[])
+    when:
+      def result = service.saveAccountStatementForBankAccount(accountStatementDoc, document)
+    then:
+      1 * s3AssetService.createTempFilesOfMultipartsFiles(_,_)
+  }
+}
