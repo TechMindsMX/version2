70202a9f390dc97e5033aa2ed0ed978587b759a1
temocpalma <temopalmag@gmail.com>
2017-06-20 11:18:08 -0500

    #299 Quitando anotación transactional

--- m1core/grails-app/services/com/modulus/uno/AccountStatementBankAccountService.groovy
+++ m1core/grails-app/services/com/modulus/uno/AccountStatementBankAccountService.groovy
@@ -1,12 +1,9 @@
 package com.modulus.uno
 
-import grails.transaction.Transactional
-
 class AccountStatementBankAccountService {
 
   S3AssetService s3AssetService
 
-  @Transactional
   AccountStatementBankAccount saveAccountStatementForBankAccount(AccountStatementBankAccount accountStatement, def document) {
     def asset = s3AssetService.createTempFilesOfMultipartsFiles(document, "accountStatementBankAccount")
     accountStatement.document = asset
@@ -15,7 +12,7 @@ class AccountStatementBankAccountService {
   }
 
   List<AccountStatementBankAccount> listAccountStatementForBankAccount(BankAccount bankAccount) {
-    AccountStatementBankAccount.findAllByBankAccount(bankAccount)
+    AccountStatementBankAccount.findAllByBankAccount(bankAccount,[sort:"month", order:"desc"])
   }
 
 }
