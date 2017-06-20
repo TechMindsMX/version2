edba833f9a1a4a5910c9ff9522995891875cd5a4
temocpalma <temopalmag@gmail.com>
2017-06-20 12:42:47 -0500

    #299 Creando test del método que guarda el estado de cuenta bancario para el caso de que el mes/año sea posterior al actual

--- m1core/src/test/groovy/com/modulus/uno/AccountStatementBankAccountServiceSpec.groovy
+++ m1core/src/test/groovy/com/modulus/uno/AccountStatementBankAccountServiceSpec.groovy
@@ -29,7 +29,7 @@ class AccountStatementBankAccountServiceSpec extends Specification {
       1 * s3AssetService.createTempFilesOfMultipartsFiles(_,_)
   }
 
-  void "Should throw business exception when the account statement for bank account is already exists"() {
+  void "Should throw business exception when the account statement for bank account to selected month/year is already exists"() {
     given:"The account statement"
       BankAccount bankAccount = new BankAccount().save(validate:false)
       Date month = new Date().parse("dd-MM-yyyy","01-06-2017")
@@ -38,6 +38,19 @@ class AccountStatementBankAccountServiceSpec extends Specification {
       def document = new MockMultipartFile("file.txt", "", "plain/text", [] as byte[])
     and:
       AccountStatementBankAccount.metaClass.static.findByMonth = { new AccountStatementBankAccount() }
+    when:
+      def result = service.saveAccountStatementForBankAccount(accountStatementDoc, document)
+    then:
+      thrown BusinessException
+  }
+
+  void "Should throw business exception when the account statement for bank account with selected month/year is after current month/year"() {
+    given:"The account statement"
+      BankAccount bankAccount = new BankAccount().save(validate:false)
+      Date month = Date.parse("MM-yyyy", new Date().format("MM-yyyy"))+35
+      AccountStatementBankAccount accountStatementDoc = new AccountStatementBankAccount(month:month, bankAccount:bankAccount)
+    and:"The document"
+      def document = new MockMultipartFile("file.txt", "", "plain/text", [] as byte[])
     when:
       def result = service.saveAccountStatementForBankAccount(accountStatementDoc, document)
     then:
