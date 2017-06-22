6dbccb08345bc54c81110b07897236cc4b14c3e8
temocpalma <temopalmag@gmail.com>
2017-06-22 09:32:48 -0500

    #143 Creando método que agrega parámetros extras en el mapa de parámetros a enviar al emailer para el caso de un recibo de honorarios ejecutado

--- m1core/grails-app/services/com/modulus/uno/NotifyService.groovy
+++ m1core/grails-app/services/com/modulus/uno/NotifyService.groovy
@@ -1,6 +1,7 @@
 package com.modulus.uno
 
 import grails.transaction.Transactional
+import java.math.RoundingMode
 
 @Transactional
 class NotifyService {
@@ -236,7 +237,7 @@ class NotifyService {
       orderStatus= "AUTORIZADA"
       break
       case FeesReceiptStatus.EJECUTADA:
-      paramsFields=['id']
+      paramsFields=['id', 'collaboratorName']
       orderStatus= "EJECUTADA"
       break
       case FeesReceiptStatus.CANCELADA:
@@ -252,6 +253,21 @@ class NotifyService {
     paramsMap.status=orderStatus
     paramsMap.company=company.toString()
     paramsMap.url=corporateService.findCorporateByCompanyId(feesReceipt.company.id)
+    if (status == FeesReceiptStatus.EJECUTADA) {
+      paramsMap = defineExtraParamsForPayedFeesReceiptTemplate(feesReceipt, paramsMap)
+    }
+    paramsMap
+  }
+
+  def defineExtraParamsForPayedFeesReceiptTemplate(FeesReceipt feesReceipt, def paramsMap) {
+    paramsMap.amount = feesReceipt.netAmount.setScale(2, RoundingMode.HALF_UP).toString()
+    paramsMap.paymentConcept = feesReceipt.transaction.paymentConcept
+    paramsMap.trackingKey = feesReceipt.transaction.trackingKey
+    paramsMap.referenceNumber = feesReceipt.transaction.referenceNumber
+    paramsMap.dateCreated = feesReceipt.transaction.dateCreated.format("dd-MM-yyyy hh:mm:ss")
+    paramsMap.destinyBank = feesReceipt.bankAccount.banco.name
+    paramsMap.destinyBankAccount = feesReceipt.bankAccount.clabe
+    paramsMap.aliasStp = feesReceipt.company.accounts.first().aliasStp
     paramsMap
   }
 
