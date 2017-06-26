7800b5307bd28440895c5f26f3f136955768632f
temocpalma <temopalmag@gmail.com>
2017-06-26 11:38:02 -0500

    #310 Creando métodos para crear y guardar la transacción de reverso, y para cambiar estatus de la orden asociada a la transacción

--- m1core/grails-app/services/com/modulus/uno/StatusOrderStpConsumerService.groovy
+++ m1core/grails-app/services/com/modulus/uno/StatusOrderStpConsumerService.groovy
@@ -1,43 +1,52 @@
 package com.modulus.uno
 
 import grails.transaction.Transactional
+import org.springframework.context.i18n.LocaleContextHolder as LCH
 import reactor.spring.context.annotation.*
 
 @Consumer
 class StatusOrderStpConsumerService {
 
+  def messageSource
   TransactionService transactionService
+  PurchaseOrderService purchaseOrderService
+  FeesReceiptService feesReceiptService
+  CashOutOrderService cashOutOrderService
 
   @Selector('reverseTransaction')
   @Transactional
   void reverseTransaction(StatusOrderStp statusOrderStp) {
     log.info "Init reverse transaction for ${statusOrderStp.dump()}"
-    //obtener la transaccion asociada
     Transaction transaction = Transaction.findByKeyTransaction(statusOrderStp.keyTransaction)
     if (transaction) {
-      def properties = [:] << transaction.properties
-      //genera la transacción inversa
-      Transaction reverseTransaction = new Transaction(
-        keyTransaction:"${transaction.keyTransaction}-DEV",
-        trackingKey:"${transaction.trackingKey}-DEV",
-        amount:transaction.amount,
-        paymentConcept:"DEVOLUCIÓN",
-        keyAccount:transaction.keyAccount,
-        referenceNumber:"${transaction.referenceNumber}-DEV",
-        transactionType:TransactionType.DEPOSIT,
-        transactionStatus:transaction.transactionStatus
-      )
-      transactionService.saveTransaction(reverseTransaction)
-      log.info "Reverse transaction saved: ${reverseTransaction.dump()}"
-      //buscar la orden asociada a la transacción
-      //si es orden de venta, poner en estatus PAGO_DEVUELTO
-      //si es recibo de honorarios, poner en estatus PAGO_DEVUELTO
-      //si es un retiro, poner en estatus PAGO_DEVUELTO
+      Transaction reverseTransaction = createAndSaveReverseTransactionFromOriginalTransaction(transaction, statusOrderStp.causeRefund)
+      changeStatusOrderLinkedToTransactionToRefundPaymentStatus(transaction)
     } else {
       log.warn "Not Found transaction to reverse"
     }
 
     log.info "End reverse transaction"
+  }
+
+  private Transaction createAndSaveReverseTransactionFromOriginalTransaction(Transaction transaction, CauseRefundStp causeRefund) {
+    Transaction reverseTransaction = new Transaction(
+      keyTransaction:"${transaction.keyTransaction}-DEV",
+      trackingKey:"${transaction.trackingKey}-DEV",
+      amount:transaction.amount,
+      paymentConcept:"DEVOLUCIÓN, Causa: "+messageSource.getMessage("stp.cause.refund.${causeRefund}", null, LCH.getLocale()),
+      keyAccount:transaction.keyAccount,
+      referenceNumber:"${transaction.referenceNumber}-DEV",
+      transactionType:TransactionType.DEPOSIT,
+      transactionStatus:transaction.transactionStatus
+    )
+    transactionService.saveTransaction(reverseTransaction)
+    log.info "Reverse transaction saved: ${reverseTransaction.dump()}"
+  }
+
+  private void changeStatusOrderLinkedToTransactionToRefundPaymentStatus(Transaction transaction) {
+    purchaseOrderService.reversePaymentPurchaseForTransaction(transaction)
+    feesReceiptService.reverseFeesReceiptForTransaction(transaction)
+    cashOutOrderService.reverseCashOutForTransaction(transaction)
   }
 
 }
