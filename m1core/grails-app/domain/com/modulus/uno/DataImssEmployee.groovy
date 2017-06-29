f152839a132d661fade54cb28463129f05afb0b6
temocpalma <temopalmag@gmail.com>
2017-06-29 17:28:46 -0500

    #312 Creando dominio de datos Imss de empleado

---
+++ m1core/grails-app/domain/com/modulus/uno/DataImssEmployee.groovy
@@ -0,0 +1,15 @@
+package com.modulus.uno
+
+class DataImssEmployee {
+
+  EmployeeLink employee
+  Date registrationDate
+  BigDecimal imssSalary
+  BigDecimal assimilableSalary
+  BigDecimal holidayBonusRate
+  BigDecimal annualBonusDays
+
+  //getAntiquity
+  //getIntegratedDailySalary
+
+}
