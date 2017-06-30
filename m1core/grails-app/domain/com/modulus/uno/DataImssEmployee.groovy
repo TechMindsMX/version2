34466219e47835a1afb5dbe60c78c7b037f1f8b6
temocpalma <temopalmag@gmail.com>
2017-06-30 12:06:01 -0500

    #312 Ajustando dominio de datos imss de empleado

--- m1core/grails-app/domain/com/modulus/uno/DataImssEmployee.groovy
+++ m1core/grails-app/domain/com/modulus/uno/DataImssEmployee.groovy
@@ -4,12 +4,32 @@ class DataImssEmployee {
 
   EmployeeLink employee
   Date registrationDate
-  BigDecimal imssSalary
-  BigDecimal assimilableSalary
+  BigDecimal baseImssMonthlySalary
+  BigDecimal netMonthlySalary
   BigDecimal holidayBonusRate
-  BigDecimal annualBonusDays
+  Integer annualBonusDays
+  PaymentPeriod paymentPeriod = PaymentPeriod.WEEKLY
 
-  //getAntiquity
-  //getIntegratedDailySalary
+  static constraints = {
+    employee nullable:false
+    imssSalary nullable:false, min:0.0
+    assimilableSalary nullable:false, min:0.0
+    holidayBonusRate nullable:false, min:0.0, max:100.0
+    annualBonusDays nullable:false, min:15
+    paymentPeriod nullable:false
+  }
 
+  /*
+  //BigDecimal assimilableMonthlySalary (calculado) //diferencia del neto a pagar en el periodo de pago menos el salario imss calculado del periodo de pago
+  //getIntegratedDailySalary salario base mensual / 30 * factor de integración 1.05452 (definido en los datos imss de la empresa)
+
+  Integer getAntiquityInWeeks() {
+  }
+
+  Integer getAntiquityInDays() {
+  }
+
+  String getAntiquityInYearsMonthsDays() {
+  }
+  */
 }
