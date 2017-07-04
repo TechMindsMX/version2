21ab5d39b4d5f91d9bb481b4e30c750975987628
temocpalma <temopalmag@gmail.com>
2017-07-04 12:28:05 -0500

    #330 Ajustando el action para crear datos imss de empleado

--- web/grails-app/controllers/com/modulus/uno/DataImssEmployeeController.groovy
+++ web/grails-app/controllers/com/modulus/uno/DataImssEmployeeController.groovy
@@ -5,7 +5,7 @@ class DataImssEmployeeController {
   def create() {
     BusinessEntity businessEntity = BusinessEntity.get(params.businessEntityId)
     EmployeeLink employee = EmployeeLink.findByEmployeeRef(businessEntity.rfc)
-    responde new DataImssEmployee(), model:[businessEntity:businessEntity, employee:employee]
+    respond new DataImssEmployee(employee:employee), model:[businessEntity:businessEntity, employee:employee]
   }
 
 }
