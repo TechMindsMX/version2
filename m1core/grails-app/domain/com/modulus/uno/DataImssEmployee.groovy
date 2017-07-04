d8a5ba776b89daf85854d9440234b7e27cdbd6ae
temocpalma <temopalmag@gmail.com>
2017-07-04 16:47:42 -0500

    #330 Agregando restricción de campo único al campo nss de los datos imss de empleado

--- m1core/grails-app/domain/com/modulus/uno/DataImssEmployee.groovy
+++ m1core/grails-app/domain/com/modulus/uno/DataImssEmployee.groovy
@@ -13,7 +13,7 @@ class DataImssEmployee {
 
   static constraints = {
     employee nullable:false
-    nss nullable:false
+    nss nullable:false, unique: true
     registrationDate nullable:false
     baseImssMonthlySalary nullable:false, min:0.0
     netMonthlySalary nullable:false, min:0.0
