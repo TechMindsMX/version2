3c2883eae34eaab841301979a5cdfa66c0bc6308
temocpalma <temopalmag@gmail.com>
2017-07-04 15:41:19 -0500

    #330 Creando action para guardar los datos imss de un empleado

--- web/grails-app/controllers/com/modulus/uno/DataImssEmployeeController.groovy
+++ web/grails-app/controllers/com/modulus/uno/DataImssEmployeeController.groovy
@@ -2,10 +2,27 @@ package com.modulus.uno
 
 class DataImssEmployeeController {
 
+  DataImssEmployeeService dataImssEmployeeService
+
   def create() {
     BusinessEntity businessEntity = BusinessEntity.get(params.businessEntityId)
     EmployeeLink employee = EmployeeLink.findByEmployeeRef(businessEntity.rfc)
-    respond new DataImssEmployee(employee:employee), model:[businessEntity:businessEntity, employee:employee]
+    respond new DataImssEmployee(), model:[businessEntity:businessEntity, employee:employee]
+  }
+
+  def save(DataImssEmployeeCommand command) {
+    log.info "Data Imss to save: ${command.dump()}"
+
+    if (command.hasErrors()) {
+      BusinessEntity businessEntity = BusinessEntity.get(params.businessEntityId)
+      EmployeeLink employee = EmployeeLink.findByEmployeeRef(businessEntity.rfc)
+      render view:"create", model:[dataImssEmployee:command, businessEntity:businessEntity, employee:employee]
+      return
+    }
+
+    dataImssEmployeeService.saveDataImss(command.createDataImssEmployee())
+
+    redirect controller:"businessEntity", action:"show", id:params.businessEntityId
   }
 
 }
