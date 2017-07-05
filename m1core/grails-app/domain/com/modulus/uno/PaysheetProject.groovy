c9c04c2349cbeade84f6d20cc880deb446846847
temocpalma <temopalmag@gmail.com>
2017-07-05 16:03:57 -0500

    #333 Ajustando dominio de proyecto de nómina relacionándolo con una empresa

--- m1core/grails-app/domain/com/modulus/uno/PaysheetProject.groovy
+++ m1core/grails-app/domain/com/modulus/uno/PaysheetProject.groovy
@@ -2,14 +2,14 @@ package com.modulus.uno
 
 class PaysheetProject {
 
-  Company company
   String name
   String description
   BigDecimal integrationFactor
   BigDecimal occupationalRiskRate
 
+  static belongsTo = [company:Company]
+
   static constrains = {
-    company nullable:false
     name nullable:false, blank:false
     description nullable:true, blank:true
     integrationFactor nullable:false, min:0.0
