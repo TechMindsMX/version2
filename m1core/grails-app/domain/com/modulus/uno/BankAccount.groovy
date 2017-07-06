b466e10fe99c5e49d46886cf55238f34fc592544
temocpalma <temopalmag@gmail.com>
2017-07-06 12:07:44 -0500

    #340 Agregando atributo número de tarjeta al dominio de cuenta bancaria

--- m1core/grails-app/domain/com/modulus/uno/BankAccount.groovy
+++ m1core/grails-app/domain/com/modulus/uno/BankAccount.groovy
@@ -7,6 +7,7 @@ class BankAccount {
   String accountNumber
   String branchNumber
   String clabe
+  String cardNumber
   boolean concentradora = false
 
   static belongsTo = [banco:Bank]
@@ -18,6 +19,7 @@ class BankAccount {
       if(!(val[val.size()-1].toInteger() == getControlDigit(val[0..(val.size()-2)])))
         return ['wrongClabe']
     }
+    cardNumber nullable:true, blank:true
   }
 
   private static Integer getControlDigit(def clabe){
