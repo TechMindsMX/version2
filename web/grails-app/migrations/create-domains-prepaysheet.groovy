9e4b4308eaf0ca6470a2331278894ceb0cf59497
temocpalma <temopalmag@gmail.com>
2017-07-21 12:04:54 -0500

    #342 Regenerando changeset que crea las tablas de pre-nómina agregando los campos rfc y curp

--- web/grails-app/migrations/create-domains-prepaysheet.groovy
+++ web/grails-app/migrations/create-domains-prepaysheet.groovy
@@ -1,6 +1,6 @@
 databaseChangeLog = {
 
-    changeSet(author: "tim (generated)", id: "1500485667048-2") {
+    changeSet(author: "tim (generated)", id: "1500656235617-2") {
         createTable(tableName: "pre_paysheet") {
             column(autoIncrement: "true", name: "id", type: "BIGINT") {
                 constraints(primaryKey: "true", primaryKeyName: "pre_paysheetPK")
@@ -40,7 +40,7 @@ databaseChangeLog = {
         }
     }
 
-    changeSet(author: "tim (generated)", id: "1500485667048-3") {
+    changeSet(author: "tim (generated)", id: "1500656235617-3") {
         createTable(tableName: "pre_paysheet_employee") {
             column(autoIncrement: "true", name: "id", type: "BIGINT") {
                 constraints(primaryKey: "true", primaryKeyName: "pre_paysheet_employeePK")
@@ -66,6 +66,10 @@ databaseChangeLog = {
                 constraints(nullable: "false")
             }
 
+            column(name: "curp", type: "VARCHAR(255)") {
+                constraints(nullable: "false")
+            }
+
             column(name: "name_employee", type: "VARCHAR(255)") {
                 constraints(nullable: "false")
             }
@@ -85,18 +89,22 @@ databaseChangeLog = {
             column(name: "pre_pay_sheet_id", type: "BIGINT") {
                 constraints(nullable: "false")
             }
+
+            column(name: "rfc", type: "VARCHAR(255)") {
+                constraints(nullable: "false")
+            }
         }
     }
 
-    changeSet(author: "tim (generated)", id: "1500485667048-5") {
+    changeSet(author: "tim (generated)", id: "1500656235617-5") {
         addForeignKeyConstraint(baseColumnNames: "company_id", baseTableName: "pre_paysheet", constraintName: "FKaged9hw8yh0hvb39rec7pb7p", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "company")
     }
 
-    changeSet(author: "tim (generated)", id: "1500485667048-6") {
+    changeSet(author: "tim (generated)", id: "1500656235617-6") {
         addForeignKeyConstraint(baseColumnNames: "pre_pay_sheet_id", baseTableName: "pre_paysheet_employee", constraintName: "FKja9awgcnkpe0bm75786k3v1nw", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "pre_paysheet")
     }
 
-    changeSet(author: "tim (generated)", id: "1500485667048-7") {
+    changeSet(author: "tim (generated)", id: "1500656235617-7") {
         addForeignKeyConstraint(baseColumnNames: "bank_id", baseTableName: "pre_paysheet_employee", constraintName: "FKsle784g1de00x4vpg20tk4ir3", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "bank")
     }
 
