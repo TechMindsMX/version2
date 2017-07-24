30ad30afed9916d6b005816342e746bc76ff53dd
neodevelop <juan@makingdevs.com>
2017-07-24 18:08:21 -0500

    #358 Moviendo MenuService a core

--- m1core/grails-app/services/com/modulus/uno/menu/MenuService.groovy
+++ m1core/grails-app/services/com/modulus/uno/menu/MenuService.groovy
@@ -1,4 +1,4 @@
-package com.modulus.uno
+package com.modulus.uno.menu
 
 import grails.transaction.Transactional
 
--- m1core/src/test/groovy/com/modulus/uno/menu/MenuServiceSpec.groovy
+++ m1core/src/test/groovy/com/modulus/uno/menu/MenuServiceSpec.groovy
@@ -1,4 +1,4 @@
-package com.modulus.uno
+package com.modulus.uno.menu
 
 import grails.test.mixin.TestFor
 import grails.test.mixin.Mock
