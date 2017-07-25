b79ae99999a09ca595bf4dfda34dbfd65035c9d3
temocpalma <temopalmag@gmail.com>
2017-07-25 13:20:45 -0500

    #342 Creando método necesarios para agregar empleados a una pre-nómina

--- m1core/grails-app/services/com/modulus/uno/PrePaysheetService.groovy
+++ m1core/grails-app/services/com/modulus/uno/PrePaysheetService.groovy
@@ -1,6 +1,7 @@
 package com.modulus.uno
 
 import grails.transaction.Transactional
+import java.math.RoundingMode
 
 class PrePaysheetService {
 
@@ -20,6 +21,27 @@ class PrePaysheetService {
     prePaysheets
   }
 
+  List<BigDecimal> getNetPaymentForEmployees(List<BusinessEntity> beEmployees, PrePaysheet prePaysheet) {
+    List<BigDecimal> netPayments = []
+    int daysPeriod = prePaysheet.paymentPeriod.getDays()
+    List dataImss = getDataImssForEmployees(beEmployees)
+    dataImss.each { di ->
+      BigDecimal netPayment = di ? (di.netMonthlySalary/30*daysPeriod).setScale(2, RoundingMode.HALF_UP) : new BigDecimal(0)
+      netPayments.add(netPayment)
+    }
+    netPayments
+  }
+
+  List<DataImssEmployee> getDataImssForEmployees(List<BusinessEntity> beEmployees) {
+    List<DataImssEmployee> dataImss = []
+    beEmployees.each { be ->
+      EmployeeLink employee = EmployeeLink.findByEmployeeRef(be.rfc)
+      DataImssEmployee dataImssEmployee = DataImssEmployee.findByEmployee(employee)
+      dataImss.add(dataImssEmployee)
+    }
+    dataImss
+  }
+
   List<BusinessEntity> getEmployeesAvailableToAdd(PrePaysheet prePaysheet) {
     List<BusinessEntity> allActiveEmployeesForCompany = businessEntityService.getAllActiveEmployeesForCompany(prePaysheet.company)
     List<BusinessEntity> currentEmployees = obtainBusinessEntitiesFromEmployeesPrePaysheet(prePaysheet)
@@ -35,26 +57,33 @@ class PrePaysheetService {
   }
 
   @Transactional
-  def addEmployeesToPrePaysheet(PrePaysheet prePaysheet, String entities) {
-    List<BusinessEntity> employees = businessEntityService.getBusinessEntitiesFromIds(entities)
+  def addEmployeesToPrePaysheet(PrePaysheet prePaysheet, Map params) {
+    List<BusinessEntity> employees = businessEntityService.getBusinessEntitiesFromIds(params.entities)
     employees.each { employee ->
-      createAndSavePrePaysheetEmployee(employee, prePaysheet)
+      createAndSavePrePaysheetEmployee(employee, prePaysheet, params)
     }
+    log.info "Employees in prePaysheet: ${prePaysheet.employees}"
   }
 
-  PrePaysheet createAndSavePrePaysheetEmployee(BusinessEntity employee, PrePaysheet prePaysheet) {
-    DataImssEmployee dataImssEmployee = businessEntityService.getDataImssEmployee(prePaysheet.company, employee, LeadType.EMPLEADO)
+  PrePaysheet createAndSavePrePaysheetEmployee(BusinessEntity employee, PrePaysheet prePaysheet, Map params) {
+    BankAccount bankAccount = params."bankAccount${employee.id}" ? BankAccount.get(params."bankAccount${employee.id}") : null
     PrePaysheetEmployee prePaysheetEmployee = new PrePaysheetEmployee(
       rfc:employee.rfc,
       curp:employee.curp,
-      numberEmployee:employee.number,
+      numberEmployee:employee.number?:"SN",
       nameEmployee:employee.toString(),
-      netPayment:dataImssEmployee ? dataImssEmployee.netMonthlySalary : new BigDecimal(0),
-      prePaysheet:prePaysheet
+      netPayment:new BigDecimal(params."netPayment${employee.id}"),
+      prePaysheet:prePaysheet,
+      note:params."note${employee.id}",
+      bank:bankAccount?.banco,
+      clabe:bankAccount?.clabe,
+      account:bankAccount?.accountNumber,
+      cardNumber:bankAccount?.cardNumber
     )
+    prePaysheetEmployee.save()
     prePaysheet.addToEmployees(prePaysheetEmployee)
     prePaysheet.save()
-    log.info "PrePaysheetEmployee saved: ${prePaysheetEmployee?.dump()}
+    log.info "PrePaysheetEmployee saved: ${prePaysheetEmployee?.dump()}"
     prePaysheet
   }
 
