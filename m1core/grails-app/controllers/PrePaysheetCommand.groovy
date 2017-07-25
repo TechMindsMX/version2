f2e4c472106c25c59b291c5c0f52dfa27e4ac523
temocpalma <temopalmag@gmail.com>
2017-07-25 09:27:27 -0500

    #342 Agregando fechas inicial y final del período de pago al command de pre-nómina

--- m1core/grails-app/controllers/PrePaysheetCommand.groovy
+++ m1core/grails-app/controllers/PrePaysheetCommand.groovy
@@ -7,18 +7,24 @@ class PrePaysheetCommand implements Validateable {
   String companyId
   String paysheetProject
   String paymentPeriod
+  String initDatePeriod
+  String endDatePeriod
   String accountExecutive
 
   static constraints = {
     paysheetProject nullable:false, blank:false
     paymentPeriod nullable:false, blank:false
     accountExecutive nullable:false, blank:false
+    initDatePeriod nullable:false
+    endDatePeriod nullable:false
   }
 
   PrePaysheet createPrePaysheet() {
     new PrePaysheet(
       paysheetProject:this.paysheetProject,
-      paymentPeriod:this.paymentPeriod,
+      paymentPeriod:PaymentPeriod.find { it.toString() == this.paymentPeriod },
+      initPeriod:Date.parse("dd-MM-yyyy", this.initDatePeriod),
+      endPeriod:Date.parse("dd-MM-yyyy",this.endDatePeriod),
       accountExecutive:this.accountExecutive,
       company:Company.get(this.companyId)
     )
