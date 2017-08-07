15ac0b3950d7b3b4d1ee7ed32e76a920cbe01cbf
temocpalma <temopalmag@gmail.com>
2017-08-07 16:56:47 -0500

    #370 Creando dominio para el desglose del cálculo de las cargas sociales del empleado y de la empresa

---
+++ m1core/grails-app/domain/com/modulus/uno/paysheet/BreakdownPaymentEmployee.groovy
@@ -0,0 +1,34 @@
+package com.modulus.uno.paysheet
+
+class BreakdownPaymentEmployee {
+
+  BigDecimal integratedDailySalary = new BigDecimal(0)
+  BigDecimal baseQuotation = new BigDecimal(0)
+  BigDecimal fixedFee = new BigDecimal(0)
+  BigDecimal diseaseAndMaternityBase = new BigDecimal(0)
+  BigDecimal diseaseAndMaternity = new BigDecimal(0)
+  BigDecimal diseaseAndMaternityEmployer = new BigDecimal(0)
+  BigDecimal pension = new BigDecimal(0)
+  BigDecimal pensionEmployer = new BigDecimal(0)
+  BigDecimal loan = new BigDecimal(0)
+  BigDecimal loanEmployer = new BigDecimal(0)
+  BigDecimal disabilityAndLife = new BigDecimal(0)
+  BigDecimal disabilityAndLifeEmployer = new BigDecimal(0)
+  BigDecimal kindergarten = new BigDecimal(0)
+  BigDecimal occupationalRisk = new BigDecimal(0)
+  BigDecimal retirementSaving = new BigDecimal(0)
+  BigDecimal infonavit = new BigDecimal(0)
+  BigDecimal unemploymentAndEld = new BigDecimal(0)
+  BigDecimal unemploymentAndEldEmployer = new BigDecimal(0)
+
+  static belongsTo = [paysheetEmployee:PaysheetEmployee]
+
+  BigDecimal getSocialQuotaEmployeeTotal() {
+    this.diseaseAndMaternity + this.pension + this.loan + this.disabilityAndLife + this.unemploymentAndEld
+  }
+
+  BigDecimal getSocialQuotaEmployer() {
+    this.fixedFee + this.diseaseAndMaternityEmployer + this.pensionEmployer + this.loanEmployer + this.disabilityAndLifeEmployer + this.kindergarten + this.occupationalRisk + this.retirementSaving + this.unemploymentAndEldEmployer + this.infonavit
+  }
+
+}
