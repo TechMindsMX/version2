a7302cee0b2c2c93809c863f31edf43dae4d40e4
temocpalma <temopalmag@gmail.com>
2017-08-09 16:23:46 -0500

    coding #377 Quitando métodos que se movieron al servicio adecuado

--- m1core/grails-app/services/com/modulus/uno/paysheet/PaysheetService.groovy
+++ m1core/grails-app/services/com/modulus/uno/paysheet/PaysheetService.groovy
@@ -2,21 +2,4 @@ package com.modulus.uno.paysheet
 
 class PaysheetService {
 
-  BreakdownPaymentEmployeeService breakdownPaymentEmployeeService
-
-  PaysheetEmployee createPaysheetEmployeeFromPrePaysheetEmployee(Paysheet paysheet, PrePaysheetEmployee prePaysheetEmployee) {
-    PaysheetEmployee paysheetEmployee = new PaysheetEmployee(
-      prePaysheetEmployee:prePaysheetEmployee,
-      paysheet:paysheet
-    )
-    calculateSalaryImssForEmployee(paysheetEmployee)
-    calculateSalaryAssimilable(paysheetEmployee)
-  }
-
-  def calculateSalaryImssForEmployee(PaysheetEmployee paysheetEmployee) {
-    BreakdownPaymentEmployee breakdownPayment = breakdownPaymentEmployeeService.generateBreakdownPaymentEmployee(paysheetEmployee)
-    paysheetEmployee.socialQuota = breakdownPayment.socialQuotaEmployeeTotal
-  }
-
-
 }
