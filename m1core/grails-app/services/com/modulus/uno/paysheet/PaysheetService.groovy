52807ab32f4590285e6d19491cb2cd7057f35d41
temocpalma <temopalmag@gmail.com>
2017-08-11 11:39:29 -0500

    #378 Creando métodos para generar la nómina de una pre-nómina

--- m1core/grails-app/services/com/modulus/uno/paysheet/PaysheetService.groovy
+++ m1core/grails-app/services/com/modulus/uno/paysheet/PaysheetService.groovy
@@ -1,5 +1,29 @@
 package com.modulus.uno.paysheet
 
+import grails.transaction.Transactional
+
 class PaysheetService {
+
+  PaysheetEmployeeService paysheetEmployeeService
+
+  @Transactional
+  Paysheet createPaysheetFromPrePaysheet(PrePaysheet prePaysheet) {
+    Paysheet paysheet = new Paysheet(
+      prePaysheet:prePaysheet,
+      company:prePaysheet.company
+    )
+    paysheet.save()
+    loadEmployeesToPaysheetFromPrePaysheet(paysheet, prePaysheet)
+    paysheet
+  }
+
+  Paysheet loadEmployeesToPaysheetFromPrePaysheet(Paysheet paysheet, PrePaysheet prePaysheet) {
+    prePaysheet.employees.each { prePaysheetEmployee ->
+      PaysheetEmployee paysheetEmployee = paysheetEmployeeService.createPaysheetEmployeeFromPrePaysheetEmployee(paysheet, prePaysheetEmployee)
+      paysheet.addToEmployees(paysheetEmployee)
+    }
+    paysheet.save()
+    paysheet
+  }
 
 }
