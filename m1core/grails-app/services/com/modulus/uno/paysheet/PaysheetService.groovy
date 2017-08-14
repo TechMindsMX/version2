4938bb0d17f7fdb206664acb32f25fc9fbf97099
temocpalma <temopalmag@gmail.com>
2017-08-14 13:02:32 -0500

    #373 Creando métodos para generar el xls de una nómina

--- m1core/grails-app/services/com/modulus/uno/paysheet/PaysheetService.groovy
+++ m1core/grails-app/services/com/modulus/uno/paysheet/PaysheetService.groovy
@@ -1,6 +1,8 @@
 package com.modulus.uno.paysheet
 
 import grails.transaction.Transactional
+import pl.touk.excel.export.WebXlsxExporter
+import java.text.SimpleDateFormat
 
 class PaysheetService {
 
@@ -34,6 +36,25 @@ class PaysheetService {
     paysheet.save()
     //TODO: enviar notificación a usuario autorizador de nómina
     paysheet
+  }
+
+  def exportPaysheetToXls(Paysheet paysheet) {
+    Map employees = getEmployeesToExport(paysheet)
+    new WebXlsxExporter().with {
+      fillRow(["PROYECTO:", paysheet.prePaysheet.paysheetProject],0)
+      fillRow(["PERIODO DE PAGO:", paysheet.prePaysheet.paymentPeriod, "DEL:", new SimpleDateFormat("dd-MM-yyyy").format(paysheet.prePaysheet.initPeriod), "AL:", new SimpleDateFormat("dd-MM-yyyy").format(paysheet.prePaysheet.endPeriod)],1)
+      fillRow(["RESIDENTE:", paysheet.prePaysheet.accountExecutive,"TOTAL:", paysheet.total], 2)
+      fillRow(employees.headers, 4)
+      add(employees.data, employees.properties, 5)
+    }
+  }
+
+  Map getEmployeesToExport(Paysheet paysheet) {
+    Map employees = [:]
+    employees.headers = ['RFC','CURP','NOMBRE','NO. EMPL.','CÓD. BANCO','BANCO','CLABE', 'CUENTA', 'TARJETA', 'SALARIO IMSS', 'CARGA SOCIAL TRABAJADOR', 'SUBSIDIO', 'ISR', 'TOTAL IMSS', 'ASIMILABLE', 'SUBTOTAL', 'CARGA SOCIAL EMPRESA', 'ISN', 'COSTO NOMINAL', 'COMISION', 'TOTAL NÓMINA', 'IVA', 'TOTAL A FACTURAR']
+    employees.properties = ['prePaysheetEmployee.rfc', 'prePaysheetEmployee.curp', 'prePaysheetEmployee.nameEmployee', 'prePaysheetEmployee.numberEmployee', 'prePaysheetEmployee.bank.bankingCode', 'prePaysheetEmployee.bank.name', 'prePaysheetEmployee.clabe', 'prePaysheetEmployee.account', 'prePaysheetEmployee.cardNumber', 'salaryImss', 'socialQuota', 'subsidySalary', 'incomeTax', 'imssSalaryNet', 'salaryAssimilable', 'totalSalaryEmployee', 'socialQuotaEmployer', 'paysheetTax', 'paysheetCost', 'commission', 'paysheetTotal', 'paysheetIva', 'totalToInvoice']
+    employees.data = paysheet.employees.sort {it.prePaysheetEmployee.nameEmployee}
+    employees
   }
 
 }
