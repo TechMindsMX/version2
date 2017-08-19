9f0ed2f527b2723a64ba18dbd61bc72f1d0a8bcf
temocpalma <temopalmag@gmail.com>
2017-08-18 20:09:19 -0500

    coding #381 Generando y agregando changeset que crea la tabla para las incidencias de un empleado de prenómina

--- web/grails-app/migrations/changelog.groovy
+++ web/grails-app/migrations/changelog.groovy
@@ -65,4 +65,5 @@ databaseChangeLog = {
     include file: 'create-domains-for-paysheet.groovy'
     include file: 'adding-column-commission-to-paysheet-project.groovy'
     include file: 'adding-column-reject-reason-to-paysheet.groovy'
+    include file: 'create-domain-prepaysheet-employee-incidence.groovy'
 }
