f1770cb85bbd231713bdc8b874344fb1d657f56c
temocpalma <temopalmag@gmail.com>
2017-08-24 16:41:57 -0500

    coding #385 Creando test del método que genera el txt de dispersión de pagos con cuenta en el mismo banco de la empresa

--- m1core/src/test/groovy/com/modulus/uno/paysheet/PaysheetServiceSpec.groovy
+++ m1core/src/test/groovy/com/modulus/uno/paysheet/PaysheetServiceSpec.groovy
@@ -5,9 +5,10 @@ import grails.test.mixin.Mock
 import spock.lang.Specification
 
 import com.modulus.uno.Company
+import com.modulus.uno.BankAccount
 
 @TestFor(PaysheetService)
-@Mock([Paysheet, PrePaysheet, Company, PaysheetEmployee, PrePaysheetEmployee])
+@Mock([Paysheet, PrePaysheet, Company, PaysheetEmployee, PrePaysheetEmployee, BankAccount])
 class PaysheetServiceSpec extends Specification {
 
   PaysheetEmployeeService paysheetEmployeeService = Mock(PaysheetEmployeeService)
@@ -35,5 +36,29 @@ class PaysheetServiceSpec extends Specification {
     prePaysheet.addToEmployees(new PrePaysheetEmployee().save(validate:false))
     prePaysheet.save(validate:false)
     prePaysheet
+  }
+
+  void "Should create a txt file imss dispersion for a employee list and a charge bank account"() {
+    given:"employees list"
+      List<PaysheetEmployee> employees = [createPaysheetEmployee()]
+    and:"The charge bank account"
+      BankAccount chargeBankAccount = new BankAccount(accountNumber:"CompanyAccount").save(validate:false)
+    when:
+      def result = service.createTxtImssDispersionFileForSameCompanyBank(employees, chargeBankAccount)
+    then:
+      result.text == "000EmployeeAccount0000CompanyAccountMXN0000000004200.00PAGO IMSS                     \n"
+  }
+
+  private PaysheetEmployee createPaysheetEmployee() {
+    PaysheetEmployee paysheetEmployee = new PaysheetEmployee(
+      prePaysheetEmployee: new PrePaysheetEmployee(account:"EmployeeAccount").save(validate:false),
+      salaryImss: new BigDecimal(1000),
+      socialQuota: new BigDecimal(100),
+      subsidySalary: new BigDecimal(500),
+      incomeTax: new BigDecimal(200),
+      salaryAssimilable: new BigDecimal(3000)
+    )
+    paysheetEmployee.save(validate:false)
+    paysheetEmployee
   }
 }
