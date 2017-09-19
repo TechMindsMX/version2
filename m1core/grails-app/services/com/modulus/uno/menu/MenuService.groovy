35c177bdd0d5d00d7149b1092625cf3194b032af
neodevelop <juan@makingdevs.com>
2017-09-19 09:44:13 -0500

    #399 Agregando métodos para agregar submenu a menu

--- m1core/grails-app/services/com/modulus/uno/menu/MenuService.groovy
+++ m1core/grails-app/services/com/modulus/uno/menu/MenuService.groovy
@@ -1,6 +1,7 @@
 package com.modulus.uno.menu
 
 import grails.transaction.Transactional
+import org.springframework.transaction.annotation.Propagation
 
 @Transactional
 class MenuService {
@@ -12,9 +13,25 @@ class MenuService {
   }
 
   Menu addSubmenuToMenu(Menu menu, Menu submenu){
-    menu.addToMenus(submenu)
+    menu.menus << submenu
+    //submenu.parentMenu = menu
     menu.save()
+    //submenu.save()
+    println menu.validate()
+    println menu.menus
     menu
+  }
+
+  Menu addSubmenuToMenu(Long menuId, Long submenuId){
+    addSubmenuToMenu(Menu.get(menuId), Menu.get(submenuId))
+  }
+
+  @Transactional(propagation = Propagation.REQUIRES_NEW)
+  Menu addFewSubmenusToMenu(Long menuId, List<Long> submenuIds){
+    submenuIds.each { submenuId ->
+      addSubmenuToMenu(menuId, submenuId)
+    }
+    Menu.get(menuId)
   }
 
   Menu addSubmenuToMenu(Menu menu, String submenuName, String internalUrl, String parameters){
