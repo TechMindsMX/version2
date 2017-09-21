84cca267c92348cc59de085c565a61d36b6edccd
Temoc Palma <temopalmag@gmail.com>
2017-09-21 10:21:35 -0500

    coding #416 Ajustando métodos para generar los archivos de dispersión de pagos de nómina

--- m1core/grails-app/services/com/modulus/uno/paysheet/PaysheetService.groovy
+++ m1core/grails-app/services/com/modulus/uno/paysheet/PaysheetService.groovy
@@ -133,7 +133,7 @@ class PaysheetService {
 		deleteCurrentDispersionFilesFromPaysheet(paysheet)
     dispersionData = complementDispersionData(dispersionData)
 		generateDispersionFileSameBank(paysheet, dispersionData)
-		generateDispersionFileInterBank(paysheet, dispersionData)
+		//generateDispersionFileInterBank(paysheet, dispersionData)
   }
 
 	def deleteCurrentDispersionFilesFromPaysheet(Paysheet paysheet){
