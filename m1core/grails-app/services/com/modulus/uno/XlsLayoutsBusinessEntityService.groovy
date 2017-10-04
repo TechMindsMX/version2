0ec9a7cc635f34f078e6550d441cba633fdd8bf9
Luis Enrique Sastre <luis@makingdevs.com>
2017-10-04 10:35:43 -0500

    #435 Download xls layout from Client/Provider/Client-Provider

--- m1core/grails-app/services/com/modulus/uno/XlsLayoutsBusinessEntityService.groovy
+++ m1core/grails-app/services/com/modulus/uno/XlsLayoutsBusinessEntityService.groovy
@@ -5,20 +5,29 @@ import pl.touk.excel.export.WebXlsxExporter
 class XlsLayoutsBusinessEntityService {
 
   def generateLayoutForCLIENTE() {
+    def headers = ['PERSONA','RFC','SITIO_WEB','RAZON_SOCIAL','PATERNO','MATERNO','NOMBRE', 'CLAVE_BANCO', 'ULTIMOS_4_DIGITOS_TARJETA',  'CALLE', 'NUMEXTERIOR', 'NUMINTERIOR', 'CODIGO_POSTAL', 'COLONIA', 'DELEGACION/MUNICIPIO', 'PAIS', 'CIUDAD', 'ENTIDAD_FEDERATIVA', 'TIPO_DE_DIRECCION']
+    def descriptions = ['FISICA ó MORAL', '', 'www.ejemplo.com', '', '', '', '', 'http://www.sat.gob.mx/fichas_tematicas/buzon_tributario/documents/catalogo_bancos.pdf', '####', '', '', '', '', '', '', '', '', '', 'FISCAL ó SOCIAL ó COMERCIAL']
     new WebXlsxExporter().with {
-      fillRow(["Layout para alta masiva de tipo CLIENTE"], 0)
+      fillRow(headers, 0)
+      fillRow(descriptions, 1)
     }
   }
 
   def generateLayoutForCLIENTE_PROVEEDOR() {
+    def headers = ['PERSONA','RFC','SITIO_WEB','RAZON_SOCIAL','PATERNO','MATERNO','NOMBRE', 'CLABE', 'CALLE', 'NUMEXTERIOR', 'NUMINTERIOR', 'CODIGO_POSTAL', 'COLONIA', 'DELEGACION/MUNICIPIO', 'PAIS', 'CIUDAD', 'ENTIDAD_FEDERATIVA', 'TIPO_DE_DIRECCION']
+    def descriptions = ['FISICA ó MORAL', '', 'www.ejemplo.com', '', '', '', '', '18 dígitos', '', '', '', '', '', '', '', '', '', 'FISCAL ó SOCIAL ó COMERCIAL']
     new WebXlsxExporter().with {
-      fillRow(["Layout para alta masiva de tipo CLIENTE_PROVEEDOR"], 0)
+      fillRow(headers, 0)
+      fillRow(descriptions, 1)    
     }
   }
 
   def generateLayoutForPROVEEDOR() {
+    def headers = ['PERSONA','RFC','SITIO_WEB','RAZON_SOCIAL','PATERNO','MATERNO','NOMBRE', 'CLABE', 'CALLE', 'NUMEXTERIOR', 'NUMINTERIOR', 'CODIGO_POSTAL', 'COLONIA', 'DELEGACION/MUNICIPIO', 'PAIS', 'CIUDAD', 'ENTIDAD_FEDERATIVA', 'TIPO_DE_DIRECCION']
+    def descriptions = ['FISICA ó MORAL', '', 'www.ejemplo.com', '', '', '', '', '18 dígitos', '', '', '', '', '', '', '', '', '', 'FISCAL ó SOCIAL ó COMERCIAL']
     new WebXlsxExporter().with {
-      fillRow(["Layout para alta masiva de tipo PROVEEDOR"], 0)
+      fillRow(headers, 0)
+      fillRow(descriptions, 1)  
     }
   }
 
