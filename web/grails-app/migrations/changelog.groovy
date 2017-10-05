a8d75459c53286b1b093fe02c121153fa1596729
Temoc Palma <temopalmag@gmail.com>
2017-10-05 10:11:34 -0500

    #232 Generando changelog para agregar columna de número de cliente a cuenta de banco

--- web/grails-app/migrations/changelog.groovy
+++ web/grails-app/migrations/changelog.groovy
@@ -74,6 +74,7 @@ databaseChangeLog = {
   include file: 'modify-column-commission-to-paysheet-project.groovy'
 	include file: 'create-roles-for-queries.groovy'
   include file: 'modify-column-commission-to-paysheet-project.groovy'
-  include file: 'adding-dispersion-files-s3asset-to-paysheet.groovy'
+	include file: 'adding-dispersion-files-s3asset-to-paysheet.groovy'
+  include file: 'adding-column-client-number-to-bankaccount.groovy'
 	include file: 'new-menu-relation.groovy'
 }
