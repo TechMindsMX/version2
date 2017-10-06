57f52573573c4606ad746666f0a004f4c10cd4f6
Luis Enrique Sastre <luis@makingdevs.com>
2017-10-06 13:18:24 -0500

    #436 Adding Map for client

--- m1core/grails-app/services/com/modulus/uno/XlsImportService.groovy
+++ m1core/grails-app/services/com/modulus/uno/XlsImportService.groovy
@@ -11,12 +11,28 @@ class XlsImportService {
     columnMap:  ['A':'RFC', 'B':'CURP', 'C':'PATERNO', 'D':'MATERNO', 'E':'NOMBRE', 'F':'NO_EMPL', 'G':'CLABE', 'H':'NUMTARJETA', 'I':'IMSS', 'J':'NSS', 'K':'FECHA_ALTA', 'L':'BASE_COTIZA', 'M':'NETO', 'N':'PRIMA_VAC', 'O':'DIAS_AGUINALDO', 'P':'PERIODO_PAGO']
   ]
 
+  Map COLUMN_MAP_CLIENT = [
+    startRow: 1,
+    columnMap:  ['A':'PERSONA', 'B':'RFC', 'C':'SITIO_WEB', 'D':'RAZON_SOCIAL', 'E':'PATERNO', 'F':'MATERNO', 'G':'NOMBRE', 'H':'CLAVE_BANCO', 'I':'ULTIMOS_4_DIGITOS_TARJETA', 'J':'CALLE', 'K':'NUMEXTERIOR', 'L':'NUMINTERIOR', 'M':'CODIGO_POSTAL', 'N':'COLONIA', 'O':'DELEGACION/MUNICIPIO', 'P':'PAIS', 'Q':'CIUDAD', 'R':'ENTIDAD_FEDERATIVA', 'T':'TIPO_DE_DIRECCION']
+  ]
+
   def parseXlsMassiveEmployee(File xlsFile) {
     Workbook workbook = getWorkbookFromXlsFile(xlsFile)
     COLUMN_MAP_EMPLOYEE.sheet = workbook.getSheetName(0)
     log.info "Column Map: ${COLUMN_MAP_EMPLOYEE}"
     ExcelImportService excelImportService = new ExcelImportService()
     List data = excelImportService.convertColumnMapConfigManyRows(workbook, COLUMN_MAP_EMPLOYEE)
+    log.info "Data: ${data}"
+    validateNotEmptyData(data)
+    data
+  }
+
+  def parseXlsMassiveClient(File xlsFile) {
+    Workbook workbook = getWorkbookFromXlsFile(xlsFile)
+    COLUMN_MAP_CLIENT.sheet = workbook.getSheetName(0)
+    log.info "Column Map: ${COLUMN_MAP_CLIENT}"
+    ExcelImportService excelImportService = new ExcelImportService()
+    List data = excelImportService.convertColumnMapConfigManyRows(workbook, COLUMN_MAP_CLIENT)
     log.info "Data: ${data}"
     validateNotEmptyData(data)
     data
