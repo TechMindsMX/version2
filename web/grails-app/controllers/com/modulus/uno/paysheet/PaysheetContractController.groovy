6e8f4592354d2a195018437045fb21c63d06eebf
Temoc Palma <temopalmag@gmail.com>
2017-10-20 17:04:08 -0500

    #448 Creando controller de contrato de nómina

---
+++ web/grails-app/controllers/com/modulus/uno/paysheet/PaysheetContractController.groovy
@@ -0,0 +1,23 @@
+package com.modulus.uno.paysheet
+
+import grails.transaction.Transactional
+
+import com.modulus.uno.BusinessEntityService
+import com.modulus.uno.CompanyService
+import com.modulus.uno.Company
+import com.modulus.uno.User
+
+@Transactional(readOnly = true)
+class PaysheetContractController {
+
+  BusinessEntityService businessEntityService
+  CompanyService companyService
+  
+  def create() {
+    Company company = Company.get(session.company)
+    def clients = businessEntityService.findBusinessEntityByKeyword("", "CLIENT", company)
+    List<User> users = companyService.getUsersWithRoleForCompany("ROLE_PAYSHEET_OPERATOR", company)
+    respond new PaysheetContract(), model:[company:company, clients:clients, users:users]
+  }
+
+}
