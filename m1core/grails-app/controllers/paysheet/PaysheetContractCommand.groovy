0bbcf6249280fcdcbe875845d778423d82946f9e
Temoc Palma <temopalmag@gmail.com>
2017-10-20 10:36:47 -0500

    #447 Agregando campo de la empresa

--- m1core/grails-app/controllers/paysheet/PaysheetContractCommand.groovy
+++ m1core/grails-app/controllers/paysheet/PaysheetContractCommand.groovy
@@ -9,18 +9,21 @@ class PaysheetContractCommand implements Validateable {
   String clientId
   String initDate
   String executiveId
+  String companyId
 
   static constraints = {
     clientId nullable:false
     initDate nullable:false
     executiveId nullable:false
+    companyId nullable:false
   }
 
   PaysheetContract createPaysheetContract() {
     new PaysheetContract(
       client:BusinessEntity.get(this.clientId),
       initDate:Date.parse("dd/MM/yyyy", this.initDate),
-      executive:User.get(this.executiveId)
+      executive:User.get(this.executiveId),
+      company:Company.get(this.companyId)
     )
   }
 }
--- m1core/grails-app/domain/com/modulus/uno/paysheet/PaysheetContratc.groovy
+++ m1core/grails-app/domain/com/modulus/uno/paysheet/PaysheetContratc.groovy
@@ -2,6 +2,7 @@ package com.modulus.uno.paysheet
 
 import com.modulus.uno.BusinessEntity
 import com.modulus.uno.User
+import com.modulus.uno.Company
 
 class PaysheetContract {
   
@@ -12,6 +13,7 @@ class PaysheetContract {
   Date initDate
   User executive
 
+  static belongsTo = [company:Company]
   static hasMany = [employees:BusinessEntity, projects:PaysheetProject]
 
   static constraint = {
