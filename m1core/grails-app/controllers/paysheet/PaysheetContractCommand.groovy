afc942eae03226d70c5bcbe8284e6023c0b7b700
Temoc Palma <temopalmag@gmail.com>
2017-10-20 09:57:58 -0500

    #447 Agregango usuario ejecutivo del contrato

--- m1core/grails-app/controllers/paysheet/PaysheetContractCommand.groovy
+++ m1core/grails-app/controllers/paysheet/PaysheetContractCommand.groovy
@@ -2,21 +2,25 @@ package com.modulus.uno.paysheet
 
 import grails.validation.Validateable
 import com.modulus.uno.BusinessEntity
+import com.modulus.uno.User
 
 class PaysheetContractCommand implements Validateable {
   
   String clientId
   String initDate
+  String executiveId
 
   static constraints = {
     clientId nullable:false
     initDate nullable:false
+    executiveId nullable:false
   }
 
   PaysheetContract createPaysheetContract() {
     new PaysheetContract(
       client:BusinessEntity.get(this.clientId),
-      initDate:Date.parse("dd/MM/yyyy", this.initDate)
+      initDate:Date.parse("dd/MM/yyyy", this.initDate),
+      executive:User.get(this.executiveId)
     )
   }
 }
