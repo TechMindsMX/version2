f816339487de44c7d14fc17eac6501d669dc665f
Temoc Palma <temopalmag@gmail.com>
2017-10-23 17:12:15 -0500

    #449 Creando método para obtener la lista de empleados disponibles a agregar a un contrato de nómina

--- m1core/grails-app/services/com/modulus/uno/paysheet/PaysheetContractService.groovy
+++ m1core/grails-app/services/com/modulus/uno/paysheet/PaysheetContractService.groovy
@@ -1,10 +1,22 @@
 package com.modulus.uno.paysheet
 
+import com.modulus.uno.BusinessEntityService
+import com.modulus.uno.BusinessEntity
+
 class PaysheetContractService {
   
+  BusinessEntityService businessEntityService
+
   def savePaysheetContract(PaysheetContract paysheetContract){
     paysheetContract.save()
     paysheetContract
   }
  
+  def getEmployeesAvailableToAdd(PaysheetContract paysheetContract){
+    List<BusinessEntity> allEmployees = businessEntityService.getAllActiveEmployeesForCompany(paysheetContract.company)
+    List<BusinessEntity> availableEmployees = allEmployees.collect { employee ->
+      if (!paysheetContract.employees.contains(employee)) { employee }
+    }.grep()
+    availableEmployees
+  }
 }
