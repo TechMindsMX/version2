1b856aabcd16fe098e4b415d0a1bfd2a360eaacd
Temoc Palma <temopalmag@gmail.com>
2017-10-24 16:18:06 -0500

    #450 Ajustando actions con nuevo flujo de proyectos de nómina

--- web/grails-app/controllers/com/modulus/uno/paysheet/PaysheetProjectController.groovy
+++ web/grails-app/controllers/com/modulus/uno/paysheet/PaysheetProjectController.groovy
@@ -6,16 +6,14 @@ class PaysheetProjectController {
 
   PaysheetProjectService paysheetProjectService
 
-  def create() {
-    Company company = Company.get(session.company)
-    respond new PaysheetProject(), model:[company:company]
+  def create(PaysheetContract paysheetContract) {
+    respond new PaysheetProject(paysheetContract:paysheetContract)
   }
 
   def save(PaysheetProjectCommand command) {
     log.info "Saving paysheet project command: ${command.dump()}"
-    Company company = Company.get(session.company)
     if (command.hasErrors()) {
-      render view:"create", model:[paysheetProject:command, company:company]
+      render view:"create", model:[paysheetProject:command, paysheetContract:PaysheetContract.get(command.contractId)]
       return
     }
 
@@ -23,25 +21,23 @@ class PaysheetProjectController {
     paysheetProjectService.savePaysheetProject(paysheetProject)
 
     if (paysheetProject.hasErrors()) {
-      render view:"create", model:[paysheetProject:paysheetProject, company:company]
+      render view:"create", model:[paysheetProject:paysheetProject, paysheetContract:paysheetProject.paysheetContract]
       return
     }
 
     log.info "Paysheet project saved: ${paysheetProject.dump()}"
 
-    redirect controller:"company", action:"show", id:company.id
+    redirect controller:"paysheetContract", action:"show", id:paysheetProject.paysheetContract.id
   }
 
   def edit(PaysheetProject paysheetProject) {
-    Company company = Company.get(session.company)
-    respond paysheetProject, model:[company:company]
+    respond paysheetProject
   }
 
   def update(PaysheetProjectCommand command) {
     log.info "Paysheet Project to update: ${command.dump()}"
-    Company company = Company.get(session.company)
     if (command.hasErrors()) {
-      render view:"edit", model:[paysheetProject:command, company:company]
+      render view:"edit", model:[paysheetProject:command]
       return
     }
 
@@ -51,20 +47,20 @@ class PaysheetProjectController {
     paysheetProjectService.savePaysheetProject(paysheetProject)
 
     if (paysheetProject.hasErrors()) {
-      render view:"edit", model:[paysheetProject:paysheetProject, company:company]
+      render view:"edit", model:[paysheetProject:paysheetProject]
       return
     }
 
     log.info "Paysheet Project Instance updated: ${paysheetProject.dump()}"
 
-    redirect controller:"company", action:"show", id:company.id
+    redirect controller:"paysheetContract", action:"show", id:paysheetProject.paysheetContract.id
   }
 
   def delete(PaysheetProject paysheetProject) {
     log.info "Deleting paysheet project ${paysheetProject.dump()}"
-    Company company = Company.get(session.company)
+    PaysheetContract paysheetContract = paysheetProject.paysheetContract
     paysheetProjectService.deletePaysheetProject(paysheetProject)
-    redirect controller:"company", action:"show", id:company.id
+    redirect controller:"paysheetContract", action:"show", id:paysheetContract.id
   }
 
 }
