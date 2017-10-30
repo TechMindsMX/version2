e163fdea5c8d1324de0a574dfea9610a576de3cb
Luis Enrique Sastre <luis@makingdevs.com>
2017-10-30 15:20:35 -0600

    #458 Check name of company in controller

--- web/grails-app/controllers/com/modulus/uno/quotation/QuotationContractController.groovy
+++ web/grails-app/controllers/com/modulus/uno/quotation/QuotationContractController.groovy
@@ -1,10 +1,13 @@
 package com.modulus.uno.quotation
 
+import com.modulus.uno.Company
+
 class QuotationContractController {
 
-    def index() { }
-
     def show(){
-
+    	Company company = Company.get(session.company)
+    	respond new QuotationContract(), model:[company:company]
     }
+
+    
 }
