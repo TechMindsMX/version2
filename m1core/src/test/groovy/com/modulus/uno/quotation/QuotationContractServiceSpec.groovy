26275406f4871cc21b660cdc003cef3ef926fdd3
leovergara1 <leovergara.dark@gmail.com>
2017-10-31 11:21:27 -0600

    458.1 adding test save

--- m1core/src/test/groovy/com/modulus/uno/quotation/QuotationContractServiceSpec.groovy
+++ m1core/src/test/groovy/com/modulus/uno/quotation/QuotationContractServiceSpec.groovy
@@ -2,12 +2,14 @@ package com.modulus.uno.quotation
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import grails.test.mixin.Mock
 import com.modulus.uno.BusinessEntity
 
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(QuotationContractService)
+@Mock([BusinessEntity])
 class QuotationContractServiceSpec extends Specification {
 
 
@@ -25,17 +27,23 @@ class QuotationContractServiceSpec extends Specification {
     }
 
     void "Save quotation"(){
-      given:"A quotation"
-        QuotationContract quotationContrac = new QuotationContract(
-                                                  client: BusinessEntity.get(1),
+      given:"A quotation and BusinessEntity"
+        BusinessEntity client = new BusinessEntity(
+                                                  rfc:"BDJBDYHSGGDVVD",
+                                                  website:"loquesea@makingdes.com",
+                                                  artemisaId:"33"
+                                                    )
+        QuotationContract quotationContract = new QuotationContract(
+                                                  client: client,
                                                   commision: 10,
                                                   initDate: new Date()
                                                   )
 
       when:"shouuld save"
-        service.save(quotationContrac)
+        println client.dump()
+        //quotationContract.save(validate:false)
       then:
-        quotationContrac
+        quotationContract
     }
 
 
