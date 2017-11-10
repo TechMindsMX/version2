9ccd7e0ac192fdf94d097477be136524c8834994
Luis Enrique Sastre <luis@makingdevs.com>
2017-11-10 14:27:24 -0600

    #486 Sacando monto de cotizaciones

--- m1core/grails-app/services/com/modulus/uno/quotation/QuotationPaymentRequestService.groovy
+++ m1core/grails-app/services/com/modulus/uno/quotation/QuotationPaymentRequestService.groovy
@@ -1,10 +1,12 @@
 package com.modulus.uno.quotation
 
 import grails.transaction.Transactional
+import com.modulus.uno.quotation.QuotationRequest
 
 @Transactional
 class QuotationPaymentRequestService {
 
+  QuotationRequestService quotationRequestService
 
   QuotationPaymentRequest create(QuotationPaymentRequest quotationPaymentRequest){
     quotationPaymentRequest.save()
@@ -15,6 +17,19 @@ class QuotationPaymentRequestService {
   }
 
   QuotationPaymentRequest process(QuotationPaymentRequest quotationPaymentRequest){
+    //QuotationContract quotationContract= quotationPaymentRequest.quotationContract
+    List<QuotationRequest> quotationRequest = QuotationRequest.findAllByQuotationContractAndStatus(quotationPaymentRequest.quotationContract, QuotationRequestStatus.PROCESSED)
+    List<QuotationPaymentRequest> quotationPaymentRequestlist = QuotationPaymentRequest.findAllByQuotationContractAndStatus(quotationPaymentRequest.quotationContract, QuotationPaymentRequestStatus.PAYED)
+    println "-----"
+    println quotationPaymentRequestlist.dump() 
+    println quotationRequest.dump()
+    def amount = 0
+    quotationRequest.each{
+      amount = amount + it.amount 
+    }
+
+    println amount
+    quotationRequest
     quotationPaymentRequest.status = QuotationPaymentRequestStatus.PAYED
     quotationPaymentRequest.save()
   }
@@ -26,6 +41,10 @@ class QuotationPaymentRequestService {
 
   QuotationPaymentRequest delete(QuotationPaymentRequest quotationPaymentRequest){
     quotationPaymentRequest.delete()
+  }
+
+  def getAmountFromQuotationRequest(){
+    def amount = quotationRequest.amount 
   }
 
 }
