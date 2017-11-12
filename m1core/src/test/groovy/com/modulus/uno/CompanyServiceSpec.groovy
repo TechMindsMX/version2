3b57ef9cff44c4b7a4200fbb52d4e0195070cc5f
Temoc Palma <temopalmag@gmail.com>
2017-11-11 22:25:24 -0600

    #484 Agregando servicio de resultado de traspaso final

--- m1core/src/test/groovy/com/modulus/uno/CompanyServiceSpec.groovy
+++ m1core/src/test/groovy/com/modulus/uno/CompanyServiceSpec.groovy
@@ -11,6 +11,7 @@ import spock.lang.Unroll
 import spock.lang.Ignore
 
 import com.modulus.uno.stp.StpService
+import com.modulus.uno.stp.FinalTransactionResultService
 
 @TestFor(CompanyService)
 @Mock([Company,Corporate,Address,S3Asset,User,UserRole,Role,UserRoleCompany,Profile, ModulusUnoAccount, Commission, Bank, Transaction, MovimientosBancarios, BankAccount])
@@ -29,6 +30,7 @@ class CompanyServiceSpec extends Specification {
   CommissionTransactionService commissionTransactionService = Mock(CommissionTransactionService)
   StpService stpService = Mock(StpService)
   MovimientosBancariosService movimientosBancariosService = Mock(MovimientosBancariosService)
+  FinalTransactionResultService finalTransactionResultService = Mock(FinalTransactionResultService)
 
   def setup(){
     service.modulusUnoService = modulusUnoService
@@ -44,6 +46,7 @@ class CompanyServiceSpec extends Specification {
     service.commissionTransactionService = commissionTransactionService
     service.stpService = stpService
     service.movimientosBancariosService = movimientosBancariosService
+    service.finalTransactionResultService = finalTransactionResultService
   }
 
   Should "create a direction for a Company"(){
