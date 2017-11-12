1db6f19b5871d0fdf5f500452e0776c0617a11d4
Temoc Palma <temopalmag@gmail.com>
2017-11-11 22:25:58 -0600

    #484 Creando servicio para guardar el resultado del traspaso final y sus tests

---
+++ m1core/grails-app/services/com/modulus/uno/stp/FinalTransactionResultService.groovy
@@ -0,0 +1,25 @@
+package com.modulus.uno.stp
+
+import grails.transaction.Transactional
+
+class FinalTransactionResultService {
+  
+  @Transactional
+  def createFinalTransactionResult(Map dataResult){
+    ExecutionMode executionMode = getExecutionMode(dataResult.dateTransaction)
+    FinalTransactionResult result = new FinalTransactionResult(
+      company:dataResult.company,
+      transactionDate:dataResult.dateTransaction,
+      comment:dataResult.comment,
+      status:dataResult.status,
+      executionMode:executionMode
+    )
+    result.save()
+    log.info "Final Transaction Result saved: ${result.dump()}"
+    result
+  }
+
+  private ExecutionMode getExecutionMode(Date dateTransaction) {
+    new Date().format("ddMMyyyy") == dateTransaction.format("ddMMyyyy") ? ExecutionMode.AUTOMATIC : ExecutionMode.MANUAL
+  }
+}
