8213dd63d755b0796549cb5de7bf8df97c6117e2
Temoc Palma <temopalmag@gmail.com>
2017-11-13 14:04:36 -0600

    #428 Agregando comportamiento de solo lectura al select de banco

--- web/grails-app/assets/javascripts/clabe.js
+++ web/grails-app/assets/javascripts/clabe.js
@@ -1,17 +1,24 @@
 function complete(){
   var clabe = this.value
   var objBanco = null
-  if ($("#banco").length)
-    objBanco = $("#banco option")
-  else
+  if ($("#bank").length) {
+    objBanco = $("#bank option")
+    $("#bank").removeAttr('readonly')
+  } else {
     objBanco = $("#sbanco option")
+    $("#sbanco").removeAttr('readonly')
+  }
   objBanco.each(function(){
     if(clabe.substring(0,3) == this.value.substring(this.value.length-3, this.value.length)){
-      if ($("#banco").length)
-        $("#banco").val(this.value)
-      else
+      if ($("#bank").length) {
+        $("#bank").val(this.value)
+        if (clabe.length)
+          $("#bank").attr('readOnly', true)
+      } else {
         $("#sbanco").val(this.value)
-      $("#bank").val(this.value)
+        if (clabe.length)
+          $("#sbanco").attr('readOnly', true)
+      }
       $("#branchNumber").val(clabe.substring(3,6))
       $("#accountNumber").val(clabe.substring(6,17))
     }
