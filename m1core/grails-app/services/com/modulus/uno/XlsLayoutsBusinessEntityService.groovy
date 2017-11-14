083832a99bd4be2534e791514ab925bc84992084
Temoc Palma <temopalmag@gmail.com>
2017-11-14 16:23:22 -0600

    #481 Complementando columnas del layout para los datos bancarios

--- m1core/grails-app/services/com/modulus/uno/XlsLayoutsBusinessEntityService.groovy
+++ m1core/grails-app/services/com/modulus/uno/XlsLayoutsBusinessEntityService.groovy
@@ -23,8 +23,8 @@ class XlsLayoutsBusinessEntityService {
   }
 
   def generateLayoutForEMPLEADO() {
-    def headers = ['RFC','CURP','PATERNO','MATERNO','NOMBRE','NUM_EMPL','CLABE','CUENTA','SUCURSAL','NUMTARJETA',"IMSS","NSS","FECHA_ALTA", "BASE_COTIZA", "NETO", "PRIMA_VAC", "DIAS_AGUINALDO", "PERIODO_PAGO"]
-    def descriptions = ['Reemplazar esta fila', '', '', '', '', 'No. de Empleado', '18 dígitos','11 dígitos máximo','3 dígitos', '16 dígitos', "S ó N","", "dd-MM-yyyy", "Salario Base de Cotización mensual", "Salario Neto mensual", "En porcentaje", "", "Semanal, Catorcenal, Quincenal, Mensual"]
+    def headers = ['RFC','CURP','PATERNO','MATERNO','NOMBRE','NUM_EMPL','BANCO','CLABE','CUENTA','SUCURSAL','NUMTARJETA',"IMSS","NSS","FECHA_ALTA", "BASE_COTIZA", "NETO", "PRIMA_VAC", "DIAS_AGUINALDO", "PERIODO_PAGO"]
+    def descriptions = ['Reemplazar esta fila', '', '', '', '', 'No. de Empleado', 'Código del Banco', '18 dígitos','11 dígitos máximo','3 dígitos', '16 dígitos', "S ó N","", "dd-MM-yyyy", "Salario Base de Cotización mensual", "Salario Neto mensual", "En porcentaje", "", "Semanal, Catorcenal, Quincenal, Mensual"]
     new WebXlsxExporter().with {
       fillRow(headers, 0)
       fillRow(descriptions, 1)
