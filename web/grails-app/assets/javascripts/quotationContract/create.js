e96fbc26ec1028a56152f0bebb564a62391807cc
leovergara1 <leovergara.dark@gmail.com>
2017-11-16 12:11:51 -0600

    #487 changed formate to date

--- web/grails-app/assets/javascripts/quotationContract/create.js
+++ web/grails-app/assets/javascripts/quotationContract/create.js
@@ -2,13 +2,13 @@ $( window ).on( "load", function() {
   $( function() {
     $( "#datepicker" ).datepicker({
       setDate: new Date(),
-      dateFormat: 'dd/mm/yy'
+      dateFormat: 'dd-mm-yy'
     });
   } );
 
   $( function() {
     $( "#datepicker1" ).datepicker({
-      dateFormat: 'dd/mm/yy'
+      dateFormat: 'dd-mm-yy'
     });
   } );
   $('#datepicker').datepicker('setDate', new Date());
