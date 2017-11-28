e7571b0fb8cd005c91735e92a526f5ac4eb63ce5
Temoc Palma <temopalmag@gmail.com>
2017-11-27 18:28:06 -0600

    #511 Regenerar changelog con restricción de nulo en campo símbolo

--- web/grails-app/migrations/create-domain-unit-type.groovy
+++ web/grails-app/migrations/create-domain-unit-type.groovy
@@ -1,6 +1,6 @@
 databaseChangeLog = {
 
-    changeSet(author: "temoc (generated)", id: "1511819506792-1") {
+    changeSet(author: "temoc (generated)", id: "1511825075759-1") {
         createTable(tableName: "unit_type") {
             column(autoIncrement: "true", name: "id", type: "BIGINT") {
                 constraints(primaryKey: "true", primaryKeyName: "unit_typePK")
@@ -14,9 +14,7 @@ databaseChangeLog = {
                 constraints(nullable: "false")
             }
 
-            column(name: "symbol", type: "VARCHAR(255)") {
-                constraints(nullable: "false")
-            }
+            column(name: "symbol", type: "VARCHAR(255)")
 
             column(name: "unit_key", type: "VARCHAR(255)") {
                 constraints(nullable: "false")
