3e8c9bb3fa8a78a6be00dc8f13bd3a189568ce84
Temoc Palma <temopalmag@gmail.com>
2017-11-29 08:27:05 -0600

    #512 Agregar empresa al dominio de tipos de unidad

--- web/grails-app/migrations/create-domain-unit-type.groovy
+++ web/grails-app/migrations/create-domain-unit-type.groovy
@@ -1,12 +1,16 @@
 databaseChangeLog = {
 
-    changeSet(author: "temoc (generated)", id: "1511889915302-1") {
+    changeSet(author: "temoc (generated)", id: "1511909273239-1") {
         createTable(tableName: "unit_type") {
             column(autoIncrement: "true", name: "id", type: "BIGINT") {
                 constraints(primaryKey: "true", primaryKeyName: "unit_typePK")
             }
 
             column(name: "version", type: "BIGINT") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "company_id", type: "BIGINT") {
                 constraints(nullable: "false")
             }
 
@@ -22,8 +26,12 @@ databaseChangeLog = {
         }
     }
 
-    changeSet(author: "temoc (generated)", id: "1511889915302-2") {
-        addUniqueConstraint(columnNames: "unit_key", constraintName: "UC_UNIT_TYPEUNIT_KEY_COL", tableName: "unit_type")
+    changeSet(author: "temoc (generated)", id: "1511909273239-2") {
+        addUniqueConstraint(columnNames: "company_id, unit_key", constraintName: "UK2291669e32b02bae732a24673a3c", tableName: "unit_type")
+    }
+
+    changeSet(author: "temoc (generated)", id: "1511909273239-3") {
+        addForeignKeyConstraint(baseColumnNames: "company_id", baseTableName: "unit_type", constraintName: "FKpgdyblfk517hdk0g6pl3w88o3", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "company")
     }
 
 }
