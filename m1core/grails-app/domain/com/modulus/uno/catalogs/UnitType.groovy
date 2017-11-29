a0cd0fcec77496de8a6fc1fc2a40e63178ff7d12
Temoc Palma <temopalmag@gmail.com>
2017-11-29 08:24:58 -0600

    #512 Agregar empresa al catálogo de tipos de unidad

--- m1core/grails-app/domain/com/modulus/uno/catalogs/UnitType.groovy
+++ m1core/grails-app/domain/com/modulus/uno/catalogs/UnitType.groovy
@@ -1,13 +1,16 @@
 package com.modulus.uno.catalogs
 
+import com.modulus.uno.Company
+
 class UnitType {
   
+  Company company
   String unitKey
   String name
   String symbol
 
   static constraints = {
-    unitKey unique:true
+    unitKey unique:"company"
     symbol nullable:true
   }
 }
