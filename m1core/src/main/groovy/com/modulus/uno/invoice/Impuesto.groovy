e65633562f94fd98ac6e17df28cb664bd454182b
Temoc Palma <temopalmag@gmail.com>
2017-11-30 09:59:01 -0600

    #513 Mover a paquete de factura y agregar campos de v3.3

--- m1core/src/main/groovy/com/modulus/uno/invoice/Impuesto.groovy
+++ m1core/src/main/groovy/com/modulus/uno/invoice/Impuesto.groovy
@@ -1,7 +1,9 @@
-package com.modulus.uno
+package com.modulus.uno.invoice
 
 class Impuesto {
+  BigDecimal base = 0.0
   BigDecimal importe = 0.0
   BigDecimal tasa = 0.0
   String impuesto
+  String tipoFactor
 }
