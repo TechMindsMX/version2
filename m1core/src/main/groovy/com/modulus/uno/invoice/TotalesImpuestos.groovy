07067df9f7ca6991c83f79e3446e5b610a388ffb
Temoc Palma <temopalmag@gmail.com>
2017-11-30 10:01:03 -0600

    #513 Crear nuevos beans para ajuste de v3.3

---
+++ m1core/src/main/groovy/com/modulus/uno/invoice/TotalesImpuestos.groovy
@@ -0,0 +1,8 @@
+package com.modulus.uno.invoice
+
+class TotalesImpuestos {
+  BigDecimal totalImpuestosTrasladados
+  BigDecimal totalImpuestoRetenidos
+  List<Impuesto> impuestos
+  List<Impuesto> retenciones
+}
