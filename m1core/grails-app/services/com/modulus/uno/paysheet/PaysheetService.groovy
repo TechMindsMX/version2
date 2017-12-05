5d1df6b360c08f833a8302815ce0e3a3cf73ee9f
Temoc Palma <temopalmag@gmail.com>
2017-12-05 16:58:51 -0600

    #489 Corregir generación de txt de dispersión de bancomer

--- m1core/grails-app/services/com/modulus/uno/paysheet/PaysheetService.groovy
+++ m1core/grails-app/services/com/modulus/uno/paysheet/PaysheetService.groovy
@@ -272,14 +272,18 @@ class PaysheetService {
 
 		String salary = schema == "SA" ? "imssSalaryNet" : "salaryAssimilable"
 		String account = schema == "SA" ? "saBankAccount" : "iasBankAccount"
-		String sourceAccount = dispersionDataForBank."${account}".accountNumber.padLeft(18,'0')
-		String currency = "MXN"
-		String message = "${schema.padLeft(3,'S')}-${clearSpecialCharsFromString(dispersionDataForBank.paymentMessage).padRight(26,' ')}"
+    String rfc = "".padLeft(16," ")
+    String type = "99"
+    String bank = "001"
+    String branch = "001"
 
-    dispersionDataForBank.employees.each { employee ->
-      String destinyAccount = employee.prePaysheetEmployee.account.padLeft(18,'0')
-      String amount = (new DecimalFormat('##0.00').format(employee."${salary}")).padLeft(16,'0')
-			file.append("${destinyAccount}${sourceAccount}${currency}${amount}${message}\n")
+    dispersionDataForBank.employees.eachWithIndex { employee, index ->
+      String counter = "${index+1}".padLeft(9,"0")
+      String destinyAccount = employee.prePaysheetEmployee.account.padRight(20,' ')
+      String amount = (new DecimalFormat('##0.00').format(employee."${salary}")).replace(".","").padLeft(15,'0')
+      String adjustName = employee.prePaysheetEmployee.nameEmployee.length() > 40 ? employee.prePaysheetEmployee.nameEmployee.substring(0,40) : employee.prePaysheetEmployee.nameEmployee
+      String name = clearSpecialCharsFromString(adjustName).padRight(40," ")
+			file.append("${counter}${rfc}${type}${destinyAccount}${amount}${name}${bank}${branch}\n")
     }
 
     log.info "File created: ${file.text}"
