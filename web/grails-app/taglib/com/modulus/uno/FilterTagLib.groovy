98c14cc8a80aa05395ff0e0d921268134c34ea01
Temoc Palma <temopalmag@gmail.com>
2017-12-12 10:35:28 -0600

    #477 Ajustar taglib de filtros

--- web/grails-app/taglib/com/modulus/uno/FilterTagLib.groovy
+++ web/grails-app/taglib/com/modulus/uno/FilterTagLib.groovy
@@ -7,24 +7,32 @@ class FilterTagLib {
   def showFilters = { attrs, body ->
     out << """
       <div class="row">
-        <g:form controller=\"${attrs.controller}\" action=\"${attrs.action}\">
+        <form action=\"/${attrs.controller}/${attrs.action}\">
    """
     Integer noFilters = attrs.filters.size()<=4 ? attrs.filters.size() : 4
     Integer width = 10 / noFilters
     attrs.filters.eachWithIndex { filter, index ->
+      String value = attrs.filterValues ? attrs.filterValues."${filter}" : ""
       out << """
         <div class="col-md-${width}">
           <label>${attrs.labels[index]}</label>
-          <input class="form-control" name="${filter}" type="text"/>
+          <input class="form-control" name="${filter}" type="text" value="${value}"/>
         </div>
       """
     }
     out << """
       <div class="col-md-2 text-right">
-        <br/>
         <button class="btn btn-primary" type="submit">Buscar</button>
+      """
+      if (attrs.filterValues) {
+        out << """
+          <br/><br/>
+          <a href=\"/${attrs.controller}/${attrs.viewAll}\" class="btn btn-primary">Ver todo</a>
+        """
+      }
+      out << """
       </div>
-    </g:form>
+    </form>
     </div>
     <hr/>
     <br/>
