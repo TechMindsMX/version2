87d3acba37c19abe29abb16c48fd6cd082b34863
Brandon Vergara <brandon@makingdevs.com>
2017-12-21 14:06:14 -0600

    #303  adding boolean to know if has quotation contract

--- m1core/grails-app/domain/com/modulus/uno/Corporate.groovy
+++ m1core/grails-app/domain/com/modulus/uno/Corporate.groovy
@@ -4,11 +4,13 @@ class Corporate {
 
   String nameCorporate
   String corporateUrl
+  Boolean hasQuotationContract
 
   static  hasMany =[companies:Company,users: User]
 
   static constraints = {
     nameCorporate nullable:false, unique:true
     corporateUrl nullable:false, unique:true
+    hasQuotationContract nullable:false
   }
 }
