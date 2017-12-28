6ee6e52735efc189bade3aeb38ca7fdcca5c09dc
LuisSas <luis@makingdevs.com>
2017-12-28 16:46:27 -0600

    #161 Mandando llamar el metodo del servicio que cambia el status del corporativo pasandoles el corporativo

--- web/grails-app/controllers/com/modulus/uno/CorporateController.groovy
+++ web/grails-app/controllers/com/modulus/uno/CorporateController.groovy
@@ -191,6 +191,11 @@ class CorporateController {
     redirect action:'defineCostCenters', id:corporate.id
   }
 
+  def enableOrDisableCorporate(Corporate corporate){
+    def corporateToEnableOrDisable = corporateService.getCorporateToEnableOrDisable(corporate)
+    redirect action:'index'
+  }
+
   def editUser(User user) {
     render view:"editUser", model:[user:user, corporateId:params.corporateId]
   }
