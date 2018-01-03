969952a784b9629d545de3500906fbae892ef224
Temoc Palma <temopalmag@gmail.com>
2018-01-03 17:13:11 -0600

    #524 Generar changelog para agregar campos faltantes de salario asimilable

--- web/grails-app/migrations/changelog.groovy
+++ web/grails-app/migrations/changelog.groovy
@@ -96,4 +96,5 @@ databaseChangeLog = {
   include file: 'updating-column-paymentMethod-quotationPaymnetRequest.groovy'
     include file: 'modify-column-netpayment-to-crudepayment-into-dataimssemployee.groovy'
     include file: 'rename-column-netpayment-to-crudepayment-into-prepaysheetemployee.groovy'
+    include file: 'adding-columns-incometaxassimilable-and-crudeassimilable-to-paysheetemployee.groovy'
 }
