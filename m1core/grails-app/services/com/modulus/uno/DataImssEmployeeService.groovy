20fda777922dcf188dcd28df8647c117461e6a62
Temoc Palma <temopalmag@gmail.com>
2018-01-17 14:14:16 -0600

    #531 Ajustar servicio que registra los datos de imss de un empleado importado

--- m1core/grails-app/services/com/modulus/uno/DataImssEmployeeService.groovy
+++ m1core/grails-app/services/com/modulus/uno/DataImssEmployeeService.groovy
@@ -18,7 +18,7 @@ class DataImssEmployeeService {
       nss:rowEmployee.NSS,
       registrationDate:Date.parse("dd-MM-yyyy", parseRegistrationDateFromRowEmployeeToString(rowEmployee.FECHA_ALTA)),
       baseImssMonthlySalary:new BigDecimal(rowEmployee.SA_BRUTO),
-      crudeMonthlySalary:new BigDecimal(rowEmployee.SA_BRUTO)+new BigDecimal(rowEmployee.IAS),
+      totalMonthlySalary:new BigDecimal(rowEmployee.SA_BRUTO)+new BigDecimal(rowEmployee.IAS),
       holidayBonusRate:new BigDecimal(rowEmployee.PRIMA_VAC),
       annualBonusDays:new Double(rowEmployee.DIAS_AGUINALDO.toString()).intValue(),
       paymentPeriod:paymentPeriod
