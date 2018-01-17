63612444f911f11809b95ae161833f25c0cd7ac7
Temoc Palma <temopalmag@gmail.com>
2018-01-17 14:21:31 -0600

    #531 Renombrar variables de salario crudo a salario total

--- web/grails-app/assets/javascripts/dataImssEmployee/salaries.js
+++ web/grails-app/assets/javascripts/dataImssEmployee/salaries.js
@@ -1,33 +1,33 @@
 $("#saSalary").on('blur',function(){
-  calculateCrudeSalary();
+  calculateTotalSalary();
 });
 
 $("#iasSalary").on('blur',function(){
-  calculateCrudeSalary();
+  calculateTotalSalary();
 });
 
-$("#crudeSalary").on('blur',function(){
+$("#totalSalary").on('blur',function(){
   calculateIasSalary();
 });
 
-function calculateCrudeSalary() {
-  $('#crudeSalary').val('');
+function calculateTotalSalary() {
+  $('#totalSalary').val('');
   var sa = $("#saSalary").val()*1;
   var ias = $("#iasSalary").val()*1;
   if (sa==="" || ias==="" || isNaN(sa) || isNaN(ias)) {
       return
   }
-  var crude = sa + ias;
-  $('#crudeSalary').val((crude).toFixed(2));
+  var total = sa + ias;
+  $('#totalSalary').val((total).toFixed(2));
 }
 
 function calculateIasSalary() {
   $('#iasSalary').val('');
   var sa = $("#saSalary").val();
-  var crude = $("#crudeSalary").val();
-  if (sa==="" || crude==="" || isNaN(sa) || isNaN(crude)){
+  var total = $("#totalSalary").val();
+  if (sa==="" || total==="" || isNaN(sa) || isNaN(crude)){
       return
   }
 
-  $('#iasSalary').val((crude-sa).toFixed(2));
+  $('#iasSalary').val((total-sa).toFixed(2));
 }
