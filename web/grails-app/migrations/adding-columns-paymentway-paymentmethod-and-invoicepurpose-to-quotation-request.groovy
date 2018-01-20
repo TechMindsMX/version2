83dd64d58e40d695635576ff739b76239a37c0a3
Temoc Palma <temopalmag@gmail.com>
2018-01-19 19:44:46 -0600

    #533 Generar changelog que agrega las columnas nuevas a solicitud de cotización para facturar con v3.3

---
+++ web/grails-app/migrations/adding-columns-paymentway-paymentmethod-and-invoicepurpose-to-quotation-request.groovy
@@ -0,0 +1,35 @@
+databaseChangeLog = {
+
+    changeSet(author: "temoc (generated)", id: "1516404863711-1") {
+        addColumn(tableName: "quotation_request") {
+            column(name: "invoice_purpose", type: "varchar(255)") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+
+    changeSet(author: "temoc (generated)", id: "1516404863711-3") {
+        addColumn(tableName: "quotation_request") {
+            column(name: "payment_method", type: "varchar(255)") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+
+    changeSet(author: "temoc (generated)", id: "1516404863711-4") {
+        addColumn(tableName: "quotation_request") {
+            column(name: "payment_way", type: "varchar(255)") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+
+  changeSet(author: "temoc (manual)", id: "initializing-values-in-new-columns-for-existing-records-into-quotation-request") {
+    grailsChange {
+      change {
+        sql.execute("update quotation_request set invoice_purpose='P01', payment_method='PUE', payment_way='TRANSFERENCIA_ELECTRONICA'")
+      }
+    }
+  }
+
+}
