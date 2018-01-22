d3e25b954a53944bc64ffb291cc1bd558f8b6f67
Temoc Palma <temopalmag@gmail.com>
2018-01-22 17:14:47 -0600

    #533 Ajustar changelog para permitir nulos en los campos nuevos de solicitud de cotización

--- web/grails-app/migrations/adding-columns-paymentway-paymentmethod-and-invoicepurpose-to-quotation-request.groovy
+++ web/grails-app/migrations/adding-columns-paymentway-paymentmethod-and-invoicepurpose-to-quotation-request.groovy
@@ -3,7 +3,7 @@ databaseChangeLog = {
     changeSet(author: "temoc (generated)", id: "1516404863711-1") {
         addColumn(tableName: "quotation_request") {
             column(name: "invoice_purpose", type: "varchar(255)") {
-                constraints(nullable: "false")
+                constraints(nullable: "true")
             }
         }
     }
@@ -11,7 +11,7 @@ databaseChangeLog = {
     changeSet(author: "temoc (generated)", id: "1516404863711-3") {
         addColumn(tableName: "quotation_request") {
             column(name: "payment_method", type: "varchar(255)") {
-                constraints(nullable: "false")
+                constraints(nullable: "true")
             }
         }
     }
@@ -19,7 +19,7 @@ databaseChangeLog = {
     changeSet(author: "temoc (generated)", id: "1516404863711-4") {
         addColumn(tableName: "quotation_request") {
             column(name: "payment_way", type: "varchar(255)") {
-                constraints(nullable: "false")
+                constraints(nullable: "true")
             }
         }
     }
@@ -27,7 +27,7 @@ databaseChangeLog = {
   changeSet(author: "temoc (manual)", id: "initializing-values-in-new-columns-for-existing-records-into-quotation-request") {
     grailsChange {
       change {
-        sql.execute("update quotation_request set invoice_purpose='P01', payment_method='PUE', payment_way='TRANSFERENCIA_ELECTRONICA'")
+        sql.execute("update quotation_request set invoice_purpose='P01', payment_method='PUE', payment_way='TRANSFERENCIA_ELECTRONICA' where status='PROCESSED'")
       }
     }
   }
