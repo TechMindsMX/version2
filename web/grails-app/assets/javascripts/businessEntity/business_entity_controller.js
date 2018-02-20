def24e844f22e5887c94333cfee1bc17fa18aa61
Temoc Palma <temopalmag@gmail.com>
2018-02-20 13:29:33 -0600

    #575 Agregar función js para validar radio buttons de tipo de relación al editar una relación comercial

--- web/grails-app/assets/javascripts/businessEntity/business_entity_controller.js
+++ web/grails-app/assets/javascripts/businessEntity/business_entity_controller.js
@@ -6,6 +6,7 @@ var BusinessEntityController = (function(){
 
   var start = function(option) {
     newProviderByInvoice();
+    disabledEntities();
   };
 
   var newProviderByInvoice = function() {
@@ -17,6 +18,25 @@ var BusinessEntityController = (function(){
       $(regimenSelected).prop('selected', true);
       $(regimenSelected).change();
     }
+  };
+
+  var disabledEntities = function() {
+    var entity = $('input[name="clientProviderType"]:checked').val();
+    var existing = $('#id').val();
+    if (typeof existing === "undefined") {
+      return
+    }
+
+      if (entity=='EMPLEADO') {
+        $('input[name="clientProviderType"][value=CLIENTE]').attr("disabled",true);
+        $('input[name="clientProviderType"][value=PROVEEDOR]').attr("disabled",true);
+        $('input[name="clientProviderType"][value=CLIENTE_PROVEEDOR]').attr("disabled",true);
+      } else {
+        $('input[name="clientProviderType"][value=CLIENTE]').attr("disabled",false);
+        $('input[name="clientProviderType"][value=PROVEEDOR]').attr("disabled",false);
+        $('input[name="clientProviderType"][value=CLIENTE_PROVEEDOR]').attr("disabled",false);
+        $('input[name="clientProviderType"][value=EMPLEADO]').attr("disabled",true);
+      }
   };
 
   return {
