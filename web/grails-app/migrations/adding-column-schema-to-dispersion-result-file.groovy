50fa3589d4d2304ace7db6b2153baab3db1abf5a
Temoc Palma <temopalmag@gmail.com>
2018-02-23 16:01:45 -0600

    #420 Cambiar nombre de la columna de esquema de pago

--- web/grails-app/migrations/adding-column-schema-to-dispersion-result-file.groovy
+++ web/grails-app/migrations/adding-column-schema-to-dispersion-result-file.groovy
@@ -2,7 +2,7 @@ databaseChangeLog = {
 
     changeSet(author: "temoc (generated)", id: "1519311923809-1") {
         addColumn(tableName: "dispersion_result_file") {
-            column(name: "schema", type: "varchar(255)") {
+            column(name: "payment_schema", type: "varchar(255)") {
                 constraints(nullable: "false")
             }
         }
