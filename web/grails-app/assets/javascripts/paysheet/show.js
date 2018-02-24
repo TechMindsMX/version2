b3c6c7913784e84fcc8ff0c4044726da85bda50c
Temoc Palma <temopalmag@gmail.com>
2018-02-24 13:50:22 -0600

    #579 Crear función js para validar visibilidad de campo de tipo de resultado para caso de banamex

--- web/grails-app/assets/javascripts/paysheet/show.js
+++ web/grails-app/assets/javascripts/paysheet/show.js
@@ -3,5 +3,19 @@ $( function() {
     dateFormat: 'dd/mm/yy',
     minDate: 0
   });
+  verifyBankNameToDispersionFileResult();
+});
+
+function verifyBankNameToDispersionFileResult() {
+  $("#resultFileTypeDiv").removeAttr('required');
+  $("#resultFileTypeDiv").hide();
+  if ($("#bank option:selected").text() == "BANAMEX") {
+    $("#resultFileTypeDiv").prop('required',true);
+    $("#resultFileTypeDiv").show();
+  }
+}
+
+$( "#bank" ).change(function() {
+  verifyBankNameToDispersionFileResult();
 });
 
