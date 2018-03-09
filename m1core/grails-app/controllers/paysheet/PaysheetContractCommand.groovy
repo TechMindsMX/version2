260c67e7f75d19fca887ffc002f07f134b417c56
Temoc Palma <temopalmag@gmail.com>
2018-03-09 09:53:37 -0600

    #577 Agregar campo de registro patronal al command de contrato de nómina

--- m1core/grails-app/controllers/paysheet/PaysheetContractCommand.groovy
+++ m1core/grails-app/controllers/paysheet/PaysheetContractCommand.groovy
@@ -11,12 +11,14 @@ class PaysheetContractCommand implements Validateable {
   String initDate
   String executiveId
   String companyId
+  String employerRegistration
 
   static constraints = {
     clientId nullable:false
     initDate nullable:false
     executiveId nullable:false
     companyId nullable:false
+    employerRegistration nullable:false, blank:false
   }
 
   PaysheetContract createPaysheetContract() {
@@ -24,7 +26,8 @@ class PaysheetContractCommand implements Validateable {
       client:BusinessEntity.get(this.clientId),
       initDate:Date.parse("dd/MM/yyyy", this.initDate),
       executive:User.get(this.executiveId),
-      company:Company.get(this.companyId)
+      company:Company.get(this.companyId),
+      employerRegistration:this.employerRegistration
     )
   }
 }
