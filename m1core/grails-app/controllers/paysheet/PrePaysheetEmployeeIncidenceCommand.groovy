affc0a45fcee34ed574febb750d50dd15ad3681d
Temoc Palma <temopalmag@gmail.com>
2018-03-22 17:05:50 -0600

    #594 Complementar command de incidencias de empleado con los nuevos campos

--- m1core/grails-app/controllers/paysheet/PrePaysheetEmployeeIncidenceCommand.groovy
+++ m1core/grails-app/controllers/paysheet/PrePaysheetEmployeeIncidenceCommand.groovy
@@ -5,32 +5,61 @@ import grails.validation.Validateable
 
 class PrePaysheetEmployeeIncidenceCommand implements Validateable{
 
-  String internalKey
-  String description
-  String keyType
+  String incidence
   String type
   String schema
   String prePaysheetEmployeeId
   String exemptAmount
   String taxedAmount
+  String extraHoursDays
+  String extraHoursType
+  String extraHoursQuantity
+  String extraHoursAmount
 
   static constraints = {
-    description nullable:false
+    incidence nullable:false
+    schema nullable:false
     exemptAmount nullable:false
     taxedAmount nullable:false
+    extraHoursDays (nullable:true, validator: { val, obj ->
+      (obj.incidence != PerceptionType.P019.toString() || val) ? true : false
+    })
+    extraHoursType (nullable:true, validator: { val, obj ->
+      (obj.incidence != PerceptionType.P019.toString() || val) ? true : false
+    })
+    extraHoursQuantity (nullable:true, validator: { val, obj ->
+      (obj.incidence != PerceptionType.P019.toString() || val) ? true : false
+    })
+    extraHoursAmount (nullable:true, validator: { val, obj ->
+      (obj.incidence != PerceptionType.P019.toString() || val) ? true : false
+    })
   }
 
   public PrePaysheetEmployeeIncidence createPrePaysheetEmployeeIncidence() {
     PrePaysheetEmployee prePaysheetEmployee = PrePaysheetEmployee.get(this.prePaysheetEmployeeId)
+    IncidenceType incidenceType = IncidenceType.values().find { it.toString() == this.type }
+    def incidenceObj = incidenceType == IncidenceType.DEDUCTION ? DeductionType.values().find { it.toString() == this.incidence } : PerceptionType.values().find { it.toString() == this.incidence }
+
+    ExtraHourIncidence extraHourIncidence
+    if (incidenceObj == PerceptionType.P019) {
+      extraHourIncidence = new ExtraHourIncidence(
+        days: this.extraHoursDays.toInteger(),
+        type: this.extraHoursType,
+        quantity: this.extraHoursQuantity.toInteger(),
+        amount: getValueInBigDecimal(this.extraHoursAmount)
+      )
+    }
+
     new PrePaysheetEmployeeIncidence(
-        internalKey:this.internalKey,
-        description:this.description,
-        keyType:this.keyType,
-        type:IncidenceType.values().find { it.toString() == this.type },
+        internalKey:incidenceObj.name(),
+        description:incidenceObj.description,
+        keyType:incidenceObj.key,
+        type: incidenceType,
         paymentSchema:PaymentSchema.values().find { it.toString() == this.schema },
-        exemptAmount: getValueInBigDecimal(this.amount),
+        exemptAmount: getValueInBigDecimal(this.exemptAmount),
         taxedAmount: getValueInBigDecimal(this.taxedAmount),
-        prePaysheetEmployee: prePaysheetEmployee
+        prePaysheetEmployee: prePaysheetEmployee,
+        extraHourIncidence: extraHourIncidence
     )
   }
 
