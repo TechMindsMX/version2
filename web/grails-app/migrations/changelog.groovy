dbf96c2f0489e33bbdcac8f0f6178d81e73caad8
Temoc Palma <temopalmag@gmail.com>
2018-03-22 12:38:15 -0600

    #594 Generar changelog para agregar las nuevas columnas de incidencias de empleado

--- web/grails-app/migrations/changelog.groovy
+++ web/grails-app/migrations/changelog.groovy
@@ -129,4 +129,5 @@ databaseChangeLog = {
   include file: 'adding-columns-for-paysheet-receipt-into-data-imss-employee.groovy'
   include file: 'adding-column-federal-entity-to-paysheet-project.groovy'
     include file: 'adding-column-branch-to-pre-paysheet-employee.groovy'
+    include file: 'adding-columns-key-keyType-taxed-amount-to-pre-paysheet-employee-incidence.groovy'
 }
