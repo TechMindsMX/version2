05533178f3750446aa87600a21ae23373e5b9337
Temoc Palma <temopalmag@gmail.com>
2018-03-22 12:35:39 -0600

    #594 Agregar campos adicionales para el recibo de nómina a las incidencias

--- m1core/grails-app/domain/com/modulus/uno/paysheet/PrePaysheetEmployeeIncidence.groovy
+++ m1core/grails-app/domain/com/modulus/uno/paysheet/PrePaysheetEmployeeIncidence.groovy
@@ -2,16 +2,20 @@ package com.modulus.uno.paysheet
 
 class PrePaysheetEmployeeIncidence {
 
+  String internalKey
   String description
+  String keyType
   IncidenceType type = IncidenceType.PERCEPTION
   PaymentSchema paymentSchema = PaymentSchema.IMSS
-  BigDecimal amount = new BigDecimal(0)
+  BigDecimal exemptAmount = new BigDecimal(0)
+  BigDecimal taxedAmount = new BigDecimal(0)
 
   static belongsTo = [prePaysheetEmployee:PrePaysheetEmployee]
 
   static constraints = {
     description nullable:false
-    amount nullable:false
+    exemptAmount nullable:false
+    taxedAmount nullable:false
   }
   
 }
