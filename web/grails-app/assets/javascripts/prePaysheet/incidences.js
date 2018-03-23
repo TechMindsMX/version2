34ddc3a6ebb2f6d7ba5165938cc1c1da3915467f
Temoc Palma <temopalmag@gmail.com>
2018-03-23 10:48:42 -0600

    #594 Ajustar validaciones de visualización de campos de horas extras y refactorización

--- web/grails-app/assets/javascripts/prePaysheet/incidences.js
+++ web/grails-app/assets/javascripts/prePaysheet/incidences.js
@@ -1,8 +1,12 @@
 $("#perceptions").hide();
 $("#deductions").hide();
+$("#extraHours").hide();
 
 $("#incidenceType").change( function() {
   $("#incidence").val("");
+  $("#extraHours").hide();
+  $("#perceptions").val("");
+  $("#deductions").val("");
   if (this.value == '') {
     $("#perceptions").hide();
     $("#deductions").hide();
@@ -19,8 +23,45 @@ $("#incidenceType").change( function() {
   }
 });
 
+function setRequiredToExtraHours() {
+  $("#extraHoursDays").attr("required", true);
+  $("#extraHoursType").attr("required", true);
+  $("#extraHoursQuantity").attr("required", true);
+  $("#extraHoursAmount").attr("required", true);
+}
+
+function quiteRequiredToExtraHours() {
+  $("#extraHoursDays").removeAttr("required");
+  $("#extraHoursType").removeAttr("required");
+  $("#extraHoursQuantity").removeAttr("required");
+  $("#extraHoursAmount").removeAttr("required"); 
+}
+
+function hideAmounts() {
+  $("#exemptAmount").hide();
+  $("#taxedAmount").hide();
+  $("#exemptAmount").removeAttr("required");
+  $("#taxedAmount").removeAttr("required");
+}
+
+function showAmounts() {
+  $("#exemptAmount").show();
+  $("#taxedAmount").show();
+  $("#exemptAmount").attr("required", true);
+  $("#taxedAmount").attr("required", true);
+}
+
 $("#perceptions").change( function() {
   $("#incidence").val(this.value);
+  if (this.value == '019 - Horas extra') {
+    $("#extraHours").show();
+    hideAmounts();
+    setRequiredToExtraHours();
+  } else {
+    $("#extraHours").hide();
+    showAmounts();
+    quiteRequiredToExtraHours();
+  }
 });
 
 $("#deductions").change( function() {
