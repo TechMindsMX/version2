0460af266dba65302b43611a7c461033bdc9621f
Temoc Palma <temopalmag@gmail.com>
2018-03-28 00:29:57 -0600

    #598 Corregir imports

--- m1core/src/main/groovy/com/modulus/uno/invoice/paysheetReceipt/PaysheetReceipt.groovy
+++ m1core/src/main/groovy/com/modulus/uno/invoice/paysheetReceipt/PaysheetReceipt.groovy
@@ -1,8 +1,8 @@
 package com.modulus.uno.invoice.paysheetReceipt
 
 import com.modulus.uno.invoice.DatosDeFacturacion
-import com.modulus.uno.Contribuyente
-import com.modulus.uno.Concepto
+import com.modulus.uno.invoice.Contribuyente
+import com.modulus.uno.invoice.Concepto
 
 class PaysheetReceipt {
   DatosDeFacturacion datosDeFacturacion
