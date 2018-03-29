7aba67211ddd4bea5d294e1c98b8959295fd3254
Temoc Palma <temopalmag@gmail.com>
2018-03-29 00:25:57 -0600

    #598 Agregar campos de las horas extras al detalle de nómina

--- m1core/src/main/groovy/com/modulus/uno/invoice/paysheetReceipt/DetalleNomina.groovy
+++ m1core/src/main/groovy/com/modulus/uno/invoice/paysheetReceipt/DetalleNomina.groovy
@@ -3,11 +3,15 @@ package com.modulus.uno.invoice.paysheetReceipt
 class DetalleNomina {
 
   String clave
-    String descripcion
-    String tipo
-    BigDecimal importeExento
-    BigDecimal importeGravado
-    BigDecimal importe
-    BigDecimal subsidio
+  String descripcion
+  String tipo
+  BigDecimal importeExento
+  BigDecimal importeGravado
+  BigDecimal importe
+  BigDecimal subsidio
+  Integer diasHrsExtra
+  String tipoHrsExtra
+  Integer totalHrsExtra
+  BigDecimal importeHrsExtra
 
 }
