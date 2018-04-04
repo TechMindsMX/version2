4a3c609f930fb31cb49d69f2c3ecf23ecb1c339c
Temoc Palma <temopalmag@gmail.com>
2018-04-04 09:34:53 -0500

    #599 Renombrar modelo del recibo de nómina y ubicar en controllers

--- m1core/grails-app/controllers/paysheet/PaysheetReceiptCommand.groovy
+++ m1core/grails-app/controllers/paysheet/PaysheetReceiptCommand.groovy
@@ -1,10 +1,12 @@
-package com.modulus.uno.invoice.paysheetReceipt
+package com.modulus.uno
 
 import com.modulus.uno.invoice.DatosDeFacturacion
 import com.modulus.uno.invoice.Contribuyente
 import com.modulus.uno.invoice.Concepto
+import com.modulus.uno.invoice.paysheetReceipt.Nomina
+import com.modulus.uno.invoice.paysheetReceipt.Empleado
 
-class PaysheetReceipt {
+class PaysheetReceiptCommand implements MessageCommand {
   DatosDeFacturacion datosDeFacturacion
   Contribuyente emisor
   Empleado receptor
