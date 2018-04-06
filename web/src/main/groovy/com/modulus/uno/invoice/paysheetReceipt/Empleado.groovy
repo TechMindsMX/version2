251c8ad97fed3fb887bb724e3f06feceb9363c96
Temoc Palma <temopalmag@gmail.com>
2018-04-06 16:09:11 -0500

    #601 Agregar los modelos del recibo de nómina al proyecto web

---
+++ web/src/main/groovy/com/modulus/uno/invoice/paysheetReceipt/Empleado.groovy
@@ -0,0 +1,11 @@
+package com.modulus.uno.invoice.paysheetReceipt
+
+class Empleado {
+
+  String rfc
+  String nombre
+  String curp
+  DatosBancarios datosBancarios
+  DatosLaborales datosLaborales
+
+}
---
+++ web/src/main/groovy/com/modulus/uno/invoice/paysheetReceipt/Percepciones.groovy
@@ -0,0 +1,7 @@
+package com.modulus.uno.invoice.paysheetReceipt
+
+class Percepciones {
+
+  List<DetalleNomina> detalles
+  
+}
