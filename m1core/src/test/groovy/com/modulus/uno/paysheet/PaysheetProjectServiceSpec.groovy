14ab73c89312b6fc60abefa1e609bb1ffe24929a
Temoc Palma <temopalmag@gmail.com>
2018-04-25 10:43:30 -0500

    #605 Agregar test del método que crea el usuario de un empleado

--- m1core/src/test/groovy/com/modulus/uno/paysheet/PaysheetProjectServiceSpec.groovy
+++ m1core/src/test/groovy/com/modulus/uno/paysheet/PaysheetProjectServiceSpec.groovy
@@ -29,23 +29,42 @@ class PaysheetProjectServiceSpec extends Specification {
   void "Should create users for employees from paysheet project"() {
     given:"The paysheet project"
       Company company = new Company(rfc:"COMPANY").save(validate:false)
-      PaysheetContract paysheetContract = new PaysheetContract(company:company)
+      PaysheetContract paysheetContract = new PaysheetContract(company:company).save(validate:false)
       PaysheetProject paysheetProject = new PaysheetProject(paysheetContract:paysheetContract).save(validate:false)
     and:"The paysheet project employees"
       BusinessEntity employee1 = new BusinessEntity(rfc:"UNO").save(validate:false)
       employee1.names = [new ComposeName(value:"NUNO", type:NameType.NOMBRE).save(validate:false), new ComposeName(value:"PUNO", type:NameType.APELLIDO_PATERNO).save(validate:false), new ComposeName(value:"MUNO", type:NameType.APELLIDO_MATERNO).save(validate:false)]
       company.businessEntities = [employee1]
+      company.save(validate:false)
       paysheetProject.employees = [employee1]
+      paysheetProject.save(validate:false)
     and:"The employee link"
       EmployeeLink employeeLink = new EmployeeLink(employeeRef:"UNO", company:company).save(validate:false)
     and:
-      corporateService.getCorporateFromCompany(_) >> new Corporate(nameCorporate:"CORPORATE", companies:[company]).save(validate:false)
+      corporateService.getCorporateFromCompany(_) >> new Corporate().save(validate:false)
     when:
       def result = service.generateUsersForEmployeesFromPaysheetProject(paysheetProject)
     then:
       1 * userService.createUserWithoutRole(_, _)
       1 * userService.setAuthorityToUser(_, _)
-      1 * corporateService.getCorporateFromCompany(_)
+  }
+
+  void "Should create user for a paysheet project employee"() {
+    given:"The paysheet project"
+      Company company = new Company(rfc:"COMPANY").save(validate:false)
+      PaysheetContract paysheetContract = new PaysheetContract(company:company).save(validate:false)
+      PaysheetProject paysheetProject = new PaysheetProject(paysheetContract:paysheetContract).save(validate:false)
+    and:"The employee"
+      BusinessEntity employee1 = new BusinessEntity(rfc:"UNO").save(validate:false)
+      employee1.names = [new ComposeName(value:"NUNO", type:NameType.NOMBRE).save(validate:false), new ComposeName(value:"PUNO", type:NameType.APELLIDO_PATERNO).save(validate:false), new ComposeName(value:"MUNO", type:NameType.APELLIDO_MATERNO).save(validate:false)]
+      company.businessEntities = [employee1]
+      company.save(validate:false)
+    and:
+      corporateService.getCorporateFromCompany(_) >> new Corporate().save(validate:false)
+    when:
+      def result = service.createUserForPaysheetProjectEmployee(paysheetProject, employee1)
+    then:
+      result
   }
 
 }
