1422658e221e383753f0179def72474ade03e05d
Temoc Palma <temopalmag@gmail.com>
2018-05-08 14:58:24 -0500

    #626 Crear dominio de nota de crédito

---
+++ m1core/grails-app/domain/com/modulus/uno/saleorder/CreditNote.groovy
@@ -0,0 +1,25 @@
+package com.modulus.uno.saleorder
+
+import com.modulus.uno.PaymentWay
+import com.modulus.uno.PaymentMethod
+import com.modulus.uno.InvoicePurpose
+import com.modulus.uno.status.CreditNoteStatus
+
+class CreditNote {
+
+  CreditNoteStatus status = CreditNoteStatus.CREATED
+  PaymentWay paymentWay = PaymentWay.TRANSFERENCIA_ELECTRONICA
+  PaymentMethod paymentMethod = PaymentMethod.PUE
+  InvoicePurpose invoicePurpose = InvoicePurpose.G02
+  String folio
+
+  Date dateCreated
+  Date lastUpdated
+  
+  static belongsTo = [saleOrder:SaleOrder]
+
+  static constraints = {
+    folio nullable:true
+  }
+
+}
