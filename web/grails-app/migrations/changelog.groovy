d456a532ae032d4b63d8d271b2f6b45c2b376246
Temoc Palma <temopalmag@gmail.com>
2018-05-08 14:59:40 -0500

    #626 Generar changelog que crea la tabla de nota de crédito

--- web/grails-app/migrations/changelog.groovy
+++ web/grails-app/migrations/changelog.groovy
@@ -139,4 +139,5 @@ databaseChangeLog = {
   include file: 'adding-column-paysheet-receipt-uuid-ias-to-paysheet-employee.groovy'
   include file: 'create-domain-user-employee.groovy'
   include file: 'adding-column-email-to-business-entity.groovy'
+    include file: 'create-domain-credit-note.groovy'
 }
