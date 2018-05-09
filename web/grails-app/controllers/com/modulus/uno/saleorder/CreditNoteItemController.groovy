07767a00ef5a43b6876b374ee4be1213c59e4c87
Temoc Palma <temopalmag@gmail.com>
2018-05-09 17:39:12 -0500

    #615 Crear controller de item de nota de crédito con action para guardar un item

---
+++ web/grails-app/controllers/com/modulus/uno/saleorder/CreditNoteItemController.groovy
@@ -0,0 +1,33 @@
+package com.modulus.uno.saleorder
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+import grails.converters.JSON
+
+@Transactional(readOnly = true)
+class CreditNoteItemController {
+
+  static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+  @Transactional
+  def save(CreditNoteItemCommand command) {
+    if (!command) {
+      transactionStatus.setRollbackOnly()
+      notFound()
+      return
+    }
+
+    CreditNoteItem creditNoteItem = command.createCreditNoteItem()
+
+    if (creditNoteItem.hasErrors()) {
+      transactionStatus.setRollbackOnly()
+      redirect controller:"creditNote", action:"show", id:creditNoteItem.creditNote.id, params:[errors:creditNoteItem.errors]
+      return
+    }
+
+    creditNoteItem.save
+
+    redirect controller:"creditNote", action:"show", id:creditNoteItem.creditNote.id
+  }
+
+}
