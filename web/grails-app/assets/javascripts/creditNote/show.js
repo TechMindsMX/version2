044ad1bb3f608d7232714828216bc0b94f00554e
Temoc Palma <temopalmag@gmail.com>
2018-05-10 11:50:32 -0500

    #615 Agregar validaciones contra cantidades originales del item a guardar en una nota de crédito

--- web/grails-app/assets/javascripts/creditNote/show.js
+++ web/grails-app/assets/javascripts/creditNote/show.js
@@ -20,13 +20,14 @@ $('#name').on('change',function(){
       return false;
     } else {
       if(data.sku!==undefined){$('#sku').val(data.sku);}
-      if(data.quantity!==undefined){$('#quantity').val(data.quantity.toFixed(2));}
+      if(data.quantity!==undefined){$('#quantity').val(data.quantity.toFixed(2));$('#originalQuantity').val(data.quantity.toFixed(2));}
       if(data.price!==undefined){$('#price').val(data.price.toFixed(2));$('#originalPrice').val(data.price.toFixed(2));}
       if(data.discount!==undefined){$('#discount').val(data.discount.toFixed(2));}
       if(data.iva!==undefined){$('#iva').val(data.iva.toFixed(2));}
       if(data.ivaRetention!==undefined){$('#ivaRetention').val(data.ivaRetention.toFixed(2));}
       if(data.unitType!==undefined){$('#unitType').val(data.unitType);}
       calculateAmountAndNetPrice()
+      setOriginalNetPriceAndOriginalAmount()
       return true;
     }
   });
@@ -41,51 +42,85 @@ function calculatePriceWithDiscount() {
 }
 
 function calculateAmountAndNetPrice(){
-  $("#amount").val("0")
+  $("#amount").val("0.00")
+  $("#netprice").val("0.00")
   if (isNaN($("#quantity").val()) || isNaN($("#price").val()) || isNaN($("#discount").val()) || isNaN($("#ivaRetention").val()) || isNaN($("#iva").val())){
-    $("#amount").val("No válido")
-    $("#netprice").val("No válido")
     return
   }
 
   if (priceIsGreaterThanOriginalPrice()) {
-    $("#amount").val("No válido")
-    $("#netprice").val("No válido")
     $("#price").focus()
     alert("El precio ingresado es mayor al precio original")
     return
   }
 
-  $("#netprice").val((calculatePriceWithDiscount()*(1 + $("#iva").val()/100.00) - $("#ivaRetention").val()).toFixed(2))
-  $("#amount").val(($("#quantity").val()*$("#netprice").val()).toFixed(2))
+  if (quantityIsGreaterThanOriginalQuantity()) {
+    $("#quantity").focus()
+    alert("La cantidad ingresada es mayor a la cantidad original")
+    return
+  }
+
+  var netPrice = (calculatePriceWithDiscount()*(1 + $("#iva").val()/100.00) - $("#ivaRetention").val()).toFixed(2)
+  var amount = ($("#quantity").val()*netPrice).toFixed(2)
+
+  $("#netprice").val(netPrice)
+  $("#amount").val(amount)
 }
 
 function priceIsGreaterThanOriginalPrice() {
   return ($("#price").val()*1) > ($("#originalPrice").val()*1)
 }
 
+function quantityIsGreaterThanOriginalQuantity() {
+  return ($("#quantity").val()*1) > ($("#originalQuantity").val()*1)
+}
+
+function setOriginalNetPriceAndOriginalAmount() {
+  $("#originalNetprice").val($("#netprice").val())
+  $("#originalAmount").val($("#amount").val())
+}
+
 $("#price").change( function() {
-   calculateAmountAndNetPrice()
+    calculateAmountAndNetPrice()
+    verifyAmount()
   }
 )
 
 $("#discount").change( function() {
     calculateAmountAndNetPrice()
+    verifyAmount()
   }
 )
 
 $("#ivaRetention").change( function() {
-   calculateAmountAndNetPrice()
+    calculateAmountAndNetPrice()
+    verifyAmount()
  }
 )
 
 $("#iva").change( function() {
-   calculateAmountAndNetPrice()
+    calculateAmountAndNetPrice()
+    verifyAmount()
   }
 )
 
 $("#quantity").change( function() {
-   calculateAmountAndNetPrice()
+    calculateAmountAndNetPrice()
+    verifyAmount()
   }
 )
 
+function verifyAmount() {
+  if (($("#amount").val()*1) > ($("#originalAmount").val()*1)) {
+    $("#amount").val("0.00")
+    alert("El importe del concepto no puede ser mayor al importe original ("+$("#originalAmount").val()+")")
+  }
+}
+
+$("form").on("submit", function() {
+  if ($("#amount").val() == "0.00") {
+    alert("El importe del concepto no puede ser cero, verifique los datos")
+    return false
+  }
+  return true
+} )
