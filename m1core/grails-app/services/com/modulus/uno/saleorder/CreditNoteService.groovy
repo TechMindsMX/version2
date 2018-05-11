c78743da24861ee50d0858e69748fba636916520
Temoc Palma <temopalmag@gmail.com>
2018-05-11 10:42:07 -0500

    #616 Crear método para enviar a autorización una nota de crédito

--- m1core/grails-app/services/com/modulus/uno/saleorder/CreditNoteService.groovy
+++ m1core/grails-app/services/com/modulus/uno/saleorder/CreditNoteService.groovy
@@ -1,12 +1,25 @@
 package com.modulus.uno.saleorder
 
 import grails.transaction.Transactional
+import com.modulus.uno.EmailSenderService
+import com.modulus.uno.status.CreditNoteStatus
 
 class CreditNoteService {
   
+  def springSecurityService
+  EmailSenderService emailSenderService
+
   @Transactional
   CreditNote saveCreditNote(CreditNote creditNote) {
     creditNote.save()
+    creditNote
+  }
+
+  @Transactional
+  CreditNote sendToAuthorize(CreditNote creditNote) {
+    creditNote.status = CreditNoteStatus.TO_AUTHORIZE
+    creditNote.save()
+    //emailSenderService.notifyCreditNoteChangeStatus(creditNote)
     creditNote
   }
 
