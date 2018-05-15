be68fe58a5b0709be88f70c20f08c938c16b810c
Temoc Palma <temopalmag@gmail.com>
2018-05-15 10:25:58 -0500

    #617 Crear métodos de servicio para procesar la autorización de una nota de crédito

--- m1core/grails-app/services/com/modulus/uno/saleorder/CreditNoteService.groovy
+++ m1core/grails-app/services/com/modulus/uno/saleorder/CreditNoteService.groovy
@@ -1,6 +1,7 @@
 package com.modulus.uno.saleorder
 
 import grails.transaction.Transactional
+import com.modulus.uno.Authorization
 import com.modulus.uno.EmailSenderService
 import com.modulus.uno.status.CreditNoteStatus
 
@@ -18,6 +19,35 @@ class CreditNoteService {
   @Transactional
   CreditNote sendToAuthorize(CreditNote creditNote) {
     creditNote.status = CreditNoteStatus.TO_AUTHORIZE
+    creditNote.save()
+    emailSenderService.notifyCreditNoteChangeStatus(creditNote)
+    creditNote
+  }
+
+  @Transactional
+  CreditNote processAuthorization(CreditNote creditNote) {
+    addAuthorizationToCreditNote(creditNote)
+    if (isFullAuthorized(creditNote)) {
+      authorizeCreditNote(creditNote)
+    }
+    creditNote
+  }
+
+  @Transactional
+  def addAuthorizationToCreditNote(CreditNote creditNote) {
+    Authorization authorization = new Authorization(user:springSecurityService.currentUser).save()
+    creditNote.addToAuthorizations(authorization)
+    creditNote.save()
+  }
+
+  Boolean isFullAuthorized (CreditNote creditNote) {
+    def alreadyAuthorizations = creditNote.authorizations ? creditNote.authorizations.size() : 0
+    alreadyAuthorizations >= creditNote.saleOrder.company.numberOfAuthorizations
+  }
+
+  @Transactional
+  CreditNote authorizeCreditNote(CreditNote creditNote) {
+    creditNote.status = CreditNoteStatus.AUTHORIZED
     creditNote.save()
     emailSenderService.notifyCreditNoteChangeStatus(creditNote)
     creditNote
