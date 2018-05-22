4166043706f0e35fb8ecd4bc1b77e1d9f2ad7f36
Temoc Palma <temopalmag@gmail.com>
2018-05-22 15:39:43 -0500

    #636 Refactor de enum de razones de rechazo

--- web/src/main/groovy/com/modulus/uno/RejectReason.groovy
+++ web/src/main/groovy/com/modulus/uno/RejectReason.groovy
@@ -1,5 +1,20 @@
 package com.modulus.uno
 
 enum RejectReason {
-  DOCUMENTO_INVALIDO, CANTIDAD_INCORRECTA, OTRO
+  DOCUMENTO_INVALIDO ("DOCUMENTO INVÁLIDO"), CANTIDAD_INCORRECTA ("CANTIDAD INCORRECTA"), OTRO ("OTRO")
+
+  private final String description
+  private final String name
+
+  RejectReason(String descrip) {
+    this.description = descrip
+  }
+
+  String getDescription() {
+    this.description
+  }
+
+  String getName() {
+    this.name()
+  }
 }
