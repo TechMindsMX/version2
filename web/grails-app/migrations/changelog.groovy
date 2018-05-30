160dcd50f0a870037ddc60e73331c2c9474fea0e
Temoc Palma <temopalmag@gmail.com>
2018-05-30 11:50:26 -0500

    #638 Descomentar changeset

--- web/grails-app/migrations/changelog.groovy
+++ web/grails-app/migrations/changelog.groovy
@@ -1,7 +1,7 @@
 databaseChangeLog = {
   include file: 'initial-structure.groovy'
   include file: 'create-m1-roles.groovy'
-  //include file: 'adding-user-admin-production.groovy'
+  include file: 'adding-user-admin-production.groovy'
   include file: 'adding-list-banks.groovy'
   include file: 'adding-colunm-reference-to-payment.groovy'
   include file: 'adding-uuid-to-purchase-order.groovy'
