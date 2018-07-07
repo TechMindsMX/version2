79650b99ca4df583d2776e1311368d6506771368
Temoc Palma <temopalmag@gmail.com>
2018-07-06 19:02:43 -0500

    #656 Generar changelogs que agregan las columnas nuevas para la autenticación de dos factores

--- web/grails-app/migrations/changelog.groovy
+++ web/grails-app/migrations/changelog.groovy
@@ -147,4 +147,5 @@ databaseChangeLog = {
   include file: 'adding-columns-invoice-serie-and-invoice-folio-to-credit-note.groovy'
   include file: 'modify-size-column-name-into-sale-order-item-and-product.groovy'
   include file: 'adding-columns-reject-reason-and-comments-to-credit-note.groovy'
+    include file: 'adding-columns-key-and-enabled-2FA-into-user.groovy'
 }
