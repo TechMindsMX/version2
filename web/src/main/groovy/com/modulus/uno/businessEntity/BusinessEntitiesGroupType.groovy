3030b384a0770899ff37a1887c6ab0123cb48909
Temoc Palma <temopalmag@gmail.com>
2018-07-19 15:31:06 -0500

    #660 Crear enum de tipos de grupo de relaciones comerciales

---
+++ web/src/main/groovy/com/modulus/uno/businessEntity/BusinessEntitiesGroupType.groovy
@@ -0,0 +1,15 @@
+package com.modulus.uno.businessEntity
+
+enum BusinessEntitiesGroupType {
+  CLIENTS("CLIENTES"), PROVIDERS("PROVEEDORES"), EMPLOYEES("EMPLEADOS")
+
+  private final String description
+
+  BusinessEntitiesGroupType(String description) {
+    this.description = description
+  }
+
+  String getDescription() {
+    this.description
+  }
+}
