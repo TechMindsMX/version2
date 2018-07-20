39631e51e02285220b0698728ec4896301522b21
Temoc Palma <temopalmag@gmail.com>
2018-07-20 15:12:08 -0500

    #660 Crear action para nuevo grupo de relaciones comerciales

--- web/grails-app/controllers/com/modulus/uno/businessEntity/BusinessEntitiesGroupController.groovy
+++ web/grails-app/controllers/com/modulus/uno/businessEntity/BusinessEntitiesGroupController.groovy
@@ -3,6 +3,8 @@ package com.modulus.uno.businessEntity
 import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
 
+import com.modulus.uno.Corporate
+
 @Transactional(readOnly = true)
 class BusinessEntitiesGroupController {
 
@@ -12,6 +14,11 @@ class BusinessEntitiesGroupController {
     params.max = 25
     params.sort = "description"
     [groupList:BusinessEntitiesGroup.list(params), groupCount:BusinessEntitiesGroup.count()]
+  }
+
+  def create() {
+    Corporate corporate = session.corporate
+    respond new BusinessEntitiesGroup(), model:[companies:corporate.companies.sort{it.bussinessName}]
   }
 
 }
