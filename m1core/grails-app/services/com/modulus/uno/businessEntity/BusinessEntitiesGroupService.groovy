cec6d4b54d56cfb240b42f5cb967913acfda64cb
Temoc Palma <temopalmag@gmail.com>
2018-08-08 12:22:45 -0500

    #663 Crear método que obtiene la lista de clientes de los grupos asociados al usuario

--- m1core/grails-app/services/com/modulus/uno/businessEntity/BusinessEntitiesGroupService.groovy
+++ m1core/grails-app/services/com/modulus/uno/businessEntity/BusinessEntitiesGroupService.groovy
@@ -67,4 +67,16 @@ class BusinessEntitiesGroupService {
     user
   }
 
+  List<BusinessEntity> findBusinessEntitiesByKeyword(User user, Company company, String dataQuery) {
+    List<BusinessEntity> allClientsForUser = []
+    List<BusinessEntitiesGroup> allClientsGroupsForUserInCompany = user.businessEntitiesGroups.findAll { group -> group.company == company && group.type == BusinessEntitiesGroupType.CLIENTS }.toList()
+    allClientsGroupsForUserInCompany.each { group ->
+      println "Business Entities group: ${group.id} - ${group.businessEntities}"
+      allClientsForUser.addAll(group.businessEntities.toList())
+    }
+    println "All clients for user: ${allClientsForUser}"
+    List<BusinessEntity> clients = dataQuery ? allClientsForUser.findAll { client -> client.rfc.contains(dataQuery) || client.toString().contains(dataQuery) } : allClientsForUser
+    clients
+  }
+
 }
