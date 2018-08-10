5679979678432e3ad2508ffd81566b772f454b3b
Temoc Palma <temopalmag@gmail.com>
2018-08-10 13:58:34 -0500

    #664 Crear método que obtiene la lista de órdenes de ventas considerando los grupos asociados al usuario

--- m1core/grails-app/services/com/modulus/uno/saleorder/SaleOrderService.groovy
+++ m1core/grails-app/services/com/modulus/uno/saleorder/SaleOrderService.groovy
@@ -20,6 +20,7 @@ import com.modulus.uno.Address
 import com.modulus.uno.Authorization
 import com.modulus.uno.Conciliation
 import com.modulus.uno.User
+import com.modulus.uno.businessEntity.BusinessEntitiesGroup
 
 import com.modulus.uno.PaymentWay
 import com.modulus.uno.Period
@@ -183,12 +184,26 @@ class SaleOrderService {
     saleOrderStatuses
   }
 
+  Map findSaleOrdersForCurrentUser(Company company, def statusOrders, def params) {
+    Map saleOrders = [:]
+    User currentUser = springSecurityService.currentUser
+    List<BusinessEntitiesGroup> clientsGroupsForUser = businessEntitiesGroupService.findClientsGroupsForUserInCompany(currentUser, company)
+    if (clientsGroups) {
+      List<BusinessEntity> userClients = businessEntitiesGroupService.getAllClientsFromUserGroups(clientsGroups)
+      saleOrders.list = SaleOrder.findAllByCompanyAndStatusInListAndRfcInList(company, statusOrders, userClients.rfc, params)
+      saleOrders.items = SaleOrder.countByCompanyAndStatusInListAndRfcInList(company, statusOrders, userClients.rfc)
+    } else {
+      saleOrders.list = SaleOrder.findAllByCompanyAndStatusInList(company, statusOrders, params)
+      saleOrders.items = SaleOrder.countByCompanyAndStatusInList(company, statusOrders)
+    }
+    saleOrders
+  }
+
   def getSaleOrdersToList(Long company, params){
     def statusOrders = getSaleOrderStatus(params.status)
     def saleOrders = [:]
     if(company){
-      saleOrders.list = SaleOrder.findAllByCompanyAndStatusInList(Company.get(company), statusOrders, params)
-      saleOrders.items = SaleOrder.countByCompanyAndStatusInList(Company.get(company), statusOrders)
+      saleOrders = findSaleOrdersForCurrentUser(Company.get(company), statusOrders, params)
     } else{
       saleOrders.list = SaleOrder.findAllByStatusInList(statusOrders, params)
       saleOrders.items = SaleOrder.countByStatusInList(statusOrders)
