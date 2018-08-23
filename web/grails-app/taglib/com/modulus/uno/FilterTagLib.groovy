f6b5ccabe8429a3c00df4482d55da33f98ef58cb
Temoc Palma <temopalmag@gmail.com>
2018-08-23 16:43:43 -0500

    #643 Agregar nombre al form de filtros y modal para mostrar mensajes

--- web/grails-app/taglib/com/modulus/uno/FilterTagLib.groovy
+++ web/grails-app/taglib/com/modulus/uno/FilterTagLib.groovy
@@ -7,7 +7,7 @@ class FilterTagLib {
   def showFilters = { attrs, body ->
     out << """
       <div class="row">
-        <form action=\"/${attrs.controller}/${attrs.action}\">
+        <form id=\"formFilters\" action=\"/${attrs.controller}/${attrs.action}\">
    """
     Integer noFilters = attrs.filters.size()<=4 ? attrs.filters.size() : 4
     Integer width = 10 / noFilters
@@ -33,6 +33,23 @@ class FilterTagLib {
       out << """
       </div>
     </form>
+      <div id="modalAlert" class="modal fade" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title">ModulusUno</h4>
+            </div>
+            <div class="modal-body">
+              <p id="messageAlert"></p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
     <hr/>
     <br/>
