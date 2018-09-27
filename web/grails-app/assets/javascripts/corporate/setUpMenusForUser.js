ede75a1eeb2f57548230f934a3c96f7cd34a9948
Temoc Palma <temopalmag@gmail.com>
2018-09-27 17:06:04 -0500

    #682 Agregar las filas con los menús obtenidos y crear función que valida los checkboxs de menús principales

--- web/grails-app/assets/javascripts/corporate/setUpMenusForUser.js
+++ web/grails-app/assets/javascripts/corporate/setUpMenusForUser.js
@@ -21,11 +21,10 @@ $("#roleId").change( function() {
     if(data.error!==undefined){
       return false;
     } else {
-      console.log("Data: " + JSON.stringify(data));
       $.each(data, function(k,v) {
-        $("#listMenus").append("<tr><td colspan='2' class='col-md-10'>" + v.name + "</td><td class='col-md-2 text-center'><input class='menu" + v.id + "' type='checkBox' name='menuId" + v.id + "'/></td></tr>");
+        $("#listMenus").append("<tr><td colspan='2' class='col-md-10'>" + v.name + "</td><td class='col-md-2 text-center'><input id='chkMenu_" + v.id + "' type='checkBox' name='menuId" + v.id + "'/></td></tr>");
         $.each(v.menus, function(sk, sv) {
-          $("#listMenus").append("<tr><td class='col-md-1 text-center'></td><td class='col-md-9'>" + sv.name + "</td><td class='col-md-2 text-center'><input class='submenu" + v.id + "' type='checkBox' name='menuId" + sv.id + "'/></td></tr>");
+          $("#listMenus").append("<tr><td class='col-md-1 text-center'></td><td class='col-md-9'>" + sv.name + "</td><td class='col-md-2 text-center'><input id=chkSubMenu_'" + v.id + "' class='submenu_" + v.id + "' type='checkBox' name='menuId" + sv.id + "'/></td></tr>");
         });
       });
       return true;
@@ -36,4 +35,20 @@ $("#roleId").change( function() {
   consulta.fail(function(){
     return false;
   });
+});
+
+$(document).on("click", "input[id^=chkMenu_]", function(event) {
+  var targetId = event.target.id;
+  var menuId = targetId.split("_")[1];
+  var submenus = $("input.submenu_"+menuId+"[type=checkbox]");
+  $.each(submenus, function(k, subm) {
+    $(subm).prop("checked", event.target.checked);
+  });
+});
+
+$(document).on("click", "input[id^=chkSubMenu_]", function(event) {
+  var targetId = event.target.id;
+  var menuId = targetId.split("_")[1];
+  console.log("Menu Id: " + menuId);
+  console.log("Checkbox name: " + event.target.name);
 });
