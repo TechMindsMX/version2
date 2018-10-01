a956e382975eacb50dfd077399edb2e85b3939b9
Temoc Palma <temopalmag@gmail.com>
2018-10-01 12:27:27 -0500

    #682 Crear función que valide los checks de los submenús para que se active/desactive su menú padre automáticamente

--- web/grails-app/assets/javascripts/corporate/setUpMenusForUser.js
+++ web/grails-app/assets/javascripts/corporate/setUpMenusForUser.js
@@ -22,9 +22,9 @@ $("#roleId").change( function() {
       return false;
     } else {
       $.each(data, function(k,v) {
-        $("#listMenus").append("<tr><td colspan='2' class='col-md-10'>" + v.name + "</td><td class='col-md-2 text-center'><input id='chkMenu_" + v.id + "' type='checkBox' name='menuId" + v.id + "'/></td></tr>");
+        $("#listMenus").append("<tr><td colspan='2' class='col-md-10'><strong>" + v.name.toUpperCase() + "</strong></td><td class='col-md-2 text-center'><input id='chkMenu_" + v.id + "' type='checkBox' name='menuId" + v.id + "'/></td></tr>");
         $.each(v.menus, function(sk, sv) {
-          $("#listMenus").append("<tr><td class='col-md-1 text-center'></td><td class='col-md-9'>" + sv.name + "</td><td class='col-md-2 text-center'><input id=chkSubMenu_'" + v.id + "' class='submenu_" + v.id + "' type='checkBox' name='menuId" + sv.id + "'/></td></tr>");
+          $("#listMenus").append("<tr><td class='col-md-1 text-center'></td><td class='col-md-9'>" + sv.name + "</td><td class='col-md-2 text-center'><input id='chkSubMenu_" + v.id + "' class='submenu_" + v.id + "' type='checkBox' name='menuId" + sv.id + "'/></td></tr>");
         });
       });
       return true;
@@ -50,5 +50,19 @@ $(document).on("click", "input[id^=chkSubMenu_]", function(event) {
   var targetId = event.target.id;
   var menuId = targetId.split("_")[1];
   console.log("Menu Id: " + menuId);
-  console.log("Checkbox name: " + event.target.name);
+  var submenus = $("input.submenu_"+menuId+"[type=checkbox]");
+  var parentMenu = $("#chkMenu_"+menuId);
+  if (event.target.checked) {
+    $(parentMenu).prop("checked", true);
+  } else {
+    var nothingChecked = true;
+    $.each(submenus, function(k, subm) {
+      if ($(subm).prop("checked")) {
+        nothingChecked = false;
+      }
+    });
+    if (nothingChecked) {
+      $(parentMenu).prop("checked", false);
+    }
+  }
 });
