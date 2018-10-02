8dff0e8eed2f124e22bef4ae5b97515b6c10550d
Temoc Palma <temopalmag@gmail.com>
2018-10-02 17:10:46 -0500

    #682 Crear servicio para obtener los menús asignados a un usuario en el rol y empresa indicados

--- m1core/grails-app/services/com/modulus/uno/menu/MenuOperationsService.groovy
+++ m1core/grails-app/services/com/modulus/uno/menu/MenuOperationsService.groovy
@@ -2,6 +2,9 @@ package com.modulus.uno.menu
 
 import grails.transaction.Transactional
 import com.modulus.uno.Role
+import com.modulus.uno.User
+import com.modulus.uno.Company
+import com.modulus.uno.UserRoleCompanyMenu
 
 @Transactional
 class MenuOperationsService {
@@ -16,5 +19,10 @@ class MenuOperationsService {
     def menuLinks = MenuLink.findAllWhere(menu:menu)
 		menuLinks.each { it.delete() }
 	}
+
+  def getMenusForUser(User user, Company company) {
+    def allMenus = UserRoleCompanyMenu.findAllByUserAndCompany(user, company)?.menus.toList().flatten()
+    allMenus?.unique { a, b -> a.id <=> b.id }
+  }
 
 }
