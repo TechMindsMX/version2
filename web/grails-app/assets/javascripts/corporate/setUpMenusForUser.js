41ca619bdfcea9eb4e9fb7090df37a16d58259e0
Temoc Palma <temopalmag@gmail.com>
2018-10-02 10:22:10 -0500

    #682 Ajustar función js que lista los menús del rol seleccionado con check activado para los menús asociados al usuario

--- web/grails-app/assets/javascripts/corporate/setUpMenusForUser.js
+++ web/grails-app/assets/javascripts/corporate/setUpMenusForUser.js
@@ -2,6 +2,8 @@ $("#divListMenus").hide();
 
 $("#roleId").change( function() {
   var theRole = $("#roleId").val()
+  var theCompany = $("#companyId").val()
+  var theUser = $("#userId").val()
   $("#listMenus").empty();
   if ($("#roleId").val() == "") {
     $("#divListMenus").hide();
@@ -12,7 +14,7 @@ $("#roleId").change( function() {
   var consulta = $.ajax({
     type:'GET',
     url:'../getMenusForRole',
-    data:{role:theRole},
+    data:{role:theRole, company:theCompany, user:theUser},
     datatype:'JSON'
   });
 
@@ -22,9 +24,17 @@ $("#roleId").change( function() {
       return false;
     } else {
       $.each(data, function(k,v) {
-        $("#listMenus").append("<tr><td colspan='2' class='col-md-10'><strong>" + v.name.toUpperCase() + "</strong></td><td class='col-md-2 text-center'><input id='chkMenu_" + v.id + "' type='checkBox' name='menuId" + v.id + "' value='" + v.id + "'/></td></tr>");
+        if(v.checked) {
+          $("#listMenus").append("<tr><td colspan='2' class='col-md-10'><strong>" + v.name.toUpperCase() + "</strong></td><td class='col-md-2 text-center'><input id='chkMenu_" + v.id + "' type='checkBox' name='menuId" + v.id + "' value='" + v.id + "' checked='true'/></td></tr>");
+        } else {
+          $("#listMenus").append("<tr><td colspan='2' class='col-md-10'><strong>" + v.name.toUpperCase() + "</strong></td><td class='col-md-2 text-center'><input id='chkMenu_" + v.id + "' type='checkBox' name='menuId" + v.id + "' value='" + v.id + "'/></td></tr>");
+        }
         $.each(v.menus, function(sk, sv) {
-          $("#listMenus").append("<tr><td class='col-md-1 text-center'></td><td class='col-md-9'>" + sv.name + "</td><td class='col-md-2 text-center'><input id='chkSubMenu_" + v.id + "' class='submenu_" + v.id + "' type='checkBox' name='menuId" + sv.id + "' value='" + sv.id + "'/></td></tr>");
+          if (sv.checked) {
+            $("#listMenus").append("<tr><td class='col-md-1 text-center'></td><td class='col-md-9'>" + sv.name + "</td><td class='col-md-2 text-center'><input id='chkSubMenu_" + v.id + "' class='submenu_" + v.id + "' type='checkBox' name='menuId" + sv.id + "' value='" + sv.id + "' checked='true'/></td></tr>");
+          } else {
+            $("#listMenus").append("<tr><td class='col-md-1 text-center'></td><td class='col-md-9'>" + sv.name + "</td><td class='col-md-2 text-center'><input id='chkSubMenu_" + v.id + "' class='submenu_" + v.id + "' type='checkBox' name='menuId" + sv.id + "' value='" + sv.id + "'/></td></tr>");
+          }
         });
       });
       return true;
