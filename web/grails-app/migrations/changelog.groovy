764511760d359534a5e3ef623248d6747ce78341
Temoc Palma <temopalmag@gmail.com>
2018-10-03 13:43:58 -0500

    #708 Generar changeset que agrega las columnas de complemento de pago en movimientos bancarios

--- web/grails-app/migrations/changelog.groovy
+++ web/grails-app/migrations/changelog.groovy
@@ -149,4 +149,5 @@ databaseChangeLog = {
   include file: 'adding-columns-reject-reason-and-comments-to-credit-note.groovy'
   include file: 'adding-columns-key-and-enabled-2FA-into-user.groovy'
   include file: 'adding-column-stamped-date-into-sale-order.groovy'
+    include file: 'adding-flag-for-payment-complement-and-payment-complement-uuid-to-banking-transaction.groovy'
 }
