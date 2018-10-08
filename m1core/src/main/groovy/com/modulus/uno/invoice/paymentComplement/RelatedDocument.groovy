ce3dbd2d5832e07f937cbc7e7d0d94b66532b337
Temoc Palma <temopalmag@gmail.com>
2018-10-08 10:14:35 -0500

    #709 Crear command y beans necesarios del complemento de pago

---
+++ m1core/src/main/groovy/com/modulus/uno/invoice/paymentComplement/RelatedDocument.groovy
@@ -0,0 +1,13 @@
+package com.modulus.uno.invoice.paymentComplement
+
+class RelatedDocument {
+  String uuid
+  String serie
+  String folio
+  String currency
+  String paymentWay
+  String partialNumber
+  String beforeAmount
+  String payedAmount
+  String newAmount
+}
