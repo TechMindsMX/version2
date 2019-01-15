77c852b2bdaa54cf8a41725c0dc916b656277d4c
Temoc Palma <temopalmag@gmail.com>
2019-01-15 13:00:31 -0600

    #718 Regenerar changeset de tabla de datos imss de empleado

--- web/grails-app/migrations/adjust-domain-data-imss-employee.groovy
+++ web/grails-app/migrations/adjust-domain-data-imss-employee.groovy
@@ -1,22 +1,6 @@
 databaseChangeLog = {
 
-    changeSet(author: "temoc (generated)", id: "1541785084905-1") {
-        addColumn(tableName: "data_imss_employee") {
-            column(name: "is_only_assimilable", type: "bit") {
-                constraints(nullable: "false")
-            }
-        }
-    }
-
-    changeSet(author: "temoc (generated)", id: "1541785084905-2") {
-        addColumn(tableName: "data_imss_employee") {
-            column(name: "is_variable_assimilable", type: "bit") {
-                constraints(nullable: "false")
-            }
-        }
-    }
-
-    changeSet(author: "temoc (generated)", id: "1541785084905-3") {
+    changeSet(author: "temoc (generated)", id: "1547511925058-1") {
         addColumn(tableName: "data_imss_employee") {
             column(name: "monthly_net_assimilable_salary", type: "decimal(19, 2)") {
                 constraints(nullable: "false")
@@ -24,23 +8,21 @@ databaseChangeLog = {
         }
     }
 
-    changeSet(author: "temoc (generated)", id: "1541785084905-28") {
-        dropNotNullConstraint(columnDataType: "int", columnName: "annual_bonus_days", tableName: "data_imss_employee")
+    changeSet(author: "temoc (generated)", id: "1547511925058-2") {
+        addColumn(tableName: "data_imss_employee") {
+            column(name: "paysheet_schema", type: "varchar(255)")
+        }
     }
 
-    changeSet(author: "temoc (generated)", id: "1541785084905-30") {
-        dropNotNullConstraint(columnDataType: "decimal(5,2)", columnName: "holiday_bonus_rate", tableName: "data_imss_employee")
+    changeSet(author: "temoc (generated)", id: "1547511925058-27") {
+        dropNotNullConstraint(columnDataType: "varchar(255)", columnName: "department", tableName: "data_imss_employee")
     }
 
-    changeSet(author: "temoc (generated)", id: "1541785084905-32") {
-        dropNotNullConstraint(columnDataType: "varchar(255)", columnName: "job_risk", tableName: "data_imss_employee")
+    changeSet(author: "temoc (generated)", id: "1547511925058-30") {
+        dropNotNullConstraint(columnDataType: "varchar(255)", columnName: "job", tableName: "data_imss_employee")
     }
 
-    changeSet(author: "temoc (generated)", id: "1541785084905-35") {
-        dropNotNullConstraint(columnDataType: "varchar(255)", columnName: "nss", tableName: "data_imss_employee")
-    }
-
-    changeSet(author: "temoc (generated)", id: "1541785084905-38") {
-        dropNotNullConstraint(columnDataType: "datetime", columnName: "registration_date", tableName: "data_imss_employee")
+    changeSet(author: "temoc (generated)", id: "1547511925058-35") {
+        dropNotNullConstraint(columnDataType: "varchar(255)", columnName: "work_day_type", tableName: "data_imss_employee")
     }
 }
