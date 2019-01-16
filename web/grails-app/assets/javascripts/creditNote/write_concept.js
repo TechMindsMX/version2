d65e72aa77852259d759ff65153a4ae6e1362990
KarlaJu <karla@makingdevs.com>
2019-01-16 14:37:46 -0600

    #700 Modificación en archivos js para mostrar y ocultar cuadro de texto al agregar producto en la nota de crédito

--- web/grails-app/assets/javascripts/creditNote/write_concept.js
+++ web/grails-app/assets/javascripts/creditNote/write_concept.js
@@ -3,9 +3,11 @@ $("#selectWriteConcept").click( function() {
     $("#selectConcept").hide()
     $("#writeConcept").show()
     $("#sku").attr("readonly", false)
+    $("#unitType").attr("readonly", false)
   } else {
     $("#selectConcept").show()
     $("#writeConcept").hide()
     $("#sku").attr("readonly", true)
+    $("#unitType").attr("readonly", true)
   }
 });
