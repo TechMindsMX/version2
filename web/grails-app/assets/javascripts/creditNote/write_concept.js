694e19d74b5256922792c36a35196bfe303ffac3
KarlaJu <karla@makingdevs.com>
2019-01-24 21:39:12 -0600

    #700 Agregando condiciones para el envío de datos a través del submit y condiciones para el llenado de campos

--- web/grails-app/assets/javascripts/creditNote/write_concept.js
+++ web/grails-app/assets/javascripts/creditNote/write_concept.js
@@ -1,13 +1,51 @@
 $("#selectWriteConcept").click( function() {
+  var select = document.getElementById("name");
+  var input = document.getElementById("selectName");
   if ($("#selectWriteConcept").is(":checked")) {
-    $("#selectConcept").hide()
     $("#writeConcept").show()
+    $("#selectConcept").hide()
     $("#sku").attr("readonly", false)
     $("#unitType").attr("readonly", false)
+    select.id="selectName"
+    select.name ="selectName"
+    input.id = "name"
+    input.name = "name"
   } else {
     $("#selectConcept").show()
     $("#writeConcept").hide()
     $("#sku").attr("readonly", true)
     $("#unitType").attr("readonly", true)
+    select.id="selectName"
+    select.name ="selectName"
+    input.id = "name"
+    input.name = "name"
   }
 });
+
+$("form").on("submit", function() {
+  if ($("#name").val() == "") {
+    alert("No ha llenado el concepto")
+    return false
+  }
+  if ($("#amount").val() == "0.00") {
+    alert("El importe del concepto no puede ser cero, verifique los datos")
+    return false
+  }
+  if ($("#price").val() == "") {
+    alert("No ha llenado el cantidad")
+    return false
+  }
+
+  if ($("#unitType").val() == "") {
+    alert("No ha llenado la medida")
+    return false
+  }
+  return true
+} )
+
+
+$("#btnSubmit").click( function(){
+  if ($("#selectWriteConcept").is(":checked")) {
+    $("#selectConcept").remove()
+  }
+})
