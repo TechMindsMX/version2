049be39ea6f08186856b2bd3ba87169fc6ae0773
Brandon Vergara <brandon@makingdevs.com>
2020-02-07 11:20:06 -0600

    coding 735  adding new router to js

--- web/grails-app/assets/javascripts/application.js
+++ web/grails-app/assets/javascripts/application.js
@@ -1,6 +1,6 @@
 //= require third-party/jquery/dist/jquery.js
 //= require third-party/bootstrap/dist/js/bootstrap.js
-//= require third-party/slimScroll/jquery.slimscroll.js
+//= require third-party/jquery-slimscroll/jquery.slimscroll.js
 //= require third-party/hisrc/hisrc.js
 //= require third-party/jquery-ui/jquery-ui.js
 //= require third-party/modulus-uno-theme/js/flex.js
