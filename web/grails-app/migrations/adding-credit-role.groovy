34669a2140240dd0a022479b20f89a2e58b2342a
Karla <karla@makingdevs.com>
2020-02-21 11:45:05 -0600

    #736 Update credit role migration

--- web/grails-app/migrations/adding-credit-role.groovy
+++ web/grails-app/migrations/adding-credit-role.groovy
@@ -1,9 +1,5 @@
 databaseChangeLog = {
 
-    changeSet(author: "karlaju (generated)", id: "1582242414259-1") {
-        dropColumn(columnName: "credit_advance", tableName: "corporate")
-    }
-
     changeSet(author: "karla", id: "add-credit-role") {
 			grailsChange {
 				change {
